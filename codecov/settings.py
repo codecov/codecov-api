97b44e8aa4125f96bc5ede16575a56d54d56d6d2
Bas Kaal <3askaal@gmail.com>
2019-04-23 12:05:35 +0200

    change pagination page size back to 20

--- codecov/settings.py
+++ codecov/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 20
 }
 
 
