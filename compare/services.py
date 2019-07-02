389196f4f4b606a5adb74c3425f32f2ca763655f
Thiago Ramos <thiago@codecov.io>
2019-07-02 12:23:11 -0300

    Add flag comparison logic

--- compare/services.py
+++ compare/services.py
@@ -22,6 +22,18 @@ class Comparison(object):
             self._git_diff = self._calculate_git_diff()
         return self._git_diff
 
+    @property
+    def base_report(self):
+        if self._base_report is None:
+            self._base_report = self.report_service.build_report_from_commit(self.base_commit)
+        return self._base_report
+
+    @property
+    def head_report(self):
+        if self._head_report is None:
+            self._head_report = self.report_service.build_report_from_commit(self.head_commit)
+        return self._head_report
+
     def _calculate_git_diff(self):
         loop = asyncio.get_event_loop()
         user = self.context.get("user")
@@ -31,8 +43,23 @@ class Comparison(object):
             user, self.base_commit.repository).get_commit_diff(base_commit_sha, head_commit_sha)
         return loop.run_until_complete(task)
 
-    def base_report(self):
+    def _calculate_base_report(self):
         return self.report_service.build_report_from_commit(self.base_commit)
 
-    def head_report(self):
+    def _calculate_head_report(self):
         return self.report_service.build_report_from_commit(self.head_commit)
+
+    def flag_comparison(self, flag_name):
+        return FlagComparison(self, flag_name)
+
+
+class FlagComparison(object):
+
+    def __init__(self, comparison, flag_name):
+        self.comparison = comparison
+        self.flag_name = flag_name
+
+    @property
+    def diff_totals(self):
+        git_diff = self.comparison.git_diff
+        return self.comparison.head_report.flag(self.flag_name).apply_diff(git_diff)
