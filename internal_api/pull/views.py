3c63b6fc4882159b7492d5b84b66b0c5c694f095
Thiago Ramos <thiago@codecov.io>
2019-07-03 10:19:59 -0300

    Adding views to show pull request flags

--- internal_api/pull/views.py
+++ internal_api/pull/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import Http404
 
-from internal_api.mixins import RepoFilterMixin
+from internal_api.mixins import RepoFilterMixin, RepoSlugUrlMixin
+from internal_api.compare.serializers import FlagComparisonSerializer
+from compare.services import get_comparison_from_pull_request
 from core.models import Pull
 from .serializers import PullSerializer
 
@@ -12,3 +15,27 @@ class RepoPullList(RepoFilterMixin, generics.ListAPIView):
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filter_fields = ('state',)
     ordering_fields = ('updatestamp', 'head__timestamp')
+
+
+class RepoPullFlagsList(RepoSlugUrlMixin, generics.ListCreateAPIView):
+    serializer_class = FlagComparisonSerializer
+
+    def get_comparison(self):
+        user = self.request.user
+        repo = self.get_repo()
+        pullid = self.kwargs['pullid']
+        pull_requests = Pull.objects.filter(
+            repository=repo,
+            pullid=pullid
+        )
+        try:
+            obj = pull_requests.get()
+        except Pull.DoesNotExist:
+            raise Http404('No pull matches the given query.')
+        return get_comparison_from_pull_request(obj, user)
+
+    def get_queryset(self):
+        comparison = self.get_comparison()
+        return list(
+            comparison.flag_comparison(flag_name) for flag_name in comparison.available_flags
+        )
--- internal_api/tests/views/test_flags_view.py
+++ internal_api/tests/views/test_flags_view.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 import json
 
-from django.test.utils import override_settings
+from django.test import override_settings
 
-
-from core.tests.factories import CommitFactory, RepositoryFactory
+from core.tests.factories import CommitFactory, RepositoryFactory, PullFactory
 from archive.services import ArchiveService
 
 current_file = Path(__file__)
@@ -16,7 +15,7 @@ class TestFlagsView(object):
     def test_commit_flag_view(self, mocker, db, client, codecov_vcr):
         mocked = mocker.patch.object(ArchiveService, 'read_chunks')
         f = open(
-            current_file.parent.parent.parent.parent.parent / 'archive/tests/samples' / 'chunks.txt',
+            current_file.parent.parent.parent.parent / 'archive/tests/samples' / 'chunks.txt',
             'r'
         )
         mocker.patch.object(ArchiveService, 'create_root_storage')
@@ -42,8 +41,6 @@ class TestFlagsView(object):
         print(response.content)
         assert response.status_code == 200
         content = json.loads(response.content.decode())
-        import pprint
-        pprint.pprint(content)
         expected_result = {
             'count': 2,
             'next': None,
@@ -182,4 +179,159 @@ class TestFlagsView(object):
         assert content == expected_result
         mocked.assert_called_with(
             'abf6d4df662c47e32460020ab14abf9303581429'
+        )
+
+    @override_settings(DEBUG=True)
+    def test_pull_request_flags_view(self, mocker, db, client, codecov_vcr):
+        head_commit_sha = '68946ef98daec68c7798459150982fc799c87d85'
+        base_commit_sha = '00c7b4b49778b3c79427f9c4c13a8612a376ff19'
+        mocked = mocker.patch.object(ArchiveService, 'read_chunks')
+        head_f = open(
+            current_file.parent.parent / f'samples/{head_commit_sha}_chunks.txt',
+            'r'
+        )
+        base_f = open(
+            current_file.parent.parent / f'samples/{base_commit_sha}_chunks.txt',
+            'r'
+        )
+        mocker.patch.object(ArchiveService, 'create_root_storage')
+        mocked.side_effect = lambda x: head_f.read() if x == head_commit_sha else base_f.read()
+        repo = RepositoryFactory.create(
+            author__unencrypted_oauth_token='testaaft3ituvli790m1yajovjv5eg0r4j0264iw',
+            author__username='ThiagoCodecov'
+        )
+        parent_commit = CommitFactory.create(
+            message='test_report_serializer',
+            commitid=base_commit_sha,
+            repository=repo,
+        )
+        commit = CommitFactory.create(
+            message='test_report_serializer',
+            commitid=head_commit_sha,
+            parent_commit_id=parent_commit.commitid,
+            repository=repo,
+        )
+        pull = PullFactory.create(
+            base=parent_commit,
+            head=commit,
+            pullid=1010101,
+            author=repo.author,
+            repository=repo
+        )
+        client.force_login(user=repo.author)
+        url = f'/internal/{repo.author.username}/{repo.name}/pulls/{pull.pullid}/flags'
+        response = client.get(url)
+        assert response.status_code == 200
+        content = json.loads(response.content.decode())
+        import pprint
+        pprint.pprint(content)
+        expected_result = {
+            'count': 2,
+            'next': None,
+            'previous': None,
+            'results': [
+                {
+                    'base_report_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '79.16667',
+                        'diff': 0,
+                        'files': 3,
+                        'hits': 19,
+                        'lines': 24,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 5,
+                        'partials': 0,
+                        'sessions': 2
+                    },
+                    'diff_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '100',
+                        'diff': 0,
+                        'files': 1,
+                        'hits': 1,
+                        'lines': 1,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 0,
+                        'partials': 0,
+                        'sessions': 0
+                    },
+                    'head_report_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '80.00000',
+                        'diff': 0,
+                        'files': 3,
+                        'hits': 20,
+                        'lines': 25,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 5,
+                        'partials': 0,
+                        'sessions': 2
+                    },
+                    'name': 'unittests'
+                },
+                {
+                    'base_report_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '79.16667',
+                        'diff': 0,
+                        'files': 3,
+                        'hits': 19,
+                        'lines': 24,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 5,
+                        'partials': 0,
+                        'sessions': 2
+                    },
+                    'diff_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '0',
+                        'diff': 0,
+                        'files': 1,
+                        'hits': 0,
+                        'lines': 1,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 1,
+                        'partials': 0,
+                        'sessions': 0
+                    },
+                    'head_report_totals': {
+                        'branches': 0,
+                        'complexity': 0,
+                        'complexity_total': 0,
+                        'coverage': '56.00000',
+                        'diff': 0,
+                        'files': 3,
+                        'hits': 14,
+                        'lines': 25,
+                        'messages': 0,
+                        'methods': 0,
+                        'misses': 11,
+                        'partials': 0,
+                        'sessions': 2
+                    },
+                    'name': 'integrations'
+                }
+            ]
+        }
+        assert content == expected_result
+        mocked.assert_any_call(
+            head_commit_sha
+        )
+        mocked.assert_any_call(
+            base_commit_sha
         )
