496671ee7e613acce4c2211c02b1dd28c1c5fb22
Sabreen Noah <sabreen.noah@gmail.com>
2020-02-19 00:15:46 -0800

    Fix validate yaml reference

--- validate/views.py
+++ validate/views.py
@@ -11,7 +11,7 @@ class ValidateYamlHandler(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST, data='No content posted.')
         else:
             try:
-                validated_yml = self.validate_yaml(self.request.body)
+                validated_yml = validate_yaml(self.request.body)
                 return Response(status=status.HTTP_200_OK, data=f"Valid!\n\n{dumps(validated_yml, indent=2)}\n")
             except InvalidYamlException:
                 self.log('error', 'User encoutered error while validating yaml file', remote_ip=str(self.request.remote_ip), request_body=self.request.body)
