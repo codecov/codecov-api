1de39a0612de1e6654ffbe9735a36438479d3995
Josh Pollara <joshpollara@gmail.com>
2020-06-01 20:23:59 +0200

    pipeline test

--- prod.sh
+++ prod.sh
@@ -7,3 +7,5 @@ if [ -f "/usr/local/bin/berglas" ]; then
 else
   gunicorn codecov.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 fi
+
+# foobar
