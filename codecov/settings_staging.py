8e0a87f4769a816a132afcc0c2a9ea961e0f16d4
Eli Hooten <elir.hooten@gmail.com>
2020-08-10 12:56:13 -0500

    Remove `CORS_ALLOW_CREDENTIALS`

--- codecov/settings_staging.py
+++ codecov/settings_staging.py
@@ -35,5 +35,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = ["https://stage-web.codecov.dev"]
 CORS_ORIGIN_REGEX_WHITELIST = [r"^https:\/\/deploy-preview-[0-9]+--zen-dubinsky-9aced3\.netlify\.app$"]
-CORS_ALLOW_CREDENTIALS = True
 CODECOV_URL = 'https://stage-web.codecov.dev'
