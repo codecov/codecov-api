072df278e4b6afc40c49f2899c6e7943d641f8c6
guillaume <guillaume@codecov.io>
2020-08-13 16:10:03 -0400

    Change the filter for the repository list
    The previous filter expected you to pass `names=test1,test2,test3,...`, but the way lists are handled in modern URLs is by passing the parameter multiple times, `names=test1&names=test2&names=test3&...`. A custom filter needed to be created to make sure we followed this standard.
    
    This PR creates a custom filter for repository names that will extract the list from the query parameters and filter with that rather than rely on the comma splitting form django.
    
    Work is related to https://codecovio.atlassian.net/browse/CE-2283

--- internal_api/repo/views.py
+++ internal_api/repo/views.py
@@ -1,10 +1,6 @@
 import uuid
 import logging
 
-from django.db.models import Subquery, OuterRef, Q
-from django.shortcuts import get_object_or_404
-from django.utils.functional import cached_property
-
 from rest_framework import filters, mixins, viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
@@ -12,10 +8,9 @@ from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS # ['GET', 'HEAD', 'OPTIONS']
 from rest_framework import status
 
-from django_filters import rest_framework as django_filters, BooleanFilter, BaseInFilter
+from django_filters import rest_framework as django_filters, BooleanFilter
 
-from codecov_auth.models import Owner
-from core.models import Repository, Commit
+from core.models import Repository
 from services.repo_providers import RepoProviderService
 from services.decorators import torngit_safe
 from internal_api.permissions import RepositoryPermissionsService
@@ -35,13 +30,30 @@ from .repository_actions import delete_webhook_on_provider, create_webhook_on_pr
 
 log = logging.getLogger(__name__)
 
+class StringListFilter(django_filters.Filter):
+    def __init__(self, query_param, *args, **kwargs):
+        super(StringListFilter, self).__init__(*args, **kwargs)
+        self.query_param = query_param
+
+    def filter(self, qs, value):
+        try:
+            request = self.parent.request
+        except AttributeError:
+            return qs
+
+        values = request.GET.getlist(self.query_param)
+        if len(values) > 0:
+            return qs.filter(**{'%s__%s'%(self.field_name, self.lookup_expr):values})
+
+        return qs
+
 
 class RepositoryFilters(django_filters.FilterSet):
     """Filter for active repositories"""
     active = BooleanFilter(field_name='active', method='filter_active')
 
     """Filter for getting multiple repositories by name"""
-    names = BaseInFilter(field_name='name', lookup_expr='in')
+    names = StringListFilter(query_param='names', field_name='name', lookup_expr='in')
 
     def filter_active(self, queryset, name, value):
         # The database currently stores 't' instead of 'true' for active repos, and nothing for inactive
