299c97602e0bd3929d3dd31e448c925a7d5b4bff
Sabreen Noah <sabreen.noah@gmail.com>
2020-08-17 20:24:09 -0700

    Parse upload request parameters

--- upload/helpers.py
+++ upload/helpers.py
@@ -1,13 +1,129 @@
 from cerberus import Validator
 from rest_framework.exceptions import ValidationError
 
-# TODO
-def validate_params(data):
+from .constants import ci
 
-    params_schema = {
-        "version": {"type": "string", "required": True, "allowed": ["v2", "v4"]}
+
+def parse_params(data):
+    """
+    This function will validate the input request parameters and do some additional parsing/tranformation of the params.
+    """
+
+    # filter out empty values from the data; this makes parsing and setting defaults a bit easier
+    filtered_data = {
+        key: value for key, value in data.items() if value not in [None, ""]
     }
 
-    v = Validator(params_schema)
-    if not v.validate(data):
+    params_schema = {
+        "version": {"type": "string", "required": True, "allowed": ["v2", "v4"]},
+        # commit SHA
+        "commit": {
+            "type": "string",
+            "required": True,
+            "regex": r"^\d+:\w{12}|\w{40}$",
+            "coerce": lambda value: value.lower(),
+        },
+        "slug": {"type": "string", "regex": r"^[\w\-\.]{1,255}\/[\w\-\.]{1,255}$"},
+        # owner username, we set this by parsing the value of "slug" if provided
+        "owner": {
+            "type": "string",
+            "nullable": True,
+            "default_setter": (
+                lambda document: document.get("slug").rsplit("/", 1)[0]
+                if document.get("slug")
+                and len(document.get("slug").rsplit("/", 1)) == 2
+                else None
+            ),
+        },
+        # repo name, we set this by parsing the value of "slug" if provided
+        "repo": {
+            "type": "string",
+            "nullable": True,
+            "default_setter": (
+                lambda document: document.get("slug").rsplit("/", 1)[1]
+                if document.get("slug")
+                and len(document.get("slug").rsplit("/", 1)) == 2
+                else None
+            ),
+        },
+        # repository upload token
+        "token": {
+            "type": "string",
+            "regex": r"^[0-9a-f]{8}(-?[0-9a-f]{4}){3}-?[0-9a-f]{12}$",
+        },
+        # name of the CI service used, must be a name in the list of CI services we support
+        "service": {
+            "type": "string",
+            "allowed": list(ci.keys()),
+            "coerce": (
+                lambda value: "travis" if value == "travis-org" else value,
+            ),  # if "travis-org" was passed as the service rename it to "travis" before validating
+        },
+        # pull request number
+        # if a value is passed to the "pull_request" field and not to "pr", we'll use that to set the value of this field
+        "pr": {
+            "type": "string",
+            "regex": r"^(\d+|false|null|undefined|true)$",
+            "nullable": True,
+            "default_setter": (lambda document: document.get("pull_request")),
+            "coerce": (
+                lambda value: None if value in ["false", "null", "undefined"] else value
+            ),
+        },
+        # pull request number
+        # "deprecated" in the sense that if a value is passed to this field, we'll use it to set "pr" and use that field instead
+        "pull_request": {  # pull request number
+            "type": "string",
+            "regex": r"^(\d+|false|null|undefined|true)$",
+            "nullable": True,
+            "coerce": (
+                lambda value: None
+                if value in ["false", "null", "undefined", "true"]
+                else value
+            ),
+        },
+        "build_url": {"type": "string", "regex": r"^https?\:\/\/(.{,100})",},
+        "flags": {"type": "string", "regex": r"^[\w\.\-\,]+$",},
+        # if prefixed with "origin/" or "refs/heads", the prefix will be removed
+        "branch": {
+            "type": "string",
+            "coerce": (
+                lambda value: value[7:]
+                if value[:7] == "origin/"
+                else value[11:]
+                if value[:11] == "refs/heads/"
+                else value,
+            ),
+        },
+        "tag": {"type": "string"},
+        # if a value is passed to "travis_job_id" and not to "job", we'll use that to set the value of this field
+        "job": {
+            "type": "string",
+            "nullable": True,
+            "default_setter": (lambda document: document.get("travis_job_id")),
+        },
+        # "deprecated" in the sense that if a value is passed to this field, we'll use it to set "job" and use that field instead
+        "travis_job_id": {"type": "string", "nullable": True, "empty": True},
+        "build": {
+            "type": "string",
+            "nullable": True,
+            "coerce": (
+                lambda value: None
+                if value in ["null", "undefined", "none", "nil"]
+                else value
+            ),
+        },
+        "name": {"type": "string"},
+        "package": {"type": "string"},
+        "s3": {"type": "integer"},
+        "yaml": {"type": "string"},
+        "url": {"type": "string"},
+        "root": {"type": "string",},  # deprecated
+    }
+
+    v = Validator(params_schema, allow_unknown=True)
+    if not v.validate(filtered_data):
         raise ValidationError(v.errors)
+
+    # return validated data, including coerced values
+    return v.document
--- upload/tests/test_upload.py
+++ upload/tests/test_upload.py
@@ -1,22 +1,163 @@
 from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from unittest.mock import patch
 from json import dumps
 from yaml import YAMLError
-
+from django.test import TestCase
 from django.conf import settings
 from django.test import RequestFactory
+from urllib.parse import urlencode
+
+from upload.helpers import parse_params
 
 
-class TestUploadHandler(APITestCase):
+class UploadHandlerHelpersTest(TestCase):
+    def test_parse_params_validates_valid_input(self):
+        request_params = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "slug": "codecov/codecov-api",
+            "token": "testbtznwf3ooi3xlrsnetkddj5od731pap9",
+            "service": "circleci",
+            "pull_request": "undefined",
+            "flags": "this-is-a-flag,this-is-another-flag",
+            "name": "",
+            "branch": "",
+            "param_doesn't_exist_but_still_should_not_error": True,
+            "s3": 123,
+            "build_url": "https://thisisabuildurl.com",
+        }
+
+        expected_result = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "slug": "codecov/codecov-api",
+            "owner": "codecov",
+            "repo": "codecov-api",
+            "token": "testbtznwf3ooi3xlrsnetkddj5od731pap9",
+            "service": "circleci",
+            "pr": None,
+            "pull_request": None,
+            "flags": "this-is-a-flag,this-is-another-flag",
+            "param_doesn't_exist_but_still_should_not_error": True,
+            "s3": 123,
+            "build_url": "https://thisisabuildurl.com",
+            "job": None,
+        }
+
+        parsed_params = parse_params(request_params)
+        assert expected_result == parsed_params
+
+    def test_parse_params_errors_for_invalid_input(self):
+        request_params = {
+            "version": "v5",
+            "slug": "not-a-valid-slug",
+            "token": "testbtznwf3ooi3xlrsnetkddj5od731pap9",
+            "service": "circleci",
+            "pr": 123,
+            "flags": "not_a_valid_flag!!?!",
+            "s3": "this should be an integer",
+            "build_url": "not a valid url!",
+        }
+
+        with self.assertRaises(ValidationError) as err:
+            parse_params(request_params)
+
+        assert len(err.exception.detail) == 7
+
+    def test_parse_params_transforms_input(self):
+        request_params = {
+            "version": "v4",
+            "commit": "3BE5C52BD748C508a7e96993c02cf3518c816e84",
+            "slug": "codecov/codecov-api",
+            "service": "travis-org",
+            "pull_request": "439",
+            "pr": "",
+            "branch": "origin/test-branch",
+            "travis_job_id": "travis-jobID",
+            "build": "nil",
+        }
+
+        expected_result = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",  # converted to lower case
+            "slug": "codecov/codecov-api",
+            "owner": "codecov",  # extracted from slug
+            "repo": "codecov-api",  # extracted from slug
+            "service": "travis",  # "travis-org" converted to "travis"
+            "pr": "439",  # populated from "pull_request" field since none was provided
+            "pull_request": "439",
+            "branch": "test-branch",  # "origin/" removed from name
+            "job": "travis-jobID",  # populated from "travis_job_id" since none was provided
+            "travis_job_id": "travis-jobID",
+            "build": None,  # "nil" coerced to None
+        }
+
+        parsed_params = parse_params(request_params)
+        assert expected_result == parsed_params
+
+        request_params = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "pr": "967",
+            "pull_request": "null",
+            "branch": "refs/heads/another-test-branch",
+            "job": "jobID",
+            "travis_job_id": "travis-jobID",
+        }
+
+        expected_result = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "owner": None,  # set to "None" since slug wasn't provided
+            "repo": None,  # set to "None" since slug wasn't provided
+            "pr": "967",  # not populated from "pull_request"
+            "pull_request": None,  # "null" coerced to None
+            "branch": "another-test-branch",  # "refs/heads" removed
+            "job": "jobID",  # not populated from "travis_job_id"
+            "travis_job_id": "travis-jobID",
+        }
+
+        parsed_params = parse_params(request_params)
+        assert expected_result == parsed_params
+
+        request_params = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "slug": "",
+            "pr": "",
+            "pull_request": "156",
+            "job": None,
+            "travis_job_id": "travis-jobID",
+        }
+
+        expected_result = {
+            "version": "v4",
+            "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+            "owner": None,  # set to "None" since slug wasn't provided
+            "repo": None,  # set to "None" since slug wasn't provided
+            "pr": "156",  # populated from "pull_request"
+            "pull_request": "156",
+            "job": "travis-jobID",  # populated from "travis_job_id"
+            "travis_job_id": "travis-jobID",
+        }
+
+        parsed_params = parse_params(request_params)
+        assert expected_result == parsed_params
+
+
+class UploadHandlerRouteTest(APITestCase):
 
     # Wrap client calls
     def _options(self, kwargs=None, data=None):
         return self.client.options(reverse("upload-handler", kwargs=kwargs))
 
-    def _post(self, kwargs=None, data=None):
-        return self.client.post(reverse("upload-handler", kwargs=kwargs), data=data)
+    def _post(self, kwargs=None, data=None, query=None):
+        query_string = f"?{urlencode(query)}" if query else ""
+        url = reverse("upload-handler", kwargs=kwargs) + query_string
+        return self.client.post(url, data=data)
 
     # Test headers
     def test_options_headers(self):
@@ -68,3 +209,29 @@ class TestUploadHandler(APITestCase):
                 "Origin, Content-Type, Accept, X-User-Agent",
             )
             assert headers["content-type"] == ("Content-Type", "text/plain",)
+
+    def test_param_parsing(self):
+        with self.subTest("valid"):
+            query_params = {
+                "commit": "3be5c52bd748c508a7e96993c02cf3518c816e84",
+                "pr": "",
+                "pull_request": "9838",
+                "branch": "",
+                "flags": "",
+                "build_url": "",
+                "travis_job_id": "abc",
+            }
+
+            response = self._post(kwargs={"version": "v4"}, query=query_params)
+
+            assert response.status_code == 200
+
+        with self.subTest("invalid"):
+            query_params = {
+                "pr": 9838,
+                "flags": "flags!!!",
+            }
+
+            response = self._post(kwargs={"version": "v5"}, query=query_params)
+
+            assert response.status_code == status.HTTP_400_BAD_REQUEST
