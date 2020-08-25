bb2b242f7efe15248f40c3342fd50de3ed9a34e4
guillaume <guillaume@codecov.io>
2020-08-25 09:58:36 -0400

    Add support for sorting repositories by various fields
    We wanted to be able to sort repositories by their coverage data using the repository list in the various pages of the new UI.
    
    This PR adds sorting to the repository endpoints using the fields form the latest commit. It will not impact the API too much and should still have the same performance. The implementation can be improved, but it will work for the needs we have right now.
    
    Work is related to https://codecovio.atlassian.net/browse/CE-2201.

--- core/managers.py
+++ core/managers.py
@@ -1,4 +1,6 @@
-from django.db.models import QuerySet, Subquery, OuterRef, Q, Count, F, FloatField
+from dateutil import parser
+
+from django.db.models import QuerySet, Subquery, OuterRef, Q, Count, F, FloatField, Max
 from django.db.models.functions import Cast
 from django.contrib.postgres.fields.jsonb import KeyTextTransform
 
@@ -16,28 +18,70 @@ class RepositoryQuerySet(QuerySet):
         )
 
     def exclude_uncovered(self):
-        from core.models import Commit
-        return self.annotate(
-            totals=Subquery(
-                Commit.objects.filter(
-                    repository_id=OuterRef('repoid'),
-                    branch=OuterRef('branch')
-                ).order_by('-timestamp').values('totals')[:1]
-            )
-        ).exclude(totals__isnull=True)
+        return self.exclude(latest_commit__totals__isnull=True)
 
-    def with_current_coverage(self):
+    def with_latest_commit_before(self, before_date, branch_param):
         """
         Annotates queryset with coverage of latest commit value. Primarily useful for
         sorting by coverage via API query parameters.
         """
         from core.models import Commit
+
+        # Parsing the date given in parameters so we receive a datetime rather than a string
+        timestamp = parser.parse(before_date)
+
+        commit_query_set = Commit.objects.filter(
+            repository_id=OuterRef('repoid'),
+            state=Commit.CommitStates.COMPLETE,
+            branch=branch_param or OuterRef("branch"),
+            # The __date cast function will case the datetime based timestamp on the commit to a date object that only
+            # contains the year, month and day. This allows us to filter through a daily granularity rather than
+            # a second granularity since this is the level of granularity we get from other parts of the API.
+            timestamp__date__lte=timestamp
+        ).order_by('-timestamp').annotate(
+            # Annotate all the database fields to better known named fields
+            coverage=Cast(KeyTextTransform("c", "totals"), output_field=FloatField()),
+            lines=Cast(KeyTextTransform("n", "totals"), output_field=FloatField()),
+            hits=Cast(KeyTextTransform("h", "totals"), output_field=FloatField()),
+            misses=Cast(KeyTextTransform("m", "totals"), output_field=FloatField()),
+            partials=Cast(KeyTextTransform("p", "totals"), output_field=FloatField()),
+            complexity=Cast(KeyTextTransform("C", "totals"), output_field=FloatField())
+        )
+
+        # We annotate a bunch of data from the latest commit to the repository to enable two things:
+        # - First we add the latest commit id to the repository so we can more easily fetch it in the serializer. This
+        # is still very slow since we keep the n+1 problem, but it will be easier to optimize later if the logic is not
+        # duplicated between the manager and serializer.
+        # - Second, we add all the specific totals fields we want to be able to sort with directly on the repository.
+        # This will enabled the ordering using rest framework and will not cause errors due to django trying to access
+        # structured fields.
         return self.annotate(
+            latest_commitid=Subquery(
+                commit_query_set.values("commitid")[:1]
+            ),
             coverage=Subquery(
-                Commit.objects.filter(
-                    repository_id=OuterRef('repoid'),
-                    branch=OuterRef('branch')
-                ).order_by('-timestamp').values('totals__c')[:1]
+                commit_query_set.values("coverage")[:1],
+                output_field=FloatField()
+            ),
+            lines=Subquery(
+                commit_query_set.values("lines")[:1],
+                output_field=FloatField()
+            ),
+            hits=Subquery(
+                commit_query_set.values("hits")[:1],
+                output_field=FloatField()
+            ),
+            partials=Subquery(
+                commit_query_set.values("partials")[:1],
+                output_field=FloatField()
+            ),
+            misses=Subquery(
+                commit_query_set.values("misses")[:1],
+                output_field=FloatField()
+            ),
+            complexity=Subquery(
+                commit_query_set.values("complexity")[:1],
+                output_field=FloatField()
             )
         )
 
