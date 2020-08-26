9ba5541fe50f23b90a57088f6229ae68adcfe668
Pierce McEntagart <pierce@codecov.io>
2020-08-26 12:38:31 -0600

    Revert "Associate logs with traces in datadog. (#295)" (#298)
    This reverts commit 1cee304c69f0828a9f8b8a441538d0a9fa70f2d9.

--- prod.sh
+++ prod.sh
@@ -3,7 +3,7 @@
 # Starts the production gunicorn server (no --reload)
 echo "Starting gunicorn in production mode"
 if [ -f "/usr/local/bin/berglas" ]; then
-  berglas exec -- ddtrace-run gunicorn codecov.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT} DD_LOGS_INJECTION=true
+  berglas exec -- ddtrace-run gunicorn codecov.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 else
-  ddtrace-run gunicorn codecov.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT} DD_LOGS_INJECTION=true
+  ddtrace-run gunicorn codecov.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 fi
