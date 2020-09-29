e35aa167e507221064e4107cc9b804ab55eea412
Subhi Beidas <subhibeidas@gmail.com>
2020-09-29 21:08:50 +0300

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -5,5 +5,5 @@ echo "Starting gunicorn in production mode"
 if [ -f "/usr/local/bin/berglas" ]; then
   berglas exec -- ddtrace-run gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 else
-  ddtrace-run gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
+  ddtrace-run gunicorn codecov.wsgi:application --workers=3 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 fi
