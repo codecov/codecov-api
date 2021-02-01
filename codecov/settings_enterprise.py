0aabb87b86a04acc30220f44bc516b8a82ec74d0
Pierce McEntagart <pierce@codecov.io>
2021-02-01 17:22:15 -0500

    Add sane defaults; add STRIPE_API_KEY = None.

--- codecov/settings_enterprise.py
+++ codecov/settings_enterprise.py
@@ -3,7 +3,17 @@ import os
 from utils.config import get_config, get_settings_module
 
 DEBUG = False
-ALLOWED_HOSTS = get_config("setup", "api_allowed_hosts")
-CORS_ORIGIN_WHITELIST = get_config("setup", "api_cors_origin_whitelist")
+ALLOWED_HOSTS = get_config("setup", "api_allowed_hosts", default=["*"])
 CORS_ALLOW_CREDENTIALS = True
 CODECOV_URL = get_config("setup", "codecov_url")
+
+# select out CODECOV_URL domain
+if CODECOV_URL.startswith("https://"):
+    DEFAULT_WHITELISTED_DOMAIN = CODECOV_URL[8:]
+elif CODECOV_URL.startswith("http://"):
+    DEFAULT_WHITELISTED_DOMAIN = CODECOV_URL[7:]
+
+CORS_ORIGIN_WHITELIST = get_config("setup", "api_cors_origin_whitelist", default=[DEFAULT_WHITELISTED_DOMAIN])
+
+# Referenced at module level of services/billing.py, so it needs to be defined
+STRIPE_API_KEY = None
