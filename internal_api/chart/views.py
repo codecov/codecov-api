6a141fa2025ffb38d420d3f112580f8d0e4d0822
Pierce McEntagart <pierce@codecov.io>
2021-02-09 14:49:21 -0500

    Clean up view code.

--- internal_api/chart/views.py
+++ internal_api/chart/views.py
@@ -9,28 +9,11 @@ from .filters import apply_default_filters, apply_simple_filters
 from .helpers import (
     annotate_commits_with_totals,
     apply_grouping,
-    aggregate_across_repositories,
     validate_params,
+    validate_analytics_chart_params,
 )
 from internal_api.permissions import ChartPermissions
 from internal_api.mixins import RepositoriesMixin
-
-from django.db import connection
-from django.db.models import F
-from django.db.models.functions import Trunc
-from datetime import datetime
-
-
-def dictfetchall(cursor):
-    """
-    Return all rows from a cursor as a dict
-    Copied from: https://docs.djangoproject.com/en/3.1/topics/db/sql/#executing-custom-sql-directly
-    """
-    columns = [col[0] for col in cursor.description]
-    return [
-        dict(zip(columns, row))
-        for row in cursor.fetchall()
-    ]
 
 
 class RepositoryChartHandler(APIView, RepositoriesMixin):
@@ -178,112 +161,54 @@ class OrganizationChartHandler(APIView, RepositoriesMixin):
     permission_classes = [IsAuthenticated]
     parser_classes = [JSONParser]
 
-    def post(self, request, *args, **kwargs):
-        request_params = {**self.request.data, **self.kwargs}
-        validate_params(request_params)
+    def _get_ordering(self, request_params):
+        return "DESC" if request_params.get("ordering") == "decreasing" else ""
 
-        organization = Owner.objects.get(service=kwargs["service"], username=kwargs["owner_username"])
-        viewable_repoid_set = organization.repository_set.viewable_repos(request.user).values_list("repoid", flat=True)
-
-        # determine start date to use
-        if "start_date" in request_params:
-            start_date = parser.parse(request_params.get("start_date"))
-        else:
-            first_commit_date = Commit.objects.filter(
-                repository__repoid__in=viewable_repoid_set,
-                repository__branch=F('branch')
-            ).annotate(
-                truncated_date=Trunc('timestamp', request_params.get("grouping_unit"))
-            ).order_by('-timestamp').values_list('truncated_date', flat=True)[0]
-
-            start_date = datetime.date(first_commit_date)
-
-        # determine end date to use
+    def _get_end_date(self, request_params):
+        # Determine end date to use, default is now
         if "end_date" in request_params:
             end_date = parser.parse(request_params.get("end_date"))
         else:
             end_date = datetime.date(datetime.now())
+        return end_date
 
-        with connection.cursor() as cursor:
-            result = cursor.execute(
-                """
-                WITH date_series AS (
-                	SELECT
-                		t::date AS "date"
-                	FROM generate_series(timestamp '{start_date}', timestamp '{end_date}', '1 {grouping_unit}') t
-                ), graph_repos AS (
-                	SELECT
-                		r.repoid,
-                		r.name,
-                        r.branch
-                	FROM 
-                		repos r
-                	WHERE r.repoid IN {viewable_repoid_set}
-                ), spine AS (
-                    SELECT
-                        ds.date,
-                        r.repoid
-                    FROM date_series ds
-                    CROSS JOIN graph_repos r
-                ), t_ranked_commits AS (
-                	SELECT
-                		ROW_NUMBER() OVER (PARTITION BY c.repoid, DATE_TRUNC('{grouping_unit}', c.timestamp) ORDER BY timestamp DESC NULLS LAST) AS commit_rank,
-                		DATE_TRUNC('{grouping_unit}', c.timestamp) AS "truncated_date",
-                		c.timestamp AS commit_timestamp,
-                		c.id,
-                		r.repoid
-                	FROM
-                		commits c
-                	INNER JOIN graph_repos r ON r.repoid = c.repoid AND r.branch = c.branch
-                ), commits_spine AS (
-                	SELECT 
-                		s.date AS spine_date,
-                		drc.truncated_date AS truncated_commit_date,
-                		drc.commit_timestamp,
-                		drc.id AS commit_id,
-                		s.repoid
-                	FROM spine s
-                	LEFT JOIN t_ranked_commits drc ON drc.truncated_date = s.date AND drc.repoid = s.repoid AND drc.commit_rank = 1
-                ), grouped AS (
-                	SELECT
-                		spine_date,
-                		truncated_commit_date,
-                		commit_id,
-                		repoid,
-                		SUM(CASE WHEN commit_id IS NOT NULL THEN 1 END) OVER (PARTITION BY repoid ORDER BY spine_date) AS grp_commit
-                    FROM commits_spine
-                ), corrected AS (
-                	SELECT
-                	  	spine_date,
-                	    truncated_commit_date,
-                	    commit_id, 
-                	    FIRST_VALUE(commit_id) OVER (PARTITION BY repoid, grp_commit ORDER BY spine_date) AS corrected_commit
-                	FROM
-                		grouped
-                )
-                SELECT
-                	spine_date::timestamp at time zone 'UTC' AS date,
-                	SUM((c.totals->>'h')::numeric) AS total_hits,
-                	SUM((c.totals->>'m')::numeric) AS total_misses,
-                	SUM((c.totals->>'p')::numeric) AS total_partials,
-                	SUM((c.totals->>'n')::numeric) AS total_lines,
-                    SUM((c.totals->>'h')::numeric) / SUM((c.totals->>'n')::numeric) * 100 AS coverage
-                FROM
-                	corrected
-                LEFT JOIN commits c ON c.id = corrected_commit
-                GROUP BY spine_date
-                ORDER BY spine_date {ordering_param};
-                """.format(
-                    start_date=start_date,
-                    end_date=end_date,
+    def _get_start_date(self, request_params):
+        # Determine start date to use
+        if "start_date" in request_params:
+            start_date = parser.parse(request_params.get("start_date"))
+        else:
+            start_date = None
+        return start_date
+
+    def _get_repoids(self, user, request_params):
+        # Get organization
+        organization = Owner.objects.get(
+            service=request_params["service"],
+            username=request_params["owner_username"]
+        )
+
+        # Get list of relevant repoids
+        repoids = organization.repository_set.viewable_repos(
+            user
+        ).values_list("repoid", flat=True)
+
+        if request_params.get("repositories", []):
+            repoids = repoids.filter(name__in=request_params.get("repositories", []))
+
+        return repoids
+
+    def post(self, request, *args, **kwargs):
+        request_params = {**self.request.data, **self.kwargs}
+        validate_analytics_chart_params(request_params)
+
+        return Response(
+            data={
+                "coverage": retrieve_org_analytics_data(
+                    repoids=self._get_repoids(request.user, request_params),
+                    start_date=self._get_start_date(request_params),
+                    end_date=self._get_end_date(request_params),
                     grouping_unit=request_params.get("grouping_unit"),
-                    viewable_repoid_set=tuple(viewable_repoid_set),
-                    ordering_param="DESC" if request_params.get("coverage_timestamp_ordering") == "decreasing" else ""
+                    ordering=self._get_ordering(request_params)
                 )
-            )
-
-            return Response(
-                data={
-                    "coverage": dictfetchall(cursor)
-                }
-            )
+            }
+        )
