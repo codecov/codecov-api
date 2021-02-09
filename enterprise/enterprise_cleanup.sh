0cea0d2a6bc1dd8bfcb7119eef40983156992bbc
Eli Hooten <elir.hooten@gmail.com>
2021-02-09 13:52:03 -0600

    Add Image Deps and Move Files

---
+++ enterprise/enterprise_cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## remove all the files from prod/dev systems that we won't need in enteprise
+cd /
+mkdir enterprise
+cp -a /app/build/lib.linux-x86_64-3.7/. /enterprise/
+cp /app/manage.py /enterprise/manage.py
+cp /app/conftest.py /enterprise/conftest.py
+cp /app/celery_config.py /enterprise/celery_config.py
+
+# must copy over migrations becuase they don't compile.
+cp -rf /app/core/migrations/ /enterprise/migrations
+
+# delete the .py files, etc
+rm -rf /app
+
+# move needed files back to /app directory
+mv /enterprise /app
