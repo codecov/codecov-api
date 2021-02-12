93c961cfeb5ec2d93fbc242ca43838c28e95cccc
Pierce McEntagart <pierce@codecov.io>
2021-02-12 13:48:36 -0500

    Add optional trailing slash for chart routes.

--- internal_api/chart/urls.py
+++ internal_api/chart/urls.py
@@ -4,12 +4,12 @@ from .views import RepositoryChartHandler, OrganizationChartHandler
 
 urlpatterns = [
     path(
-        "<str:service>/<str:owner_username>/coverage/repository",
+        "<str:service>/<str:owner_username>/coverage/repository/?",
         RepositoryChartHandler.as_view(),
         name="chart-coverage-repository",
     ),
     path(
-        "<str:service>/<str:owner_username>/coverage/organization",
+        "<str:service>/<str:owner_username>/coverage/organization/?",
         OrganizationChartHandler.as_view(),
         name="chart-coverage-organization",
     ),
