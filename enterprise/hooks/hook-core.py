7d5149438fb363e301e64a3eb277d3f7b211cea5
trent.schmidt <sch43228@gmail.com>
2021-03-04 11:37:07 -0600

    Fixing comments from Eli and mock_redis test error.

--- enterprise/hooks/hook-core.py
+++ enterprise/hooks/hook-core.py
@@ -1,4 +1,3 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
-# This collects all dynamically imported scrapy modules and data files.
 datas = collect_data_files('core', include_py_files=True, subdir="migrations")
--- enterprise/package.sh
+++ enterprise/package.sh
@@ -15,11 +15,6 @@ echo $(find . -name "*.so")
 mkdir src
 echo 'true' > src/is_enterprise
 
-# move migrations from app folders to api dir for sideloading. 
-# cd /app
-# find -type d | grep -i /migrations | sed 's|^./||' | xargs -i mkdir -p /app/dist/{}
-# find -type f | grep -i /migrations/ | sed 's|^./||' | xargs -i cp -rf {} /app/dist/{}
-
 # # Exclude pycrypto and PyInstaller from built packages
 pyinstaller -F --debug=imports --exclude-module pycrypto --exclude-module PyInstaller --add-data src:/src --name api --clean --key $random_key ${args_to_use} ${pyinstaller_args} /app/manage.py
 
