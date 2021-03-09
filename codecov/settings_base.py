f7b6c5a5748691b403b68dbd3190fe92d40824e9
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-09 11:44:55 -0600

    setup whitenoise to serve static files

--- codecov/settings_base.py
+++ codecov/settings_base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
