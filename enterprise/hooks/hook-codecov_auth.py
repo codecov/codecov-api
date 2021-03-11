9162c7a5d77ee4cefbc4944bd5041d44f4a1b7dc
trent.schmidt <sch43228@gmail.com>
2021-03-11 11:58:01 -0600

    Adding to codecov auth to test why my hook did not work

--- enterprise/hooks/hook-codecov_auth.py
+++ enterprise/hooks/hook-codecov_auth.py
@@ -5,7 +5,7 @@ package_path = get_package_paths("text_unidecode")
 data_bin_path = os.path.join(package_path[1], "data.bin")
 datas = (collect_data_files('codecov_auth', include_py_files=True, subdir="migrations") +
          collect_data_files('text_unidecode') +
-         (data_bin_path, 'text_unidecode'))
+         [(data_bin_path, 'text_unidecode')])
 
 
 
