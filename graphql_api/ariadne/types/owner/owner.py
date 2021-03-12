eeb0fd12d1bd1dc89c2dc66192f26cd8261877e4
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-12 12:37:59 -0600

    expose a list of repositories on ariadne

--- graphql_api/ariadne/types/owner/owner.py
+++ graphql_api/ariadne/types/owner/owner.py
@@ -1,6 +1,12 @@
 from ariadne import ObjectType
 
 from graphql_api.helpers import ariadne_load_local_graphql
+from graphql_api.actions.repository import list_repository_for_owner
 
 owner = ariadne_load_local_graphql(__file__, "owner.graphql")
 owner_bindable = ObjectType("Owner")
+
+@owner_bindable.field("repositories")
+def resolve_repositories(owner, info):
+    actor = info.context['request'].user
+    return list_repository_for_owner(actor, owner)
