dc16ac2a8ffc99e11e29336c73cff19eee8b38dc
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-12 16:54:53 -0600

    list my repositories in graphql

--- graphql_api/graphene/types/owner.py
+++ graphql_api/graphene/types/owner.py
@@ -1,9 +1,19 @@
-from graphene_django import DjangoObjectType
+from graphene import relay
+from graphene_django import DjangoObjectType, DjangoConnectionField
 
 from codecov_auth.models import Owner
-
+from core.models import Repository
+from graphql_api.actions.repository import list_repository_for_owner
+from .repository import RepositoryType
 
 class OwnerType(DjangoObjectType):
+
     class Meta:
         model = Owner
         fields = ("username",)
+
+    repositories = DjangoConnectionField(RepositoryType)
+
+    def resolve_repositories(self, info, *args, **kwargs):
+        actor = info.context.user
+        return list_repository_for_owner(actor, self)
