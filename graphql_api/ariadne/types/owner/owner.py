9b08f61325286291c391eb2c86641f2a3380495a
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-12 15:23:31 -0600

    make the list of repository paginated

--- graphql_api/ariadne/types/owner/owner.py
+++ graphql_api/ariadne/types/owner/owner.py
@@ -1,12 +1,32 @@
 from ariadne import ObjectType
+from cursor_pagination import CursorPaginator
 
 from graphql_api.helpers import ariadne_load_local_graphql
 from graphql_api.actions.repository import list_repository_for_owner
 
+
 owner = ariadne_load_local_graphql(__file__, "owner.graphql")
 owner_bindable = ObjectType("Owner")
 
+
 @owner_bindable.field("repositories")
-def resolve_repositories(owner, info):
+def resolve_repositories(owner, info, first=None, after=None, last=None, before=None):
     actor = info.context['request'].user
-    return list_repository_for_owner(actor, owner)
+    queryset = list_repository_for_owner(actor, owner)
+    paginator = CursorPaginator(queryset, ordering=('-repoid',))
+    page = paginator.page(first=first, after=after, last=last, before=before)
+    return {
+        "edges": [
+            {
+                "cursor": paginator.cursor(page[pos]),
+                "node": repository,
+            } for pos, repository in enumerate(page)
+        ],
+        "totalCount": queryset.count(),
+        "pageInfo": {
+            "hasNextPage": page.has_next,
+            "hasPreviousPage": page.has_previous,
+            "startCursor": paginator.cursor(page[0]) if len(page) > 0 else None,
+            "endCursor": paginator.cursor(page[-1]) if len(page) > 0 else None,
+        }
+    }
