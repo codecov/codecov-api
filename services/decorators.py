3f376bff02c5bd8a412b7c4126c762a20abe35f5
Pierce McEntagart <pierce@codecov.io>
2021-03-17 16:46:02 -0400

    Handle stripe errors in account viewset

--- services/decorators.py
+++ services/decorators.py
@@ -1,5 +1,6 @@
 from shared.torngit.exceptions import TorngitClientError
 from rest_framework.exceptions import APIException
+import stripe
 
 
 def torngit_safe(method):
@@ -15,5 +16,20 @@ def torngit_safe(method):
             exception.status_code = e.code
             raise exception
     # This is needed to decorate custom DRF viewset actions
+    exec_method.__name__ = method.__name__
+    return exec_method
+
+def stripe_safe(method):
+    """
+    Translates stripe-api errors into DRF APIExceptions.
+    """
+    def exec_method(*args, **kwargs):
+        try:
+            return method(*args, **kwargs)
+        except stripe.error.StripeError as e:
+            exception = APIException(detail=e.user_message)
+            exception.status_code = e.http_status
+            raise exception
+
     exec_method.__name__ = method.__name__
     return exec_method
