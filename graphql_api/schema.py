ede3a45ab387b0e71c724fd9545df6045f4c47b5
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-23 17:20:46 -0600

    use default snake case resolver to expose avatar url

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,6 +1,6 @@
-from ariadne import QueryType, gql, make_executable_schema
+from ariadne import QueryType, gql, make_executable_schema, snake_case_fallback_resolvers
 from ariadne.asgi import GraphQL
 
 from .types import types, bindables
 
-schema = make_executable_schema(types, *bindables)
+schema = make_executable_schema(types, *bindables, snake_case_fallback_resolvers)
