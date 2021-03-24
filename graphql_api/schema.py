12659ae672fcdff2cf83bb41303dd3f13d57110d
Dorian Amouroux <dor.amouroux@gmail.com>
2021-03-24 16:58:07 -0600

    add a comment to explain the snake case resolver

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -3,4 +3,7 @@ from ariadne.asgi import GraphQL
 
 from .types import types, bindables
 
+# snake_case_fallbck_resolvers gives use a default resolver which convert automatically
+# the field name from camelCase to snake_case and try to get it from the object
+# see https://ariadnegraphql.org/docs/resolvers#fallback-resolvers
 schema = make_executable_schema(types, *bindables, snake_case_fallback_resolvers)
