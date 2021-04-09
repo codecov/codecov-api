f900fc0fd5953e7041d851a7eb07b2f94b2c6406
Dorian Amouroux <dor.amouroux@gmail.com>
2021-04-09 17:46:18 -0500

    enable repo filtering by active

--- graphql_api/actions/repository.py
+++ graphql_api/actions/repository.py
@@ -1,6 +1,5 @@
 from core.models import Repository
 
-
 def list_repository_for_owner(current_user, owner):
     queryset = Repository.objects.viewable_repos(current_user).filter(author=owner)
     return queryset
@@ -9,7 +8,11 @@ def list_repository_for_owner(current_user, owner):
 def search_repos(current_user, filters={}):
     filters = filters if filters else {}
     term = filters.get("term")
-    queryset = Repository.objects.viewable_repos(current_user)
+    active = filters.get("active")
+    authors_from = [current_user.ownerid] + (current_user.organizations or [])
+    queryset = Repository.objects.filter(author__ownerid__in=authors_from)
     if term:
         queryset = queryset.filter(name__contains=term)
+    if active:
+        queryset = queryset.filter(active=active)
     return queryset
