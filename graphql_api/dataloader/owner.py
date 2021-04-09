b8bee105c7348442733928b82a0f894c9b276d8b
Dorian Amouroux <dor.amouroux@gmail.com>
2021-04-09 13:10:29 -0500

    add some docs on dataloader

--- graphql_api/dataloader/owner.py
+++ graphql_api/dataloader/owner.py
@@ -7,6 +7,8 @@ class OwnerLoader(DataLoader):
 
     @sync_to_async
     def batch_load_fn(self, ids):
+        # Need to return a list of owners in the same order as the ids
+        # So fetching in bulk and generate a list based on ids
         owners = Owner.objects.in_bulk(ids, field_name='ownerid')
         return [owners.get(id) for id in ids]
 
@@ -15,6 +17,7 @@ CONTEXT_KEY = "__owner_loader"
 
 def load_owner_by_id(info, id):
     if CONTEXT_KEY not in info.context:
+        # One loader per HTTP request that we init when we need it
         info.context[CONTEXT_KEY] = OwnerLoader()
 
     return info.context[CONTEXT_KEY].load(id)
