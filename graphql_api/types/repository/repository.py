7879d1f7f7e904a520676b0fa146d3f7a4380267
Dorian Amouroux <dor.amouroux@gmail.com>
2021-04-09 17:47:33 -0500

    expose coverage on the repository

--- graphql_api/types/repository/repository.py
+++ graphql_api/types/repository/repository.py
@@ -1,3 +1,11 @@
 from ariadne import ObjectType
 
 repository_bindable = ObjectType("Repository")
+
+@repository_bindable.field("coverage")
+def resolve_coverage(repo, info):
+    try:
+        return repo.cache['commit']['totals']['c']
+    except:
+        # No coverage in the cache
+        return None
