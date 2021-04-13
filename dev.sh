05a15da254e6c024782f4fb1da33ad2513ead79c
trent.schmidt <sch43228@gmail.com>
2021-04-13 11:28:09 -0500

    Adding dd-trace to dev

--- dev.sh
+++ dev.sh
@@ -4,4 +4,4 @@
 # NEVER run production with the --reload option command
 echo "Starting gunicorn in dev mode"
 export PYTHONWARNINGS=always
-gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-'
+ddtrace-run gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
