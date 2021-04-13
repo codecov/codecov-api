1a21faa2529c80adc13b8811910a2c1706d54dec
Dorian Amouroux <dor.amouroux@gmail.com>
2021-04-13 15:54:36 +0200

    install opentracing extension

--- graphql_api/urls.py
+++ graphql_api/urls.py
@@ -5,8 +5,20 @@ from django.views.decorators.csrf import csrf_exempt
 from .schema import schema
 from .views import AriadneView
 
+from ariadne.contrib.tracing.opentracing import OpenTracingExtensionSync
+
+from ddtrace.opentracer import Tracer, set_global_tracer
+
+tracer = Tracer("GraphQL")
+set_global_tracer(tracer)
+
+
 urlpatterns = [
     path(
-        "<str:service>", csrf_exempt(AriadneView.as_view(schema=schema)), name="graphql"
+        "<str:service>",
+        csrf_exempt(
+            AriadneView.as_view(schema=schema, extensions=[OpenTracingExtensionSync])
+        ),
+        name="graphql",
     ),
 ]
