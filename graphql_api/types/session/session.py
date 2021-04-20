4405d9cd2366b9bd5448c6a18f2c162141f245f5
Dorian Amouroux <dor.amouroux@gmail.com>
2021-04-20 12:18:35 +0200

    expose the last 4 digits of a token

--- graphql_api/types/session/session.py
+++ graphql_api/types/session/session.py
@@ -1,3 +1,8 @@
 from ariadne import ObjectType
 
 session_bindable = ObjectType("Session")
+
+
+@session_bindable.field("lastFour")
+def resolve_last_four(session, _):
+    return str(session.token)[-4:]
