5c7cc68357dc0c7ca6b62bb0afc624838b89697d
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-06 14:21:47 +0200

    add mutation to trigger a sync

--- graphql_api/actions/sync.py
+++ graphql_api/actions/sync.py
@@ -5,3 +5,15 @@ from services.task import TaskService
 
 def is_syncing(current_user):
     return TaskService().is_refreshing(current_user.ownerid)
+
+
+@sync_to_async
+def trigger_sync(current_user):
+    if not current_user.is_authenticated:
+        return {"error": "unauthenticated"}
+    TaskService().refresh(
+        current_user.ownerid,
+        current_user.username,
+        using_integration=bool(current_user.integration_id),
+    )
+    return {}
--- graphql_api/types/mutation/mutation.py
+++ graphql_api/types/mutation/mutation.py
@@ -1,10 +1,12 @@
 from ariadne import MutationType
 
 from .create_api_token import resolve_create_api_token
+from .sync_with_git_provider import resolve_sync_with_git_provider
 from .delete_session import resolve_delete_session
 
 mutation_bindable = MutationType()
 
 # Here, bind the resolvers from each subfolder to the Mutation type
 mutation_bindable.field("createApiToken")(resolve_create_api_token)
+mutation_bindable.field("syncWithGitProvider")(resolve_sync_with_git_provider)
 mutation_bindable.field("deleteSession")(resolve_delete_session)
