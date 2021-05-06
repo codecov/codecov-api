ccda43f058e2b99c725c95582a6523d20b788af6
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-06 14:55:49 +0200

    make the resolve_is_syncing_with_git_provider resolver async

--- graphql_api/actions/sync.py
+++ graphql_api/actions/sync.py
@@ -3,6 +3,7 @@ from asgiref.sync import sync_to_async
 from services.task import TaskService
 
 
+@sync_to_async
 def is_syncing(current_user):
     return TaskService().is_refreshing(current_user.ownerid)
 
