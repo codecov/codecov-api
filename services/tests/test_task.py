53ae907541157e48b003bf480ad0e313fc54d7da
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-06 13:30:22 +0200

    add specs for test task refresh

---
+++ services/tests/test_task.py
@@ -0,0 +1,12 @@
+from services.task import TaskService
+
+from services.redis_configuration import get_redis_connection
+
+
+def test_schedule_refresh_task():
+    ownerid = 5
+    username = "codecov"
+    task_service = TaskService(queue="testing")
+    assert task_service.is_refreshing(ownerid) == False
+    task_service.refresh(ownerid, username)
+    assert task_service.is_refreshing(ownerid) == True
