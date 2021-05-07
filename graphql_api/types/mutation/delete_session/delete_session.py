d615721682bdce09da5f40446faa7a25ceb4b6ab
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-07 16:38:11 +0200

    remove debug

--- graphql_api/types/mutation/delete_session/delete_session.py
+++ graphql_api/types/mutation/delete_session/delete_session.py
@@ -4,9 +4,5 @@ from graphql_api.helpers.mutation import wrap_error_handling_mutation
 @wrap_error_handling_mutation
 async def resolve_delete_session(_, info, input):
     command = info.context["executor"].get_command("owner")
-    print(input.get("sessionid"))
-    print(input.get("sessionid"))
-    print(input.get("sessionid"))
-    print(input.get("sessionid"))
     await command.delete_session(input.get("sessionid"))
     return None
