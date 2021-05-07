f647c1522dd1c3ff7ef85cc32f5b8cbe11429d58
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-07 15:45:27 +0200

    create the first command and interactor for owner

---
+++ graphql_api/commands/owner_interactors/create_api_token.py
@@ -0,0 +1,19 @@
+from asgiref.sync import sync_to_async
+
+from codecov_auth.models import Session
+from graphql_api.commands.base import BaseInteractor
+
+
+class CreateApiTokenInteractor(BaseInteractor):
+    def validate(self):
+        if not self.current_user.is_authenticated:
+            raise Exception("unauthenticated")
+
+    def create_token(self, name):
+        type = Session.SessionType.API
+        return Session.objects.create(name=name, owner=self.current_user, type=type)
+
+    @sync_to_async
+    def execute(self, name):
+        self.validate()
+        return self.create_token(name)
