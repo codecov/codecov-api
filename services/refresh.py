7a4f611d3b8e70207bf2f18384836315776b1eef
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-25 16:22:41 +0200

    init redis in construction in refresh service

--- services/refresh.py
+++ services/refresh.py
@@ -9,17 +9,17 @@ from services.task import TaskService
 class RefreshService(object):
     def __init__(self):
         self.task_service = TaskService()
+        self.redis = get_redis_connection()
 
     def is_refreshing(self, ownerid):
-        redis = get_redis_connection()
-        data_task = redis.hget("refresh", ownerid)
+        data_task = self.redis.hget("refresh", ownerid)
         if not data_task:
             return False
         with suppress(ValueError, TypeError):
             result = result_from_tuple(loads(data_task))
             if not result.ready():
                 return True
-        redis.hdel("refresh", ownerid)
+        self.redis.hdel("refresh", ownerid)
         return False
 
     def trigger_refresh(
@@ -36,5 +36,4 @@ class RefreshService(object):
             ownerid, username, sync_repos, sync_teams, using_integration
         )
         # store in redis the task data to be used for `is_refreshing` logic
-        redis = get_redis_connection()
-        redis.hset("refresh", ownerid, dumps(resp.as_tuple()))
+        self.redis.hset("refresh", ownerid, dumps(resp.as_tuple()))
