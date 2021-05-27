1c0ba496ca28d3b1637d450ee7b32c470e2ef26a
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-27 18:44:37 +0200

    add unit tests for the owner interactors

---
+++ graphql_api/commands/tests/test_owner.py
@@ -0,0 +1,23 @@
+from django.test import TransactionTestCase
+from unittest.mock import patch
+
+from codecov_auth.tests.factories import OwnerFactory
+from graphql_api.commands.owner import OwnerCommands
+
+
+class CreateApiTokenInteractorTest(TransactionTestCase):
+    def setUp(self):
+        self.user = OwnerFactory(username="codecov-user")
+        self.command = OwnerCommands(self.user)
+
+    @patch("graphql_api.commands.owner.CreateApiTokenInteractor.execute")
+    def test_create_api_token_delegate_to_interactor(self, interactor_mock):
+        name = "new api token"
+        self.command.create_api_token(name)
+        interactor_mock.assert_called_once_with(name)
+
+    @patch("graphql_api.commands.owner.DeleteSessionInteractor.execute")
+    def test_create_api_token_delegate_to_interactor(self, interactor_mock):
+        sessionid = 12
+        self.command.delete_session(sessionid)
+        interactor_mock.assert_called_once_with(sessionid)
