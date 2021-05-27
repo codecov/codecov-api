624ed8af518df491b7ece5e01905db9ec9f5ceac
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-27 19:18:10 +0200

    remove catch all in mutation helper

--- graphql_api/helpers/mutation.py
+++ graphql_api/helpers/mutation.py
@@ -7,7 +7,5 @@ def wrap_error_handling_mutation(resolver):
             return await resolver(*args, **kwargs)
         except Unauthenticated as e:
             return {"error": "unauthenticated"}
-        except Exception as e:
-            return {"error": str(e)}
 
     return resolver_with_error_handling
