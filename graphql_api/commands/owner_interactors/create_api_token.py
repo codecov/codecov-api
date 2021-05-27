901db0877d43980b480c9cc00d0c82a3d8f38826
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-27 16:27:59 +0200

    proper exception system in commands

--- graphql_api/commands/owner_interactors/create_api_token.py
+++ graphql_api/commands/owner_interactors/create_api_token.py
@@ -2,12 +2,13 @@ from asgiref.sync import sync_to_async
 
 from codecov_auth.models import Session
 from graphql_api.commands.base import BaseInteractor
+from graphql_api.commands.exceptions import Unauthenticated
 
 
 class CreateApiTokenInteractor(BaseInteractor):
     def validate(self):
         if not self.current_user.is_authenticated:
-            raise Exception("unauthenticated")
+            raise Unauthenticated()
 
     def create_token(self, name):
         type = Session.SessionType.API
--- graphql_api/helpers/mutation.py
+++ graphql_api/helpers/mutation.py
@@ -1,7 +1,12 @@
+from graphql_api.commands.exceptions import Unauthenticated
+
+
 def wrap_error_handling_mutation(resolver):
     async def resolver_with_error_handling(*args, **kwargs):
         try:
             return await resolver(*args, **kwargs)
+        except Unauthenticated as e:
+            return {"error": "unauthenticated"}
         except Exception as e:
             return {"error": str(e)}
 
