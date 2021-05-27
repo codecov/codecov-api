3bace3357f14658d4d79a2b8033856908c73b0d3
Dorian Amouroux <dor.amouroux@gmail.com>
2021-05-27 19:44:28 +0200

    fix naming test

--- graphql_api/commands/owner/tests/test_owner.py
+++ graphql_api/commands/owner/tests/test_owner.py
@@ -5,7 +5,7 @@ from codecov_auth.tests.factories import OwnerFactory
 from ..owner import OwnerCommands
 
 
-class CreateApiTokenInteractorTest(TransactionTestCase):
+class OwnerCommandsTest(TransactionTestCase):
     def setUp(self):
         self.user = OwnerFactory(username="codecov-user")
         self.command = OwnerCommands(self.user)
@@ -17,7 +17,7 @@ class CreateApiTokenInteractorTest(TransactionTestCase):
         interactor_mock.assert_called_once_with(name)
 
     @patch("graphql_api.commands.owner.owner.DeleteSessionInteractor.execute")
-    def test_create_api_token_delegate_to_interactor(self, interactor_mock):
+    def test_delete_session_delegate_to_interactor(self, interactor_mock):
         sessionid = 12
         self.command.delete_session(sessionid)
         interactor_mock.assert_called_once_with(sessionid)
