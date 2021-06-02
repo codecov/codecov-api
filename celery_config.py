4f0657443f0e7b9ce0e85a7bbba0a352bb62874e
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-02 15:08:03 +0200

    add default config for result backend celery

--- celery_config.py
+++ celery_config.py
@@ -4,8 +4,13 @@ from utils.config import get_config
 broker_url = get_config("services", "celery_broker") or get_config(
     "services", "redis_url"
 )
-result_backend = get_config("services", "redis_url")
+
+result_backend = get_config("services", "celery_broker") or get_config(
+    "services", "redis_url"
+)
 
 task_default_queue = get_config(
     "setup", "tasks", "celery", "default_queue", default="celery"
 )
+
+result_extended = True
