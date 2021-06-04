0e73db6c8eb84f8a5f996b80887350eec5316421
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-04 16:54:50 +0200

    implement interactor to fetch the repository

--- graphql_api/commands/repository/repository.py
+++ graphql_api/commands/repository/repository.py
@@ -1,6 +1,8 @@
 from ..base import BaseCommand
 
+from .interactors.fetch_repository import FetchRepositoryInteractor
+
 
 class RepositoryCommands(BaseCommand):
-    async def fetch_repository(self, name):
-        return None
+    def fetch_repository(self, owner, name):
+        return self.get_interactor(FetchRepositoryInteractor).execute(owner, name)
