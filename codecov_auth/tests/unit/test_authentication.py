89cdfb84c5c112ba05e4dbda044aef2e3679e0df
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-04 14:57:27 -0400

    fix tests

--- codecov_auth/tests/unit/test_authentication.py
+++ codecov_auth/tests/unit/test_authentication.py
@@ -22,12 +22,13 @@ from codecov_auth.authentication import (
 
 
 class TestAuthentication(BaseTestCase):
-    def test_auth(self, db):
-        a = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
+    def test_auth_asd(self, db):
+        github_token = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
         session = SessionFactory.create(token="8f9bc6cb-fd14-43bc-bbb5-be1e7c948f34")
         request_factory = APIRequestFactory()
+        request_factory.cookies["github-token"] = github_token
         request = request_factory.post(
-            "/notes/", {"title": "new idea"}, HTTP_AUTHORIZATION=f"frontend {a}"
+            "/notes/", {"title": "new idea"}, HTTP_TOKEN_TYPE="github-token"
         )
         authenticator = CodecovTokenAuthentication()
         result = authenticator.authenticate(request)
@@ -44,7 +45,6 @@ class TestAuthentication(BaseTestCase):
 
     def test_decode_token_bad_signature(self):
         val = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|aaaaaaaa7baad2e220faaae02c07c377aaaabca32ad0c2b8baab2aa8cfbe3aaa"
-        expected_response = "8f9bc6cb-fd14-43bc-bbb5-be1e7c948f34"
         authenticator = CodecovTokenAuthentication()
         with pytest.raises(rest_framework.exceptions.AuthenticationFailed):
             authenticator.decode_token_from_cookie(val)
@@ -59,8 +59,9 @@ class TestAuthentication(BaseTestCase):
         SessionFactory.create()
         token = uuid4()
         request_factory = APIRequestFactory()
+        request_factory.cookies["github-token"] = token
         request = request_factory.post(
-            "/notes/", {"title": "new idea"}, HTTP_AUTHORIZATION=f"frontend {token}"
+            "/notes/", {"title": "new idea"}, HTTP_TOKEN_TYPE="github-token"
         )
         authenticator = CodecovTokenAuthentication()
         with pytest.raises(rest_framework.exceptions.AuthenticationFailed):
@@ -94,11 +95,12 @@ class TestAuthentication(BaseTestCase):
         assert session.useragent == new_user_agent
 
     def test_authenticate_updates_session(self, db, mocker):
-        a = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
+        github_token = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
         session = SessionFactory.create(token="8f9bc6cb-fd14-43bc-bbb5-be1e7c948f34")
         request_factory = APIRequestFactory()
+        request_factory.cookies["github-token"] = github_token
         request = request_factory.post(
-            "/notes/", {"title": "new idea"}, HTTP_AUTHORIZATION=f"frontend {a}"
+            "/notes/", {"title": "new idea"}, HTTP_TOKEN_TYPE="github-token"
         )
         mocked_verify_session = mocker.patch(
             "codecov_auth.authentication.CodecovTokenAuthentication.update_session"
@@ -111,12 +113,11 @@ class TestAuthentication(BaseTestCase):
 
 class CodecovAuthMixinImpersonationTests(TestCase):
     def setUp(self):
-        token = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
+        self.token = "2|1:0|10:1557329312|15:bitbucket-token|48:OGY5YmM2Y2ItZmQxNC00M2JjLWJiYjUtYmUxZTdjOTQ4ZjM0|459669157b19d2e220f461e02c07c377a455bc532ad0c2b8b69b2648cfbe3914"
         self.session = SessionFactory(
             token="8f9bc6cb-fd14-43bc-bbb5-be1e7c948f34", owner=OwnerFactory(staff=True)
         )
 
-        self.authorization_header = f"frontend {token}"
         self.user_to_impersonate = "codecov"
         self.impersonated_user = OwnerFactory(username=self.user_to_impersonate)
         self.authenticator = CodecovTokenAuthentication()
@@ -124,9 +125,8 @@ class CodecovAuthMixinImpersonationTests(TestCase):
 
     def _create_request(self, cookie="", service=""):
         self.request_factory.cookies["staff_user"] = cookie
-        request = Request(
-            self.request_factory.get("", HTTP_AUTHORIZATION=self.authorization_header)
-        )
+        self.request_factory.cookies["github-token"] = self.token
+        request = Request(self.request_factory.get("", HTTP_TOKEN_TYPE="github-token"))
         request.parser_context = {
             "kwargs": {"service": service or self.impersonated_user.service}
         }
