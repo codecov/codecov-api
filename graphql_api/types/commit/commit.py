7b66e6ebed748142e66a0bcecb03dfbd63c366d1
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-09 15:20:07 +0200

    expose some extra fields on the commit type on graphql

--- graphql_api/types/commit/commit.py
+++ graphql_api/types/commit/commit.py
@@ -4,3 +4,11 @@ from graphql_api.dataloader.owner import load_owner_by_id
 
 
 commit_bindable = ObjectType("Commit")
+
+commit_bindable.set_alias("createdAt", "timestamp")
+
+
+@commit_bindable.field("author")
+def resolve_author(commit, info):
+    if commit.author_id:
+        return load_owner_by_id(info, commit.author_id)
