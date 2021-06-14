fb08cf84366ab520bb8087b2569c138fc7a9c5be
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-14 14:42:17 +0200

    expose commit parent and commit totals

--- graphql_api/types/commit/commit.py
+++ graphql_api/types/commit/commit.py
@@ -1,4 +1,5 @@
 from ariadne import ObjectType
+from asgiref.sync import sync_to_async
 
 from graphql_api.dataloader.owner import load_owner_by_id
 
@@ -12,3 +13,9 @@ commit_bindable.set_alias("createdAt", "timestamp")
 def resolve_author(commit, info):
     if commit.author_id:
         return load_owner_by_id(info, commit.author_id)
+
+
+@commit_bindable.field("parent")
+@sync_to_async
+def resolve_parent(commit, info):
+    return commit.parent_commit
