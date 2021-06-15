8cdd9cf6c8b560cd7b89675ea1b48f67e290fe58
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-15 12:46:05 -0400

    Allow for collecting static files for Django

--- codecov/settings_base.py
+++ codecov/settings_base.py
@@ -146,7 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 LOGGING = {
     "version": 1,
