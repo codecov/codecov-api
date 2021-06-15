744d0775f8813d1b03551b804ab6ece2974d62fe
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-15 12:13:30 +0200

    expose the final yaml on the graph

--- graphql_api/types/commit/commit.py
+++ graphql_api/types/commit/commit.py
@@ -1,3 +1,4 @@
+import yaml
 from ariadne import ObjectType
 from asgiref.sync import sync_to_async
 
@@ -16,8 +17,13 @@ def resolve_author(commit, info):
     if commit.author_id:
         return load_owner_by_id(info, commit.author_id)
 
-
 @commit_bindable.field("parent")
 def resolve_parent(commit, info):
     command = info.context["executor"].get_command("commit")
     return command.fetch_commit(commit.repository, commit.parent_commit_id)
+
+@commit_bindable.field("yaml")
+async def resolve_yaml(commit, info):
+    command = info.context["executor"].get_command("commit")
+    final_yaml = await command.get_final_yaml(commit)
+    return yaml.dump(final_yaml)
