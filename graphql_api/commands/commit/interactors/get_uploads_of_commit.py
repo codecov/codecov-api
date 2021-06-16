4a251c9927eb0bc3d86d393e9b18cf2e8f3f65c1
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-16 18:44:39 +0200

    parse the list of uploads from the interactor

--- graphql_api/commands/commit/interactors/get_uploads_of_commit.py
+++ graphql_api/commands/commit/interactors/get_uploads_of_commit.py
@@ -1,6 +1,9 @@
 from graphql_api.commands.base import BaseInteractor
 
+from shared.utils.sessions import Session
+
 
 class GetUploadsOfCommitInteractor(BaseInteractor):
     def execute(self, commit):
-        return [1, 2, 3]
+        uploads_raw = commit.report.get("sessions", {}).values()
+        return [Session.parse_session(**upload) for upload in uploads_raw]
