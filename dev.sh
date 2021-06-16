459ddde38c581016ff28800b885269d7898e7899
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-16 14:38:05 -0400

    collecstatic in dockerfile

--- dev.sh
+++ dev.sh
@@ -4,7 +4,6 @@
 # NEVER run production with the --reload option command
 echo "Starting gunicorn in dev mode"
 export PYTHONWARNINGS=always
-python manage.py collectstatic --no-input
 if [[ "$STATSD_HOST" ]]; then
 ddtrace-run gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 else
