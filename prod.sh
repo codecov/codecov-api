40939a214ae17b8f3f9b5e2c6b2abf2f207f321c
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-16 14:24:09 -0400

    collect static files in prod

--- prod.sh
+++ prod.sh
@@ -3,7 +3,9 @@
 # Starts the production gunicorn server (no --reload)
 echo "Starting gunicorn in production mode"
 if [ -f "/usr/local/bin/berglas" ]; then
+  berglas exec -- python manage.py collectstatic --no-input
   berglas exec -- ddtrace-run gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 else
+  python manage.py collectstatic --no-input
   ddtrace-run gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
 fi
