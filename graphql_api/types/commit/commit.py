e74cc7ccb18e88542aad2b3b92f24cb474b0317b
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-17 12:40:51 +0200

    expose the list of uploads as a connection on the commit object

--- graphql_api/types/commit/commit.py
+++ graphql_api/types/commit/commit.py
@@ -3,7 +3,8 @@ from ariadne import ObjectType
 from asgiref.sync import sync_to_async
 
 from graphql_api.dataloader.owner import load_owner_by_id
-
+from graphql_api.helpers.connection import queryset_to_connection
+from graphql_api.types.enums import OrderingDirection
 
 commit_bindable = ObjectType("Commit")
 
@@ -17,10 +18,12 @@ def resolve_author(commit, info):
     if commit.author_id:
         return load_owner_by_id(info, commit.author_id)
 
+
 @commit_bindable.field("parent")
 def resolve_parent(commit, info):
     command = info.context["executor"].get_command("commit")
     return command.fetch_commit(commit.repository, commit.parent_commit_id)
+
 
 @commit_bindable.field("yaml")
 async def resolve_yaml(commit, info):
@@ -28,9 +31,11 @@ async def resolve_yaml(commit, info):
     final_yaml = await command.get_final_yaml(commit)
     return yaml.dump(final_yaml)
 
-@commit_bindable.field("listUploads")
-def resolve_list_uploads(commit, info):
+
+@commit_bindable.field("uploads")
+async def resolve_list_uploads(commit, info, **kwargs):
     command = info.context["executor"].get_command("commit")
-    uploads = command.get_uploads_of_commit(commit)
-    upload_with_commits = [{"upload": upload, "commit": commit} for upload in uploads]
-    return upload_with_commits
+    queryset = await command.get_uploads_of_commit(commit)
+    return await queryset_to_connection(
+        queryset, ordering="id", ordering_direction=OrderingDirection.ASC, **kwargs
+    )
