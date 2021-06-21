de8d089d3cdef6ef9ad3cec7cff90b3d8d0ee657
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-21 11:01:52 +0200

    clean up hard coded raised exception

--- graphql_api/helpers/mutation.py
+++ graphql_api/helpers/mutation.py
@@ -11,29 +11,8 @@ def wrap_error_handling_mutation(resolver):
     async def resolver_with_error_handling(*args, **kwargs):
         try:
             return await resolver(*args, **kwargs)
-        except Unauthenticated as e:
-            return {"error": "unauthenticated"}
-        except Unauthorized as e:
-            return {"error": "unauthorized"}
-        except ValidationError as e:
-            return {"error": str(e)}
-        except NotFound as e:
-            return {"error": "not found"}
-
-    return resolver_with_error_handling
-
-
-def new_wrap_error_handling_mutation(resolver):
-    async def resolver_with_error_handling(*args, **kwargs):
-        try:
-            return await resolver(*args, **kwargs)
         except Exception as e:
-            print("--------------")
-            print(type(e))
-            print(issubclass(type(e), BaseException))
-            print("--------------")
             if issubclass(type(e), BaseException):
-                print("so it wont raise")
                 return {"error": e}
             raise e
 
--- graphql_api/types/mutation/create_api_token/create_api_token.py
+++ graphql_api/types/mutation/create_api_token/create_api_token.py
@@ -5,19 +5,8 @@ from graphql_api.helpers.mutation import (
 )
 
 
-# @wrap_error_handling_mutation
-# async def resolve_create_api_token(_, info, input):
-#     command = info.context["executor"].get_command("owner")
-#     session = await command.create_api_token(input.get("name"))
-#     return {"session": session, "full_token": session.token}
-#
-
-
-@new_wrap_error_handling_mutation
+@wrap_error_handling_mutation
 async def resolve_create_api_token(_, info, input):
-    from graphql_api.commands.exceptions import Unauthenticated
-
-    return {"error": Unauthenticated()}
     command = info.context["executor"].get_command("owner")
     session = await command.create_api_token(input.get("name"))
     return {"session": session, "full_token": session.token}
