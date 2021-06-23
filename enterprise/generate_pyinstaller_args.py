9c19b2e4dc4b69f030f2bb7b0eb24b214e6cb3a9
trent.schmidt <sch43228@gmail.com>
2021-06-23 16:19:40 -0500

    Adding runtime hook for pyinstaller

--- enterprise/generate_pyinstaller_args.py
+++ enterprise/generate_pyinstaller_args.py
@@ -115,6 +115,7 @@ def main():
             for x in sorted(hidden_imports, key=lambda x: (len(x.split(".")), x))
         ]
     )
+    args.extend([f"--runtime-hook=pyi_rth_django.py"])
     args.extend([f"--additional-hooks-dir /hooks"])
 
     print(" ".join(args))
---
+++ enterprise/pyi_rth_django.py
@@ -0,0 +1,32 @@
+import django.core.management
+import django.utils.autoreload
+
+
+def _get_commands():
+    # Django groupss commands by app.
+    # This returns static dict() as it is for django 1.8 and the default project.
+    commands = {
+        'runserver': 'django.core',
+        'migrate': 'legacy_migrations',
+    }
+    return commands
+
+
+_old_restart_with_reloader = django.utils.autoreload.restart_with_reloader
+
+
+def _restart_with_reloader(*args):
+    import sys
+    a0 = sys.argv.pop(0)
+    try:
+        return _old_restart_with_reloader(*args)
+    finally:
+        sys.argv.insert(0, a0)
+
+
+# Override get_commands() function otherwise the app will complain that
+# there are no commands.
+django.core.management.get_commands = _get_commands
+# Override restart_with_reloader() function otherwise the app might
+# complain that some commands do not exist. e.g. runserver.
+django.utils.autoreload.restart_with_reloader = _restart_with_reloader
