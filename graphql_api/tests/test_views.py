820f38f22122909e8c1581af2d92ca238c0d5e22
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-30 17:07:09 +0200

    add specs for error formatter

---
+++ graphql_api/tests/test_views.py
@@ -0,0 +1,65 @@
+import json
+from ariadne import ObjectType, make_executable_schema
+from django.test import TestCase, override_settings, RequestFactory
+from django.urls import ResolverMatch
+
+from graphql_api.commands.exceptions import Unauthorized
+from .helper import GraphQLTestHelper
+from ..views import AsyncGraphqlView
+
+
+def generate_schema_that_raise_with(exception):
+    types = """
+    type Query {
+        failing: Boolean
+    }
+    """
+    query_bindable = ObjectType("Query")
+
+    @query_bindable.field("failing")
+    def failing_bindable(*_):
+        raise exception
+
+    return make_executable_schema(types, query_bindable)
+
+
+class ArianeViewTestCase(GraphQLTestHelper, TestCase):
+    async def do_query(self, schema):
+        view = AsyncGraphqlView.as_view(schema=schema)
+        request = RequestFactory().post(
+            "/graphql/gh", {"query": "{ failing }"}, content_type="application/json"
+        )
+        match = ResolverMatch(func=lambda: None, args=(), kwargs={"service": "github"})
+
+        request.resolver_match = match
+        request.user = None
+        res = await view(request, service="gh")
+        return json.loads(res.content)
+
+    @override_settings(DEBUG=True)
+    async def test_when_debug_is_true(self):
+        schema = generate_schema_that_raise_with(Exception("hello"))
+        data = await self.do_query(schema)
+        print(data)
+        assert data["errors"] is not None
+        assert data["errors"][0]["message"] == "hello"
+        assert data["errors"][0]["extensions"] is not None
+
+    @override_settings(DEBUG=False)
+    async def test_when_debug_is_false_and_random_exception(self):
+        schema = generate_schema_that_raise_with(Exception("hello"))
+        data = await self.do_query(schema)
+        print(data)
+        assert data["errors"] is not None
+        assert data["errors"][0]["message"] == "INTERNAL SERVER ERROR"
+        assert data["errors"][0].get("extensions") is None
+
+    @override_settings(DEBUG=False)
+    async def test_when_debug_is_false_and_exception_we_know(self):
+        schema = generate_schema_that_raise_with(Unauthorized())
+        data = await self.do_query(schema)
+        print(data)
+        assert data["errors"] is not None
+        assert data["errors"][0]["message"] == "You are not authorized"
+        assert data["errors"][0]["type"] == "Unauthorized"
+        assert data["errors"][0].get("extensions") is None
