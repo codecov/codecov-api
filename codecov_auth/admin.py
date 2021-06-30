0c7fdeb63cdc3d197f16320f0cb9e72bca7ddd17
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-30 12:48:41 -0400

    Add ability to impersonate an Owner to Django Admin

--- codecov_auth/admin.py
+++ codecov_auth/admin.py
@@ -1,6 +1,32 @@
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
+from django.shortcuts import redirect
 
 from codecov_auth.models import Owner
+from utils.services import get_short_service_name
+
+
+def impersonate_owner(self, request, queryset):
+    if queryset.count() != 1:
+        self.message_user(
+            request, "You must impersonate exactly one Owner.", level=messages.ERROR
+        )
+        return
+
+    owner = queryset.first()
+    response = redirect(
+        f"{settings.CODECOV_URL}/{get_short_service_name(owner.service)}/"
+    )
+    response.set_cookie(
+        "staff_user",
+        owner.username,
+        domain=settings.COOKIES_DOMAIN,
+        samesite="Strict",
+    )
+    return response
+
+
+impersonate_owner.short_description = "Impersonate the selected user"
 
 
 @admin.register(Owner)
@@ -9,6 +35,7 @@ class OwnerAdmin(admin.ModelAdmin):
     list_display = ("name", "username", "email", "service")
     readonly_fields = []
     search_fields = ("username__iexact",)
+    actions = [impersonate_owner]
 
     def get_readonly_fields(self, _, obj=None):
         fields = (
