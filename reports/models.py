0b23a803a0c71672674cd0d57934273261cbc87c
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-05 10:33:51 +0200

    move base codecov modal to the main folder

--- reports/models.py
+++ reports/models.py
@@ -3,18 +3,9 @@ import uuid
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+from codecov.models import BaseCodecovModel
 from utils.services import get_short_service_name
 from upload.constants import ci
-
-
-class BaseCodecovModel(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    external_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
 
 
 class AbstractTotals(BaseCodecovModel):
