8b4f9e5abaa486f4904df7055315e8b23ebb5c10
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-05 11:39:42 +0200

    add specs for compare admin

---
+++ compare/tests/test_admin.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from codecov_auth.tests.factories import OwnerFactory
+
+
+class CompareAdminTest(TestCase):
+    def setUp(self):
+        self.staff_user = OwnerFactory(staff=True)
+        self.client.force_login(user=self.staff_user)
+
+    def test_compare_admin_detail_page(self):
+        response = self.client.get(
+            reverse(f"admin:compare_commitcomparison_changelist")
+        )
+        self.assertEqual(response.status_code, 200)
