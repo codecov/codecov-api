74e291a4b96025f2e505bd19f7d1cff5e2aca321
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-05 11:46:56 +0200

    create comparison for admin tests

--- compare/tests/test_admin.py
+++ compare/tests/test_admin.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 from django.urls import reverse
 
 from codecov_auth.tests.factories import OwnerFactory
+from .factories import CommitComparisonFactory
 
 
 class CompareAdminTest(TestCase):
     def setUp(self):
+        # Create a couple of comparison so the list has something to display
+        CommitComparisonFactory()
+        CommitComparisonFactory()
         self.staff_user = OwnerFactory(staff=True)
         self.client.force_login(user=self.staff_user)
 
