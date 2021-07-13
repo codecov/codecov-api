e66809c7f7b9c427fe528d46378830f567fc5ad8
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-13 12:03:46 +0200

    add specs for the task service

--- services/tests/test_task.py
+++ services/tests/test_task.py
@@ -1,7 +1,19 @@
-from services.task import TaskService
+from services.task import TaskService, celery_app
+from shared import celery_config
 
 
 def test_refresh_task(mocker):
     chain_mock = mocker.patch("services.task.chain")
     TaskService().refresh(5, "codecov")
     chain_mock.assert_called()
+
+
+def test_compute_comparison_task(mocker):
+    signature_mock = mocker.patch("services.task.signature")
+    TaskService().compute_comparison(5)
+    signature_mock.assert_called_with(
+        celery_config.compute_comparison_task_name,
+        args=None,
+        kwargs=dict(comparison_id=5),
+        app=celery_app,
+    )
