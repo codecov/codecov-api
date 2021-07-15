312f26c718f63912368af48d564d3d8745f4b552
Thiago <44376991+ThiagoCodecov@users.noreply.github.com>
2021-07-15 13:15:29 -0300

    Revert "Download build handler migration (#670)"
    This reverts commit af150f832976d2603c2b319ee7f37c578a74f7dd.

--- upload/urls.py
+++ upload/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path
-from .views import UploadHandler, UploadDownloadHandler
+from django.urls import re_path
+from .views import UploadHandler
 
 
 urlpatterns = [
     # use regex to make trailing slash optional
-    path("upload/<str:version>/", UploadHandler.as_view(), name="upload-handler"),
-    path(
-        "upload/<str:service>/<str:owner_username>/<str:repo_name>/download",
-        UploadDownloadHandler.as_view(),
-        name="upload-download",
-    ),
+    re_path("^/?", UploadHandler.as_view(), name="upload-handler"),
 ]
