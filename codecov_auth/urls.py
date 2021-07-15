84137d5930ab9304b3ce61e5fc6458a223eec045
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-15 13:23:11 +0200

    make the authentication works with short names for the services

--- codecov_auth/urls.py
+++ codecov_auth/urls.py
@@ -4,7 +4,34 @@ from .views.gitlab import GitlabLoginView
 from .views.bitbucket import BitbucketLoginView
 
 urlpatterns = [
-    path("github", GithubLoginView.as_view(), name="github-login",),
-    path("gitlab", GitlabLoginView.as_view(), name="gitlab-login",),
-    path("bitbucket", BitbucketLoginView.as_view(), name="bitbucket-login",),
+    path(
+        "github",
+        GithubLoginView.as_view(),
+        name="github-login",
+    ),
+    path(
+        "gh",
+        GithubLoginView.as_view(),
+        name="gh-login",
+    ),
+    path(
+        "gitlab",
+        GitlabLoginView.as_view(),
+        name="gitlab-login",
+    ),
+    path(
+        "gl",
+        GitlabLoginView.as_view(),
+        name="gl-login",
+    ),
+    path(
+        "bitbucket",
+        BitbucketLoginView.as_view(),
+        name="bitbucket-login",
+    ),
+    path(
+        "bb",
+        BitbucketLoginView.as_view(),
+        name="bb-login",
+    ),
 ]
