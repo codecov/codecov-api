af150f832976d2603c2b319ee7f37c578a74f7dd
Felipe Ballesteros <felipe@codecov.io>
2021-07-15 17:51:39 +0200

    Download build handler migration (#670)
    * Initial commit
    
    * Added tests
    
    * added fetch_owner command
    
    * Use commands pattern
    
    * fix unauthorized repo error
    
    * mock root storage
    
    * mock read_file
    
    * translate to class viewm handler
    
    * fixed tests
    
    * hash verify comment
    
    * linting and improvements
    
    * migrate build app to upload app
    
    * use new build download handler url for report session modal
    
    * use new build download handler url for report session model
    
    * refactor download_url
    
    * changed build to upload
    
    * replaced build by upload on ReportSession
    
    * fixed test

--- codecov_auth/commands/owner/owner.py
+++ codecov_auth/commands/owner/owner.py
@@ -4,7 +4,7 @@ from codecov_auth.models import Owner, Session
 from .interactors.create_api_token import CreateApiTokenInteractor
 from .interactors.set_yaml_on_owner import SetYamlOnOwnerInteractor
 from .interactors.delete_session import DeleteSessionInteractor
-
+from .interactors.fetch_owner import FetchOwnerInteractor
 
 class OwnerCommands(BaseCommand):
     def create_api_token(self, name):
@@ -15,3 +15,6 @@ class OwnerCommands(BaseCommand):
 
     def set_yaml_on_owner(self, username, yaml):
         return self.get_interactor(SetYamlOnOwnerInteractor).execute(username, yaml)
+
+    def fetch_owner(self, username):
+        return self.get_interactor(FetchOwnerInteractor).execute(username)
--- upload/urls.py
+++ upload/urls.py
@@ -1,8 +1,13 @@
-from django.urls import re_path
-from .views import UploadHandler
+from django.urls import path
+from .views import UploadHandler, UploadDownloadHandler
 
 
 urlpatterns = [
     # use regex to make trailing slash optional
-    re_path("^/?", UploadHandler.as_view(), name="upload-handler"),
+    path("upload/<str:version>/", UploadHandler.as_view(), name="upload-handler"),
+    path(
+        "upload/<str:service>/<str:owner_username>/<str:repo_name>/download",
+        UploadDownloadHandler.as_view(),
+        name="upload-download",
+    ),
 ]
