9fdc0fd84be282eb917c3250e13885622a0b5118
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-20 12:56:54 +0200

    add spec for branch type in graphql

---
+++ graphql_api/tests/test_branch.py
@@ -0,0 +1,41 @@
+import yaml
+import datetime
+from unittest.mock import patch
+from django.test import TransactionTestCase
+from ariadne import graphql_sync
+
+from codecov_auth.tests.factories import OwnerFactory
+from core.tests.factories import RepositoryFactory, CommitFactory, BranchFactory
+from .helper import GraphQLTestHelper
+
+query_branch = """
+query FetchBranch($org: String!, $repo: String!, $branch: String!) {
+  owner(username: $org) {
+    repository(name: $repo) {
+      branch(name: $branch) {
+        %s
+      }
+    }
+  }
+}
+"""
+
+
+class TestCommit(GraphQLTestHelper, TransactionTestCase):
+    def setUp(self):
+        self.org = OwnerFactory(username="codecov")
+        self.repo = RepositoryFactory(author=self.org, name="gazebo", private=False)
+        self.head = CommitFactory(repository=self.repo)
+        self.branch = BranchFactory(repository=self.repo, head=self.head.commitid)
+
+    def test_fetch_branch(self):
+        query = query_branch % "name, head { commitid }"
+        variables = {
+            "org": self.org.username,
+            "repo": self.repo.name,
+            "branch": self.branch.name,
+        }
+        data = self.gql_request(query, variables=variables)
+        branch = data["owner"]["repository"]["branch"]
+        assert branch["name"] == self.branch.name
+        assert branch["head"]["commitid"] == self.head.commitid
