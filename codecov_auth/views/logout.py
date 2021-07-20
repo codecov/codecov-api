b3a3b453bee82bea90e8d800ac851a7bac90bf87
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-20 11:00:43 +0200

    logout from django constructs

--- codecov_auth/views/logout.py
+++ codecov_auth/views/logout.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import logout
 from django.shortcuts import redirect
 
 from utils.config import get_config
@@ -13,16 +14,15 @@ def delete_session(request, service):
     if encoded_cookie:
         secret = get_config("setup", "http", "cookie_secret")
         token = decode_token_from_cookie(secret, encoded_cookie)
-        print(Session.objects.filter(token=token).delete())
+        Session.objects.filter(token=token).delete()
 
 
 def logout_view(request, service):
     service_name = get_long_service_name(service)
     response = redirect("/")
-    # from django.http import HttpResponse
-    # response = HttpResponse()
     if request.user.is_authenticated:
         delete_session(request, service_name)
+        logout(request)
         kwargs_cookie = dict(domain=settings.COOKIES_DOMAIN, samesite="Strict")
         response.delete_cookie("staff_user", **kwargs_cookie)
         response.delete_cookie(f"{service_name}-username", **kwargs_cookie)
