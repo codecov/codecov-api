c6e5fa132b5cc242ae678b41d59b705678f5621e
Felipe Ballesteros <felipe@codecov.io>
2021-08-03 17:54:58 +0200

    Add partials to file coverage endpoint (#712)
    * Added partials
    
    * added partials test

--- core/commands/commit/interactors/get_file_coverage.py
+++ core/commands/commit/interactors/get_file_coverage.py
@@ -1,9 +1,20 @@
 from services.archive import ReportService
 from codecov.commands.base import BaseInteractor
 from asgiref.sync import sync_to_async
+from fractions import Fraction
+import math
 
 
 class GetFileCoverageInteractor(BaseInteractor):
+    def get_coverage(self, _coverage):
+        if _coverage == 1:
+            return 1
+        elif _coverage == 0:
+            return 0
+        elif type(_coverage) is str:
+            partial = math.ceil(float(Fraction(_coverage)))
+            return 0 if partial == 0 else 2
+
     @sync_to_async
     def get_file_coverage(self, commit, path):
         report_service = ReportService()
@@ -11,7 +22,10 @@ class GetFileCoverageInteractor(BaseInteractor):
 
         file_report = report.get(path)
         return [
-            {"line": line_report[0], "coverage": line_report[1].coverage}
+            {
+                "line": line_report[0],
+                "coverage": self.get_coverage(line_report[1].coverage),
+            }
             for line_report in file_report.lines
         ]
 
