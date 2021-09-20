c2fbcd09d42aa2bad53ad49c863a888f0a5689bd
trent-codecov <trent@codecov.io>
2021-09-20 11:41:17 -0500

    Rollback dev.sh and add staging.sh

--- staging.sh
+++ staging.sh
@@ -4,12 +4,16 @@
 # NEVER run production with the --reload option command
 echo "Starting gunicorn in dev mode"
 export PYTHONWARNINGS=always
+prefix=""
+if [ -f "/usr/local/bin/berglas" ]; then
+  prefix="berglas exec --"
+fi
 suffix=""
 if [[ "$STATSD_HOST" ]]; then
   suffix="--statsd-host ${STATSD_HOST}:${STATSD_PORT}"
 fi
 if [ $ELASTIC_APM_ENABLED ]; then
-  gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' $suffix
+  $prefix gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' $suffix
 else
-  ddtrace-run gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' $suffix
+  $prefix ddtrace-run gunicorn codecov.wsgi:application --reload --bind 0.0.0.0:8000 --access-logfile '-' $suffix
 fi
