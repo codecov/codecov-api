15cf923d8887933d4c1043436ee83e3bdc7be1d9
Adrian Viquez <adrian@codecov.io>
2021-09-30 14:02:44 -0700

    added repository test to cover pulls

--- graphql_api/tests/test_repository.py
+++ graphql_api/tests/test_repository.py
@@ -1,12 +1,10 @@
 from freezegun import freeze_time
-import datetime
 
 from django.test import TransactionTestCase
-from ariadne import graphql_sync
 
 from codecov_auth.tests.factories import OwnerFactory
-from core.tests.factories import RepositoryFactory
-from .helper import GraphQLTestHelper, paginate_connection
+from core.tests.factories import RepositoryFactory, PullFactory
+from .helper import GraphQLTestHelper
 
 query_repository = """{
     me {
@@ -28,11 +26,33 @@ query_repository = """{
 }
 """
 
+query_repository_with_pull = """{
+    me {
+        owner {
+            repository(name: "test-repo") {
+                name
+                pulls {
+                    edges {
+                        node {
+                            title
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
 
 class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
     def fetch_repository(self):
         data = self.gql_request(query_repository, user=self.user)
         return data["me"]["owner"]["repositories"]["edges"][0]["node"]
+
+    def fetch_repository_with_pulls(self):
+        data = self.gql_request(query_repository_with_pull, user=self.user)
+        return data["me"]["owner"]["repository"]["pulls"]["edges"][0]["node"]
 
     def setUp(self):
         self.user = OwnerFactory(username="codecov-user")
@@ -65,4 +85,11 @@ class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
             "private": True,
             "coverage": 75,
             "updatedAt": "2021-01-01T00:00:00+00:00",
+        }
+
+    def test_when_repository_has_pull_request(self):
+        repo = RepositoryFactory(author=self.user, active=True, private=True, name="test-repo")
+        PullFactory(pullid=10, repository_id=repo.repoid, title="test-pull-request")
+        assert self.fetch_repository_with_pulls() == {
+            "title": "test-pull-request"
         }
