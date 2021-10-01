b60e5d7d1d2e44c53b10d7443c25c2355d769798
Adrian Viquez <adrian@codecov.io>
2021-10-01 16:19:50 -0700

    got rid of line that considered the set non-existend (will always be existent cause of django foreign key relations)

--- core/commands/pull/interactors/fetch_pull_requests.py
+++ core/commands/pull/interactors/fetch_pull_requests.py
@@ -6,6 +6,4 @@ from core.models import Pull
 class FetchPullRequestsInteractor(BaseInteractor):
     @sync_to_async
     def execute(self, repository):
-        if not repository.pull_requests:
-            return Pull.objects.none()
         return repository.pull_requests.all()
