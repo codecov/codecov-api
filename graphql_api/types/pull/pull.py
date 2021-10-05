01cc37f15e7845c7219b4f73924d04b01a869a84
Adrian Viquez <adrian@codecov.io>
2021-10-05 10:23:30 -0700

    wip

--- graphql_api/types/pull/pull.py
+++ graphql_api/types/pull/pull.py
@@ -1,5 +1,18 @@
+from os import sync
 from ariadne import ObjectType
+from graphql_api.dataloader.owner import load_owner_by_id
+from asgiref.sync import sync_to_async
 
 pull_bindable = ObjectType("Pull")
 
 pull_bindable.set_alias("pullId", "pullid")
+
+@pull_bindable.field("author")
+def resolve_author(pull, info):
+    return load_owner_by_id(info, pull.author_id)
+
+@pull_bindable.field("head")
+@sync_to_async
+def resolve_head(pull, info):
+  from core.models import Commit
+  return Commit.objects.get(commitid=pull.head, repository_id=pull.repository_id)
