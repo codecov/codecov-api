0605419625540b2fa1a56e63c66ce9ad753f8030
Adrian Viquez <adrian@codecov.io>
2021-10-08 12:16:33 -0700

    split pull test into its own file

---
+++ graphql_api/tests/test_pull.py
@@ -0,0 +1,72 @@
+from freezegun import freeze_time
+
+from django.test import TransactionTestCase
+
+from core.models import Pull
+from codecov_auth.tests.factories import OwnerFactory
+from core.tests.factories import RepositoryFactory, PullFactory, CommitFactory
+from .helper import GraphQLTestHelper
+
+
+query_repository = """{
+    me {
+        owner {
+            repository(name: "test-repo-for-pull") {
+                name
+                pulls {
+                    edges {
+                        node {
+                            title
+                            state
+                            pullId
+                            updatestamp
+                            author {
+                                username
+                            }
+                            head {
+                                commitid
+                            }
+                            base {
+                                commitid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+
+class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
+    def fetch_repository(self):
+        data = self.gql_request(query_repository, user=self.user)
+        return data["me"]["owner"]["repository"]["pulls"]["edges"][0]["node"]
+
+    @freeze_time("2021-02-02")
+    def setUp(self):
+        self.user = OwnerFactory(username="test-pull-user", name="boii")
+        self.repository = RepositoryFactory(author=self.user, active=True, private=True, name="test-repo-for-pull")
+        self.pull = PullFactory(
+            repository=self.repository,
+            title="test-pull-request",
+            author=self.user,
+            head=CommitFactory(repository=self.repository, author=self.user, commitid="5672734ij1n234918231290j12nasdfioasud0f9").commitid,
+            base=None,
+        )
+
+    def test_when_repository_has_null_base(self):
+        assert self.fetch_repository() == {
+            'title': None,
+            'state': 'open',
+            'pullId': 1,
+            'updatestamp': None,
+            'author': {
+                'username': 'test-pull-user'
+            },
+            'head': {
+                'commitid': '5672734ij1n234918231290j12nasdfioasud0f9'
+            },
+            'base': None
+        }
--- graphql_api/tests/test_repository.py
+++ graphql_api/tests/test_repository.py
@@ -2,9 +2,8 @@ from freezegun import freeze_time
 
 from django.test import TransactionTestCase
 
-from core.models import Pull
 from codecov_auth.tests.factories import OwnerFactory
-from core.tests.factories import RepositoryFactory, PullFactory, CommitFactory
+from core.tests.factories import RepositoryFactory
 from .helper import GraphQLTestHelper
 
 query_repository = """{
@@ -27,45 +26,10 @@ query_repository = """{
 }
 """
 
-query_repository_with_pull = """{
-    me {
-        owner {
-            repository(name: "test-repo") {
-                name
-                pulls {
-                    edges {
-                        node {
-                            title
-                            state
-                            pullId
-                            updatestamp
-                            author {
-                                username
-                            }
-                            head {
-                                commitid
-                            }
-                            base {
-                                commitid
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-"""
-
-
 class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
     def fetch_repository(self):
         data = self.gql_request(query_repository, user=self.user)
         return data["me"]["owner"]["repositories"]["edges"][0]["node"]
-
-    def fetch_repository_with_pulls(self):
-        data = self.gql_request(query_repository_with_pull, user=self.user)
-        return data["me"]["owner"]["repository"]["pulls"]["edges"][0]["node"]
 
     def setUp(self):
         self.user = OwnerFactory(username="codecov-user")
@@ -98,30 +62,4 @@ class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
             "private": True,
             "coverage": 75,
             "updatedAt": "2021-01-01T00:00:00+00:00",
-        }
-
-    @freeze_time("2021-01-01")
-    def test_when_repository_has_null_base(self):
-        self.repository = RepositoryFactory(author=self.user, active=True, private=True, name="test-repo")
-        self.pull = PullFactory(
-            pullid=40,
-            repository=self.repository,
-            title="test-pull-request",
-            author=self.user,
-            head=CommitFactory(repository=self.repository, author=self.user, commitid="5672734ij1n234918231290j12nasdfioasud0f9").commitid,
-            base=None,
-        )
-        Pull.objects.filter(id=1).delete()
-        assert self.fetch_repository_with_pulls() == {
-            'title': 'test-pull-request',
-            'state': 'open',
-            'pullId': 40,
-            'updatestamp': '2021-01-01T00:00:00',
-            'author': {
-                'username': 'codecov-user'
-            },
-            'head': {
-                'commitid': '5672734ij1n234918231290j12nasdfioasud0f9'
-            },
-            'base': None
         }
