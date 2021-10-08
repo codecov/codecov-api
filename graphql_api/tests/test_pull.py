9fda57f81526027f0cbcd34bd9099bb4ba241ee3
Adrian Viquez <adrian@codecov.io>
2021-10-08 15:05:46 -0700

    add test to cover case when base is not none

--- graphql_api/tests/test_pull.py
+++ graphql_api/tests/test_pull.py
@@ -42,26 +42,27 @@ query_repository = """{
 class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
     def fetch_repository(self):
         data = self.gql_request(query_repository, user=self.user)
-        return data["me"]["owner"]["repository"]["pulls"]["edges"][0]["node"]
+        return data["me"]["owner"]["repository"]["pulls"]["edges"]
+
+    def setUp(self):
+        self.user = OwnerFactory(username="test-pull-user")
+        self.repository = RepositoryFactory(author=self.user, active=True, private=True, name="test-repo-for-pull")
 
     @freeze_time("2021-02-02")
-    def setUp(self):
-        self.user = OwnerFactory(username="test-pull-user", name="boii")
-        self.repository = RepositoryFactory(author=self.user, active=True, private=True, name="test-repo-for-pull")
-        self.pull = PullFactory(
+    def test_when_repository_has_null_base(self):
+        PullFactory(
             repository=self.repository,
-            title="test-pull-request",
+            pullid=18,
+            title="test-null-base",
             author=self.user,
             head=CommitFactory(repository=self.repository, author=self.user, commitid="5672734ij1n234918231290j12nasdfioasud0f9").commitid,
             base=None,
         )
-
-    def test_when_repository_has_null_base(self):
-        assert self.fetch_repository() == {
-            'title': None,
+        assert self.fetch_repository()[1]["node"] == {
+            'title': "test-null-base",
             'state': 'OPEN',
-            'pullId': 1,
-            'updatestamp': None,
+            'pullId': 18,
+            'updatestamp': '2021-02-02T00:00:00',
             'author': {
                 'username': 'test-pull-user'
             },
@@ -69,4 +70,30 @@ class TestFetchRepository(GraphQLTestHelper, TransactionTestCase):
                 'commitid': '5672734ij1n234918231290j12nasdfioasud0f9'
             },
             'base': None
+        }
+
+    @freeze_time("2021-02-02")
+    def test_with_complete_pull_request(self):
+        PullFactory(
+            repository=self.repository,
+            title="test-pull-request",
+            pullid=13,
+            author=self.user,
+            head=CommitFactory(repository=self.repository, author=self.user, commitid="5672734ij1n234918231290j12nasdfioasud0f9").commitid,
+            base=CommitFactory(repository=self.repository, author=self.user, commitid="9asd78fa7as8d8fa97s8d7fgagsd8fa9asd8f77s").commitid,
+        )
+        assert self.fetch_repository()[1]["node"] == {
+            'title': 'test-pull-request',
+            'state': 'OPEN',
+            'pullId': 13,
+            'updatestamp': '2021-02-02T00:00:00',
+            'author': {
+                'username': 'test-pull-user'
+            },
+            'head': {
+                'commitid': '5672734ij1n234918231290j12nasdfioasud0f9'
+            },
+            'base': {
+                'commitid': '9asd78fa7as8d8fa97s8d7fgagsd8fa9asd8f77s'
+            }
         }
