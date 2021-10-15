80544b7fa1e28361d0e3afde3f425f31adb88ea3
Dorian Amouroux <dorian@codecov.io>
2021-10-15 12:43:23 +0200

    add command to compare with parent

--- compare/commands/compare/compare.py
+++ compare/commands/compare/compare.py
@@ -15,5 +15,16 @@ class CompareCommands(BaseCommand):
             commit, parent_commit
         )
 
+    async def compare_pull_request(self, pull):
+        head_commit = await self.get_command("commit").fetch_commit(
+            pull.repository, pull.head
+        )
+        base_commit = await self.get_command("commit").fetch_commit(
+            pull.repository, pull.compared_to
+        )
+        return await self.get_interactor(CompareCommitsInteractor).execute(
+            head_commit, base_commit
+        )
+
     def get_impacted_files(self, comparison):
         return self.get_interactor(GetImpactedFilesInteractor).execute(comparison)
