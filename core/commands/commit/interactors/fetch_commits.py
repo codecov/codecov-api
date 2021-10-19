071f0452e50c6f3ea1a137eebe113fd1a3ca85d5
Adrian Viquez <adrian@codecov.io>
2021-10-19 11:03:51 -0700

    prefetched reports when fetching for commits

--- core/commands/commit/interactors/fetch_commits.py
+++ core/commands/commit/interactors/fetch_commits.py
@@ -1,5 +1,8 @@
 from asgiref.sync import sync_to_async
 from codecov.commands.base import BaseInteractor
+from django.db.models import Prefetch
+
+from reports.models import CommitReport
 
 class FetchCommitsInteractor(BaseInteractor):
     def apply_filters_to_commits_queryset(self, queryset, filters):
@@ -11,6 +14,10 @@ class FetchCommitsInteractor(BaseInteractor):
 
     @sync_to_async
     def execute(self, repository, filters):
-        queryset = repository.commits.defer("report").select_related("author").all()
+        # prefetch the CommitReport with the ReportLevelTotals
+        prefetch = Prefetch(
+            "reports", queryset=CommitReport.objects.select_related("reportleveltotals")
+        )
+        queryset = repository.commits.prefetch_related(prefetch).all()
         queryset = self.apply_filters_to_commits_queryset(queryset, filters)
         return queryset
--- graphql_api/types/commit/commit.py
+++ graphql_api/types/commit/commit.py
@@ -1,9 +1,7 @@
-from os import sync
 import yaml
 from ariadne import ObjectType
 
 from graphql_api.dataloader.owner import load_owner_by_id
-from graphql_api.dataloader.totals import load_totals_by_id
 from graphql_api.dataloader.commit import load_commit_by_id
 from graphql_api.helpers.connection import queryset_to_connection
 from graphql_api.types.enums import OrderingDirection
@@ -30,9 +28,9 @@ def resolve_file(commit, info, path, flags=None):
 
 @commit_bindable.field("totals")
 def resolve_totals(commit, info):
-    if not commit.totals:
-        return None
-    return load_totals_by_id(info, commit.commitid, commit.repository_id)
+    if commit.commitreport and hasattr(commit.commitreport, "reportleveltotals"):
+        return commit.commitreport.reportleveltotals
+    return None
 
 
 @commit_bindable.field("author")
