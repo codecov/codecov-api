beb608340dcb3d5d4f0dcc57fe0a0bf7fa1761b0
Adrian Viquez <adrian@codecov.io>
2021-11-30 16:02:48 -0800

    More fixes

--- compare/commands/compare/interactors/change_with_parent.py
+++ compare/commands/compare/interactors/change_with_parent.py
@@ -3,6 +3,7 @@ from asgiref.sync import sync_to_async
 from codecov.commands.base import BaseInteractor
 
 
+# pylint: disable=too-few-public-methods
 class ChangeWithParentInteractor(BaseInteractor):
     @sync_to_async
     def execute(self, current_commit_totals, parent_commit_totals):
--- compare/commands/compare/interactors/tests/test_change_with_parent.py
+++ compare/commands/compare/interactors/tests/test_change_with_parent.py
@@ -2,7 +2,6 @@ from asgiref.sync import async_to_sync
 from django.contrib.auth.models import AnonymousUser
 from django.test import TransactionTestCase
 
-from codecov_auth.tests.factories import OwnerFactory
 from core.tests.factories import CommitFactory
 from reports.tests.factories import CommitReportFactory, ReportLevelTotalsFactory
 
