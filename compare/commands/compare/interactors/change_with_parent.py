b3acc995f577cf32e8317aebc1a880dd017aca20
Adrian Viquez <adrian@codecov.io>
2021-12-02 00:41:29 -0800

    Made separate function to add pylint

--- compare/commands/compare/interactors/change_with_parent.py
+++ compare/commands/compare/interactors/change_with_parent.py
@@ -2,9 +2,10 @@ from asgiref.sync import sync_to_async
 
 from codecov.commands.base import BaseInteractor
 
-
-# pylint: disable=too-few-public-methods
 class ChangeWithParentInteractor(BaseInteractor):
+    def change_coverage(self, commit_totals, compare_to_totals):
+        return commit_totals.coverage - compare_to_totals.coverage
+
     @sync_to_async
     def execute(self, current_commit_totals, parent_commit_totals):
         if not hasattr(current_commit_totals, "coverage") or not hasattr(
@@ -12,4 +13,4 @@ class ChangeWithParentInteractor(BaseInteractor):
         ):
             return None
 
-        return current_commit_totals.coverage - parent_commit_totals.coverage
+        return self.change_coverage(current_commit_totals, parent_commit_totals)
