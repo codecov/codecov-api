e3883c4e97ff292862fc9299feca7f124af991d4
Terry Smith <terry@codecov.io>
2021-12-07 17:56:22 -0400

    isort plz

--- reports/tests/factories.py
+++ reports/tests/factories.py
@@ -6,8 +6,8 @@ import factory
 from factory.django import DjangoModelFactory
 
 from codecov_auth.tests.factories import OwnerFactory
-from graphql_api.types.enums import UploadErrorEnum
 from core.tests.factories import CommitFactory, RepositoryFactory
+from graphql_api.types.enums import UploadErrorEnum
 from reports import models
 
 
