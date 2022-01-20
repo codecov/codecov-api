8725c5a67cf16ac125e05519b2e6628455596f6a
Eli Hooten <eli@codecov.io>
2022-01-20 07:15:22 -0600

    Update to v4.6.4

---
+++ core/migrations/0008_version_v4_6_4.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+
+def add_version(apps, schema):
+    version = apps.get_model("core", "Version")
+    version.objects.all().delete()
+    v = version(version="v4.6.4")
+    v.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0006_version_v4_6_3"),
+    ]
+
+    operations = [
+        migrations.RunPython(add_version),
+    ]
