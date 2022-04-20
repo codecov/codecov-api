c48ec2fcfaf15a36668f0952b27eecb60737f894
Gguidini <giovanni@codecov.io>
2022-04-20 18:32:21 -0300

    Add routes for ghe and gle webhooks

--- webhook_handlers/urls.py
+++ webhook_handlers/urls.py
@@ -9,7 +9,9 @@ from .views.gitlab import GitLabWebhookHandler
 
 urlpatterns = [
     path("github", GithubWebhookHandler.as_view(), name="github-webhook"),
+    path("github_enterprise", GithubWebhookHandler.as_view(), name="github_enterprise-webhook"),
     path("bitbucket", BitbucketWebhookHandler.as_view(), name="bitbucket-webhook"),
     path("gitlab", GitLabWebhookHandler.as_view(), name="gitlab-webhook"),
+    path("gitlab_enterprise", GitLabWebhookHandler.as_view(), name="gitlab_enterprise-webhook"),
     path("stripe", StripeWebhookHandler.as_view(), name="old-stripe-webhook"),
 ]
