a39da96cf841430b3ec730da8c41898865f2e4c9
Gguidini <giovanni@codecov.io>
2022-05-02 12:11:52 -0300

    refactor GHE, GLE to override less functions

--- codecov_auth/views/github_enterprise.py
+++ codecov_auth/views/github_enterprise.py
@@ -1,7 +1,5 @@
 import logging
-from urllib.parse import urlencode, urljoin
 
-from asgiref.sync import async_to_sync
 from django.conf import settings
 from shared.torngit import GithubEnterprise
 
@@ -15,7 +13,7 @@ class GithubEnterpriseLoginView(GithubLoginView):
     error_redirection_page = "/"
 
     @property
-    def get_repo_service_instance(self):
+    def repo_service_instance(self):
         return GithubEnterprise(
             oauth_consumer_token=dict(
                 key=settings.GITHUB_ENTERPRISE_CLIENT_ID,
@@ -23,15 +21,9 @@ class GithubEnterpriseLoginView(GithubLoginView):
             )
         )
 
-    def get_url_to_redirect_to(self, scope):
-        repo_service = GithubEnterprise()
-        base_url = urljoin(repo_service.service_url, "login/oauth/authorize")
-        state = self.generate_state()
-        query = dict(
-            response_type="code",
-            scope=",".join(scope),
+    @property
+    def redirect_info(self):
+        return dict(
+            repo_service=GithubEnterprise(),
             client_id=settings.GITHUB_ENTERPRISE_CLIENT_ID,
-            state=state,
         )
-        query_str = urlencode(query)
-        return f"{base_url}?{query_str}"
