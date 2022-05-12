0e330e3607a4b6d5f608e067da04951bab71a528
Dana <dana@codecov.io>
2022-05-12 19:59:01 +0300

    add tests

--- core/tests/test_admin.py
+++ core/tests/test_admin.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
 
 from codecov_auth.tests.factories import OwnerFactory
+from django.contrib.admin.sites import AdminSite
+
+from core.admin import RepositoryAdmin
+from core.models import Repository
 
 
 class AdminTest(TestCase):
     def setUp(self):
         self.user = OwnerFactory()
+        self.owner_admin = RepositoryAdmin(Repository, AdminSite)
 
     def test_staff_can_access_admin(self):
         self.user.staff = True
@@ -19,3 +24,8 @@ class AdminTest(TestCase):
         self.client.force_login(user=self.user)
         response = self.client.get("/admin/")
         self.assertEqual(response.status_code, 302)
+
+    def test_readonly_fields(self):
+        readonly_fields = self.owner_admin.get_readonly_fields(request=None)
+        assert "bot" not in readonly_fields
+        assert "using_integration" not in readonly_fields
