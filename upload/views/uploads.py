492ee1fe84165087e6eca9dfdc9d47a205a0bc40
Dana <dana@codecov.io>
2022-06-13 14:54:44 +0300

    create upload permission and add it to the upload view

--- upload/views/uploads.py
+++ upload/views/uploads.py
@@ -2,17 +2,22 @@ import logging
 
 from django.http import HttpRequest, HttpResponseNotAllowed, HttpResponseNotFound
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, BasePermission
 
 from upload.serializers import UploadSerializer
 
 log = logging.getLogger(__name__)
 
 
+class CanDoCoverageUploadsPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.auth is not None and "upload" in request.auth.get_scopes()
+
+
 class UploadViews(ListCreateAPIView):
     serializer_class = UploadSerializer
     permission_classes = [
-        AllowAny,
+        CanDoCoverageUploadsPermission,
     ]
 
     def create(self, request: HttpRequest, repo: str, commit_id: str, report_id: str):
