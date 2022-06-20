c7ded3a683ef509f505d3075742a9a5928bbd6d1
Dana <dana@codecov.io>
2022-06-20 21:23:10 +0300

    temporary response status change in tests

--- upload/tests/views/test_uploads.py
+++ upload/tests/views/test_uploads.py
@@ -7,11 +7,11 @@ def test_uploads_get_not_allowed(client):
     url = reverse("new_upload.uploads", args=["the-repo", "commit-sha", "report-id"])
     assert url == "/upload/the-repo/commits/commit-sha/reports/report-id/uploads"
     res = client.get(url)
-    assert res.status_code == 405
+    assert res.status_code == 401
 
 
 def test_uploads_post_empty(client):
     url = reverse("new_upload.uploads", args=["the-repo", "commit-sha", "report-id"])
     assert url == "/upload/the-repo/commits/commit-sha/reports/report-id/uploads"
     res = client.post(url, content_type="application/json", data={})
-    assert res.status_code == 404
+    assert res.status_code == 401
