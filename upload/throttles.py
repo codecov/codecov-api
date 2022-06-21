92b6261b5fead55e6b91edc4a9ef62ebb4d95153
Dana <dana@codecov.io>
2022-06-21 15:09:10 +0300

    simple modifications in throttle classes

--- upload/throttles.py
+++ upload/throttles.py
@@ -2,6 +2,7 @@ import logging
 from datetime import timedelta
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.utils import timezone
 from rest_framework.throttling import BaseThrottle
@@ -21,10 +22,10 @@ class UploadsPerCommitThrottle(BaseThrottle):
         try:
             repository = Repository.objects.get(
                 name=view.kwargs.get("repo"),
-                author__username=request.user,
+                author=request.user,
             )
             commit = Commit.objects.get(
-                commitid=request.commit_id, repository=repository
+                commitid=view.kwargs.get("commit_id"), repository=repository
             )
             new_session_count = ReportSession.objects.filter(
                 ~Q(state="error"),
@@ -38,7 +39,7 @@ class UploadsPerCommitThrottle(BaseThrottle):
                     log.info(
                         "Old session count would not have blocked this upload",
                         extra=dict(
-                            commit=request.commit_id,
+                            commit=view.kwargs.get("commit_id"),
                             session_count=session_count,
                             repoid=repository.repoid,
                             old_session_count=session_count,
@@ -48,17 +49,19 @@ class UploadsPerCommitThrottle(BaseThrottle):
                 log.warning(
                     "Too many uploads to this commit",
                     extra=dict(
-                        commit=request.commit_id,
+                        commit=view.kwargs.get("commit_id"),
                         session_count=session_count,
                         repoid=repository.repoid,
                     ),
                 )
+                print(new_session_count)
+                print(current_upload_limit)
                 return False
             elif session_count > current_upload_limit:
                 log.info(
                     "Old session count would block this upload",
                     extra=dict(
-                        commit=request.commit_id,
+                        commit=view.kwargs.get("commit_id"),
                         session_count=session_count,
                         repoid=repository.repoid,
                         old_session_count=session_count,
@@ -66,52 +69,54 @@ class UploadsPerCommitThrottle(BaseThrottle):
                     ),
                 )
             return True
-        except Commit.DoesNotExist:
+        except ObjectDoesNotExist:
             return True
 
 
 class UploadsPerWindowThrottle(BaseThrottle):
     def allow_request(self, request, view):
         commit_id = view.kwargs.get("commit_id")
-
-        repository = Repository.objects.get(
-            name=view.kwargs.get("repo"),
-            author__username=request.user,
-        )
-        commit = Commit.objects.defer("report").get(
-            commitid=commit_id,
-            repository=repository,
-        )
-
-        if settings.UPLOAD_THROTTLING_ENABLED and repository.private:
-            owner = _determine_responsible_owner(repository)
-            limit = USER_PLAN_REPRESENTATIONS.get(owner.plan, {}).get(
-                "monthly_uploads_limit"
+        try:
+            repository = Repository.objects.get(
+                name=view.kwargs.get("repo"),
+                author=request.user,
             )
-            if limit is not None:
-                did_commit_uploads_start_already = ReportSession.objects.filter(
-                    report__commit=commit
-                ).exists()
-                if not did_commit_uploads_start_already:
-                    limit = USER_PLAN_REPRESENTATIONS[owner.plan].get(
-                        "monthly_uploads_limit"
-                    )
-                    uploads_used = ReportSession.objects.filter(
-                        report__commit__repository__author_id=owner.ownerid,
-                        report__commit__repository__private=True,
-                        created_at__gte=timezone.now() - timedelta(days=30),
-                        # attempt at making the query more performant by telling the db to not
-                        # check old commits, which are unlikely to have recent uploads
-                        report__commit__timestamp__gte=timezone.now()
-                        - timedelta(days=60),
-                        upload_type="uploaded",
-                    )[:limit].count()
-                    if uploads_used >= limit:
-                        log.warning(
-                            "User exceeded its limits for usage",
-                            extra=dict(
-                                ownerid=owner.ownerid, repoid=commit.repository_id
-                            ),
+            commit = Commit.objects.defer("report").get(
+                commitid=commit_id,
+                repository=repository,
+            )
+
+            if settings.UPLOAD_THROTTLING_ENABLED and repository.private:
+                owner = _determine_responsible_owner(repository)
+                limit = USER_PLAN_REPRESENTATIONS.get(owner.plan, {}).get(
+                    "monthly_uploads_limit"
+                )
+                if limit is not None:
+                    did_commit_uploads_start_already = ReportSession.objects.filter(
+                        report__commit=commit
+                    ).exists()
+                    if not did_commit_uploads_start_already:
+                        limit = USER_PLAN_REPRESENTATIONS[owner.plan].get(
+                            "monthly_uploads_limit"
                         )
-                        return False
-        return True
+                        uploads_used = ReportSession.objects.filter(
+                            report__commit__repository__author_id=owner.ownerid,
+                            report__commit__repository__private=True,
+                            created_at__gte=timezone.now() - timedelta(days=30),
+                            # attempt at making the query more performant by telling the db to not
+                            # check old commits, which are unlikely to have recent uploads
+                            report__commit__timestamp__gte=timezone.now()
+                            - timedelta(days=60),
+                            upload_type="uploaded",
+                        )[:limit].count()
+                        if uploads_used >= limit:
+                            log.warning(
+                                "User exceeded its limits for usage",
+                                extra=dict(
+                                    ownerid=owner.ownerid, repoid=commit.repository_id
+                                ),
+                            )
+                            return False
+            return True
+        except ObjectDoesNotExist:
+            return True
