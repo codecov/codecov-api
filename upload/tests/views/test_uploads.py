8336ffe8111c480e75b153b61697794e89337af2
Gguidini <giovanni@codecov.io>
2022-07-04 14:53:56 -0300

    mock the upload task during test

--- upload/tests/views/test_uploads.py
+++ upload/tests/views/test_uploads.py
@@ -15,13 +15,16 @@ def test_uploads_get_not_allowed(client):
     assert res.status_code == 401
 
 
-def test_uploads_post_empty(db, mocker):
+def test_uploads_post_empty(db, mocker, mock_redis):
     mocker.patch.object(
         CanDoCoverageUploadsPermission, "has_permission", return_value=True
     )
     mocker.patch(
         "services.archive.StorageService.create_presigned_put",
         return_value="presigned put",
+    )
+    mocker.patch(
+        "upload.views.uploads.UploadViews.trigger_upload_task", return_value=True
     )
     repository = RepositoryFactory(name="the_repo", author__username="codecov")
     commit = CommitFactory(repository=repository)
