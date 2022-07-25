a0fd0a204423c3a0b8a00a7c3b4dac222f7b4120
Dana <dana@codecov.io>
2022-07-25 23:08:36 +0300

    triggering the update commit task

--- upload/views/commits.py
+++ upload/views/commits.py
@@ -6,6 +6,7 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import AllowAny
 
 from core.models import Commit, Repository
+from services.task import TaskService
 from upload.serializers import CommitSerializer
 
 log = logging.getLogger(__name__)
@@ -24,16 +25,20 @@ class CommitViews(ListCreateAPIView):
         return Commit.objects.filter(repository=repository)
 
     def perform_create(self, serializer):
+        # TODO we should make sure that the commit is not already there, otherwise we'll get 500
         repository = self.get_repo()
-        return serializer.save(repository=repository)
+        commit = serializer.save(repository=repository)
+        TaskService().update_commit(commitid=commit.commitid, repoid=commit.repoid)
+        return commit
 
     def list(self, request: HttpRequest, repo: str):
         return HttpResponseNotAllowed(permitted_methods=["POST"])
 
     def get_repo(self) -> Repository:
         # TODO this is not final - how is getting the repo is still in discuss
-        repoid = self.kwargs["repo"]
+        repoid = self.kwargs.get("repo")
         try:
+            # TODO fix this: this might return multiple repos
             repository = Repository.objects.get(name=repoid)
             return repository
         except Repository.DoesNotExist:
