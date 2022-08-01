c5b663667f181158b9c6f6ff760dba7ea999d3cc
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-08-01 13:41:37 -0300

    Feat/new upload endpoint (#1097)
    * mark current upload endpoint as legacy
    * add empty new routes

--- upload/urls.py
+++ upload/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path
 
-from .views import UploadDownloadHandler, UploadHandler
+from upload.views.commits import CommitViews
+from upload.views.legacy import UploadDownloadHandler, UploadHandler
+from upload.views.reports import ReportViews
+from upload.views.uploads import UploadViews
 
 urlpatterns = [
     # use regex to make trailing slash optional
@@ -9,5 +12,18 @@ urlpatterns = [
         UploadDownloadHandler.as_view(),
         name="upload-download",
     ),
+    # Empty routes that will become the new upload endpoint eventually
+    path(
+        "<str:repo>/commits/<str:commit_sha>/reports/<str:reportid>/uploads",
+        UploadViews.as_view(),
+        name="new_upload.uploads",
+    ),
+    path(
+        "<str:repo>/commits/<str:commit_sha>/reports",
+        ReportViews.as_view(),
+        name="new_upload.reports",
+    ),
+    path("<str:repo>/commits", CommitViews.as_view(), name="new_upload.commits"),
+    # This was getting in the way of the new endpoints, so I moved to the end
     re_path("(?P<version>\w+)/?", UploadHandler.as_view(), name="upload-handler"),
 ]
