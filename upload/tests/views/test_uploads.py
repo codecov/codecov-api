7a46e3add6fa496c186a5b708a3520bd46955ecc
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-08-15 09:12:52 -0300

    New upload endpoint/upload serializer (#1100)
    * implement UploadSerializer
    
    * add UploadSerializer to UploadView and create uploads in PUSH
    
    * avoid recalculating values that can be reused + make exception error strings static
    
    * improve tests making sure the exception we get is the one we expect
    
    * change class used for ValidationError from django to rest_framework. The rest_framework one doesn't accept params so I removed those, we can get them from the request anyway
    
    * remove duplicated fields + better organize them

--- upload/tests/views/test_uploads.py
+++ upload/tests/views/test_uploads.py
@@ -1,15 +1,119 @@
+import uuid
+
+import pytest
 from django.urls import reverse
+from rest_framework.exceptions import ValidationError
+from rest_framework.test import APIClient
+
+from core.tests.factories import CommitFactory, RepositoryFactory
+from reports.models import CommitReport
+from upload.views.uploads import UploadViews
 
 
-def test_upload_get_not_allowed(client):
-    url = reverse("new_upload.uploads", args=["the-repo", "commit-sha", "reportid"])
-    assert url == "/upload/the-repo/commits/commit-sha/reports/reportid/uploads"
+def test_uploads_get_not_allowed(client):
+    url = reverse("new_upload.uploads", args=["the-repo", "commit-sha", "report-id"])
+    assert url == "/upload/the-repo/commits/commit-sha/reports/report-id/uploads"
     res = client.get(url)
     assert res.status_code == 405
 
 
-def test_upload_post_empty(client):
-    url = reverse("new_upload.uploads", args=["the-repo", "commit-sha", "reportid"])
-    assert url == "/upload/the-repo/commits/commit-sha/reports/reportid/uploads"
-    res = client.post(url, content_type="application/json", data={})
-    assert res.status_code == 404
+def test_get_repo(db):
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    repository.save()
+    upload_views = UploadViews()
+    upload_views.kwargs = dict(repo=repository.name)
+    recovered_repo = upload_views.get_repo()
+    assert recovered_repo == repository
+
+
+def test_get_repo_error(db):
+    upload_views = UploadViews()
+    upload_views.kwargs = dict(repo="repo_missing")
+    with pytest.raises(ValidationError) as exp:
+        upload_views.get_repo()
+    assert exp.match("Repository not found")
+
+
+def test_get_commit(db):
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    commit = CommitFactory(repository=repository)
+    repository.save()
+    commit.save()
+    upload_views = UploadViews()
+    upload_views.kwargs = dict(repo=repository.name, commit_sha=commit.commitid)
+    recovered_commit = upload_views.get_commit(repository)
+    assert recovered_commit == commit
+
+
+def test_get_commit_error(db):
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    repository.save()
+    upload_views = UploadViews()
+    upload_views.kwargs = dict(repo=repository.name, commit_sha="missing_commit")
+    with pytest.raises(ValidationError) as exp:
+        upload_views.get_commit(repository)
+    assert exp.match("Commit SHA not found")
+
+
+def test_get_report(db):
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    commit = CommitFactory(repository=repository)
+    report = CommitReport(commit=commit)
+    repository.save()
+    commit.save()
+    report.save()
+    upload_views = UploadViews()
+    upload_views.kwargs = dict(
+        repo=repository.name, commit_sha=commit.commitid, reportid=report.external_id
+    )
+    recovered_report = upload_views.get_report(commit)
+    assert recovered_report == report
+
+
+def test_get_report_error(db):
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    commit = CommitFactory(repository=repository)
+    repository.save()
+    commit.save()
+    upload_views = UploadViews()
+    report_uuid = uuid.uuid4()
+    upload_views.kwargs = dict(
+        repo=repository.name, commit_sha=commit.commitid, reportid=report_uuid
+    )
+    with pytest.raises(ValidationError) as exp:
+        upload_views.get_report(commit)
+    assert exp.match("Report not found")
+
+
+def test_uploads_post_empty(db, mocker, mock_redis):
+    presigned_put_mock = mocker.patch(
+        "services.archive.StorageService.create_presigned_put",
+        return_value="presigned put",
+    )
+    repository = RepositoryFactory(name="the_repo", author__username="codecov")
+    commit = CommitFactory(repository=repository)
+    commit_report = CommitReport.objects.create(commit=commit)
+    repository.save()
+    commit_report.save()
+
+    owner = repository.author
+    client = APIClient()
+    client.force_authenticate(user=owner)
+    url = reverse(
+        "new_upload.uploads",
+        args=[repository.name, commit.commitid, commit_report.external_id],
+    )
+    response = client.post(
+        url,
+        {"state": "uploaded"},
+        format="json",
+    )
+    response_json = response.json()
+    assert response.status_code == 201
+    assert all(
+        map(
+            lambda x: x in response_json.keys(),
+            ["external_id", "created_at", "raw_upload_location"],
+        )
+    )
+    presigned_put_mock.assert_called()
