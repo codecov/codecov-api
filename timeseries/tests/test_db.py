cd10fcaa872d098bc31e9ca56cedd9654b133960
Scott Nelson <scott@codecov.io>
2022-09-19 13:35:32 -0400

    Update test

--- timeseries/tests/test_db.py
+++ timeseries/tests/test_db.py
@@ -14,7 +14,18 @@ class DatabaseTests(TransactionTestCase):
 
     @patch("django.db.backends.postgresql.base.DatabaseWrapper.is_usable")
     def test_db_reconnect(self, is_usable):
-        is_usable.return_value = False
+        timeseries_database_engine = settings.DATABASES["timeseries"]["ENGINE"]
+        settings.DATABASES["timeseries"]["ENGINE"] = "codecov.db"
+
+        is_usable.return_value = True
 
         with connections["timeseries"].cursor() as cursor:
             cursor.execute("SELECT 1")
+
+        is_usable.return_value = False
+
+        # it should reconnect and not raise an error
+        with connections["timeseries"].cursor() as cursor:
+            cursor.execute("SELECT 1")
+
+        settings.DATABASES["timeseries"]["ENGINE"] = timeseries_database_engine
