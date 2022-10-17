4893a4af873083a34f4f4e860767c71c19a746e2
Dana <dana@codecov.io>
2022-10-17 16:31:06 +0300

    making redis lock expire after some time when it's not being used or when the python process is done

--- legacy_migrations/management/commands/migrate.py
+++ legacy_migrations/management/commands/migrate.py
@@ -71,7 +71,9 @@ class Command(MigrateCommand):
             return MockLock()
 
         redis_connection = get_redis_connection()
-        lock = redis_lock.Lock(redis_connection, MIGRATION_LOCK_NAME)
+        lock = redis_lock.Lock(
+            redis_connection, MIGRATION_LOCK_NAME, expire=180, auto_renewal=True
+        )
         acquired = lock.acquire(timeout=180)
 
         if not acquired:
