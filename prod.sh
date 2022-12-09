304b315d09bec7506f337c9fbc8c86a0109b0698
trent-codecov <trent@codecov.io>
2022-12-09 15:04:48 -0600

    Add gunicorn timeout as env var

--- prod.sh
+++ prod.sh
@@ -7,4 +7,4 @@ if [ -f "/usr/local/bin/berglas" ]; then
   prefix="berglas exec --"
 fi
 
-$prefix gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT}
+$prefix gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT} --timeout "${GUNICORN_TIMEOUT:-600}"
