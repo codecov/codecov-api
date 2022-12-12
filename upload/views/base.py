232456f98e21a896b379a86efb6c0346a11a2e8d
Dana <dana@codecov.io>
2022-12-12 18:45:09 +0200

    adding logs because we lack some

--- upload/views/base.py
+++ upload/views/base.py
@@ -1,3 +1,5 @@
+import logging
+
 from rest_framework.exceptions import ValidationError
 
 from codecov_auth.models import Service
@@ -5,19 +7,26 @@ from core.models import Commit, Repository
 from reports.models import CommitReport
 from upload.views.helpers import get_repository_from_string
 
+log = logging.getLogger(__name__)
+
 
 class GetterMixin:
     def get_repo(self) -> Repository:
         service = self.kwargs.get("service")
+        repo_slug = self.kwargs.get("repo")
         try:
             service_enum = Service(service)
         except ValueError:
+            log.info(f"Service not found: {service}", extra=dict(repo_slug=repo_slug))
             raise ValidationError(f"Service not found: {service}")
 
-        repo_slug = self.kwargs.get("repo")
         repository = get_repository_from_string(service_enum, repo_slug)
 
         if not repository:
+            log.info(
+                "Repository not found",
+                extra=dict(repo_slug=repo_slug, repo=repository.name),
+            )
             raise ValidationError(f"Repository not found")
         return repository
 
@@ -29,6 +38,10 @@ class GetterMixin:
             )
             return commit
         except Commit.DoesNotExist:
+            log.info(
+                "Commit SHA not found",
+                extra=dict(repo=repo.name, commit_sha=commit_sha),
+            )
             raise ValidationError("Commit SHA not found")
 
     def get_report(self, commit: Commit) -> CommitReport:
@@ -39,4 +52,8 @@ class GetterMixin:
             report = CommitReport.objects.get(code=report_code, commit=commit)
             return report
         except CommitReport.DoesNotExist:
+            log.info(
+                "Report not found",
+                extra=dict(commit_sha=commit.commitid, report_code=report_code),
+            )
             raise ValidationError(f"Report not found")
