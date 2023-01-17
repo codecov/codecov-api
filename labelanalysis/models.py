b3191d98036935008aaf204010c121f0d6535468
Thiago Ramos <thiago@codecov.io>
2023-01-17 16:51:49 -0300

    Allowing null bases on label request models
    From trying to run this in the CI, we found that it might be hard for the customer to have a strong idea of which commit to use as the base. It might be easier for us to just ask the customer to give us the pull request and let us calculate the base ourselves
    
    ```
    BEGIN;
    --
    -- Add field processing_params to labelanalysisrequest
    --
    ALTER TABLE "labelanalysis_labelanalysisrequest" ADD COLUMN "processing_params" jsonb NULL;
    --
    -- Alter field base_commit on labelanalysisrequest
    --
    ALTER TABLE "labelanalysis_labelanalysisrequest" ALTER COLUMN "base_commit_id" DROP NOT NULL;
    --
    -- Alter field state_id on labelanalysisrequest
    --
    COMMIT;
    ``

--- labelanalysis/models.py
+++ labelanalysis/models.py
@@ -7,12 +7,16 @@ from codecov.models import BaseCodecovModel
 
 class LabelAnalysisRequest(BaseCodecovModel):
     base_commit = models.ForeignKey(
-        "core.Commit", on_delete=models.CASCADE, related_name="label_requests_as_base"
+        "core.Commit",
+        on_delete=models.CASCADE,
+        related_name="label_requests_as_base",
+        null=True,
     )
     head_commit = models.ForeignKey(
         "core.Commit", on_delete=models.CASCADE, related_name="label_requests_as_head"
     )
     requested_labels = ArrayField(models.TextField(), null=True)
+    processing_params = models.JSONField(null=True)
     state_id = models.IntegerField(
         null=False, choices=LabelAnalysisRequestState.choices()
     )
