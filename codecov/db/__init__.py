a4ad0a05fe1ddc0d07e2b33a1ff2a105073eeb6b
Scott Nelson <scott@codecov.io>
2023-01-23 09:00:08 -0500

    Configure read-only database connections

--- codecov/db/__init__.py
+++ codecov/db/__init__.py
@@ -11,26 +11,34 @@ class DatabaseRouter:
     https://docs.djangoproject.com/en/4.0/topics/db/multi-db/#automatic-database-routing
     """
 
-    # mapping of app label -> database name
-    # (if an entry is missing then the `default` database is assumed)
-    databases = {
-        "timeseries": "timeseries",
-    }
-
     def db_for_read(self, model, **hints):
-        return self.databases.get(model._meta.app_label, "default")
+        if model._meta.app_label == "timeseries":
+            if settings.TIMESERIES_DATABASE_READ_REPLICA_ENABLED:
+                return "timeseries_read"
+            else:
+                return "timeseries"
+        else:
+            if settings.DATABASE_READ_REPLICA_ENABLED:
+                return "default_read"
+            else:
+                return "default"
 
     def db_for_write(self, model, **hints):
-        return self.databases.get(model._meta.app_label, "default")
-
-    def allow_relation(self, obj1, obj2, **hints):
-        obj1_database = self.databases.get(obj1._meta.app_label, "default")
-        obj2_database = self.databases.get(obj2._meta.app_label, "default")
-        return obj1_database == obj2_database
+        if model._meta.app_label == "timeseries":
+            return "timeseries"
+        else:
+            return "default"
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if db == "timeseries" and not settings.TIMESERIES_ENABLED:
             log.warning("Skipping timeseries migration")
             return False
 
-        return db == self.databases.get(app_label, "default")
+        if db == "default_read":
+            log.warning("Skipping migration of read-only database")
+            return False
+
+        if app_label == "timeseries":
+            return db == "timeseries"
+        else:
+            return db == "default"
