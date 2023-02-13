579eb22412f01c0d6f39fcc9904eba4112b685ba
Dana <dana@codecov.io>
2023-02-13 22:15:06 +0200

    adding default value for max upload limit at app level

--- codecov_auth/models.py
+++ codecov_auth/models.py
@@ -125,7 +125,7 @@ class Owner(models.Model):
     student_updated_at = DateTimeWithoutTZField(null=True)
     onboarding_completed = models.BooleanField(default=False)
     is_superuser = models.BooleanField(null=True, default=False)
-    max_upload_limit = models.IntegerField(null=True)
+    max_upload_limit = models.IntegerField(null=True, default=150)
 
     objects = OwnerManager()
 
