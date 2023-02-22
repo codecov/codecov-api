bdf10b2dea346eb0daa58a8f384a1af561147a31
Adrian <adrian@codecov.io>
2023-02-22 12:15:07 -0800

    Add validation logic w/ username instead of org

--- codecov_auth/commands/owner/interactors/update_default_organization.py
+++ codecov_auth/commands/owner/interactors/update_default_organization.py
@@ -7,14 +7,14 @@ from codecov_auth.models import Owner, OwnerProfile
 
 
 class UpdateDefaultOrganizationInteractor(BaseInteractor):
-    def validate(self, default_org: Optional[Owner]) -> Owner:
+    def validate(
+        self,
+        default_org: Optional[Owner],
+    ) -> Optional[Owner]:
         if not self.current_user.is_authenticated:
             raise Unauthenticated()
 
-        if default_org is None:
-            return
-
-        if (
+        if default_org is None or (
             default_org.ownerid not in self.current_user.organizations
             and default_org.ownerid != self.current_user.ownerid
         ):
@@ -22,7 +22,7 @@ class UpdateDefaultOrganizationInteractor(BaseInteractor):
                 "Organization does not belong in current user's organization list"
             )
 
-    def update_default_organization(self, default_org: Owner):
+    def update_default_organization(self, default_org: Optional[Owner]):
         owner_profile, _ = OwnerProfile.objects.get_or_create(
             owner_id=self.current_user.ownerid
         )
@@ -31,9 +31,12 @@ class UpdateDefaultOrganizationInteractor(BaseInteractor):
 
     @sync_to_async
     def execute(self, default_org_username: str):
-        default_org = Owner.objects.filter(
-            username=default_org_username, service=self.service
-        ).first()
-        self.validate(default_org)
-        self.update_default_organization(default_org)
+        if default_org_username is None:
+            self.update_default_organization(None)
+        else:
+            default_org = Owner.objects.filter(
+                username=default_org_username, service=self.service
+            ).first()
+            self.validate(default_org)
+            self.update_default_organization(default_org)
         return default_org_username
