4c7f3a1e78d54525bbe4b9024a1bb2f19f8ffdf8
Rula Abu Hasna <rula@codecov.io>
2023-03-20 16:30:12 +0200

    keep the hash till gazebo is updated

--- graphql_api/types/owner/owner.py
+++ graphql_api/types/owner/owner.py
@@ -85,6 +85,12 @@ def resolve_is_current_user_an_admin(owner, info):
     return command.get_is_current_user_an_admin(owner, current_user)
 
 
+@owner_bindable.field("hashOwnerid")
+def resolve_hash_ownerid(owner, info):
+    hash_ownerid = sha1(str(owner.ownerid).encode())
+    return hash_ownerid.hexdigest()
+
+
 @owner_bindable.field("orgUploadToken")
 def resolve_org_upload_token(owner, info, **kwargs):
     command = info.context["executor"].get_command("owner")
