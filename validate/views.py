84b66719eceda26b6b2d6dd3748fb227572349ae
Scott Nelson <scott@codecov.io>
2023-05-01 16:14:29 -0400

    Added V2 YAML validation endpoint

--- validate/views.py
+++ validate/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from shared.validation.exceptions import InvalidYamlException
 from shared.yaml.validation import validate_yaml
@@ -13,7 +14,7 @@ from yaml import YAMLError, safe_load
 log = logging.getLogger(__name__)
 
 
-class ValidateYamlHandler(APIView):
+class V1ValidateYamlHandler(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
@@ -69,4 +70,72 @@ class ValidateYamlHandler(APIView):
                 f"Error at {str(e.error_location)}: \n{e.error_message}\n",
                 status=status.HTTP_400_BAD_REQUEST,
                 content_type="text/plain",
+            )
+
+
+class V2ValidateYamlHandler(V1ValidateYamlHandler):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        if not self.request.body:
+            return Response(
+                {
+                    "valid": False,
+                    "message": "YAML is empty",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Parse the yaml from the request body
+        try:
+            yaml_dict = safe_load(self.request.body)
+            if not isinstance(yaml_dict, dict):
+                log.warning(
+                    f"yaml_dict result from loading validate request body is not a dict",
+                    extra=dict(
+                        yaml_dict=yaml_dict, request_body=str(self.request.body)
+                    ),
+                )
+                return Response(
+                    {
+                        "valid": False,
+                        "message": "YAML must be a dictionary type",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        except YAMLError as e:
+            mark = e.problem_mark
+            return Response(
+                {
+                    "valid": False,
+                    "message": "YAML could not be parsed",
+                    "parse_error": {
+                        "problem": e.problem,
+                        "line": mark.line + 1,
+                        "column": mark.column + 1,
+                    },
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Validate the parsed yaml
+        try:
+            validated_yaml = validate_yaml(yaml_dict)
+            return Response(
+                {
+                    "valid": True,
+                    "message": "YAML is valid",
+                    "validated_yaml": validated_yaml,
+                }
+            )
+
+        except InvalidYamlException as e:
+            return Response(
+                {
+                    "valid": False,
+                    "message": "YAML does not match the accepted schema",
+                    "validation_error": e.error_dict,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
             )
