3dcc409813ee79d0e89421c0807c5c17107664f9
Scott Nelson <scott@codecov.io>
2023-05-10 09:32:25 -0400

    Added log warning to comparison dataloader

--- graphql_api/dataloader/comparison.py
+++ graphql_api/dataloader/comparison.py
@@ -1,3 +1,5 @@
+import logging
+
 from codecov.db import sync_to_async
 from compare.models import CommitComparison
 from core.models import Commit
@@ -6,6 +8,8 @@ from services.task import TaskService
 
 from .commit import CommitLoader
 from .loader import BaseLoader
+
+log = logging.getLogger(__name__)
 
 comparison_table = CommitComparison._meta.db_table
 commit_table = Commit._meta.db_table
@@ -84,23 +88,39 @@ class ComparisonLoader(BaseLoader):
         """
         Insert new comparisons for the given keys (skipping insert of any duplicates).
         """
+        comparisons = [
+            CommitComparison(
+                base_commit=commit_cache.get_by_commitid(base_commitid),
+                compare_commit=commit_cache.get_by_commitid(compare_commitid),
+            )
+            for (base_commitid, compare_commitid) in keys
+            if base_commitid
+            and commit_cache.get_by_commitid(base_commitid)
+            and compare_commitid
+            and commit_cache.get_by_commitid(compare_commitid)
+        ]
         CommitComparison.objects.bulk_create(
-            [
-                CommitComparison(
-                    base_commit=commit_cache.get_by_commitid(base_commitid),
-                    compare_commit=commit_cache.get_by_commitid(compare_commitid),
-                )
-                for (base_commitid, compare_commitid) in keys
-                if base_commitid
-                and commit_cache.get_by_commitid(base_commitid)
-                and compare_commitid
-                and commit_cache.get_by_commitid(compare_commitid)
-            ],
+            comparisons,
             ignore_conflicts=True,
         )
 
-        # refetch missing comparisons (since they cannot be returned from the create call abbove)
-        return self.batch_queryset(keys)
+        # refetch missing comparisons (since they cannot be returned from the create call above
+        # due to the use of `ignore_conflicts`)
+        results = self.batch_queryset(keys)
+
+        if len(results) != len(comparisons):
+            # We've been seeing some instances of commit comparisons being created but no
+            # corresponding compute comparisons task being enqueued.
+            # Not sure why this would happen but curious to see if we see this line in the logs
+            log.warning(
+                "Failed to refetch all commit comparisons",
+                extra=dict(
+                    created_count=len(comparisons),
+                    fetched_count=len(results),
+                ),
+            )
+
+        return results
 
     def _refresh_comparisons(self, comparisons, missing_keys, commit_cache):
         """
@@ -108,6 +128,8 @@ class ComparisonLoader(BaseLoader):
         """
         comparison_ids = []
         for key, comparison in comparisons.items():
+            # we already have these commits fetched so we might as well store them
+            # on the comparison for the call to `needs_recompute` below
             comparison.base_commit = commit_cache.get_by_pk(comparison.base_commit_id)
             comparison.compare_commit = commit_cache.get_by_pk(
                 comparison.compare_commit_id
@@ -116,9 +138,10 @@ class ComparisonLoader(BaseLoader):
             commit_comparison_service = CommitComparisonService(comparison)
             if key in missing_keys or commit_comparison_service.needs_recompute():
                 comparison_ids.append(comparison.pk)
-                commit_comparison_service.commit_comparison.state = (
-                    CommitComparison.CommitComparisonStates.PENDING
-                )
+
+                # optimistically update the state so we don't need to refetch this comparison
+                # (actual database update happens below)
+                comparison.state = CommitComparison.CommitComparisonStates.PENDING
 
         if len(comparison_ids) > 0:
             CommitComparison.objects.filter(pk__in=comparison_ids).update(
