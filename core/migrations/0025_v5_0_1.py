9f7faf02a5817404ec92e91dfa1edad38a54cae5
Trent Schmidt <trent@codecov.io>
2023-06-01 14:58:32 -0400

    Create 0025_v5_0_1.py

---
+++ core/migrations/0025_v5_0_1.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+
+def add_version(apps, schema):
+    version = apps.get_model("core", "Version")
+    version.objects.all().delete()
+    v = version(version="v5.0.1")
+    v.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("core", "0024_alter_commit_timestamp_alter_commit_updatestamp_and_more")]
+
+    operations = [migrations.RunPython(add_version)]
