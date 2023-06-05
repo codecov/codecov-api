fec562ded6b5ecdbfd01aea5ce83a24710bb8689
Rula Abu Hasna <rula@codecov.io>
2023-06-05 16:01:11 +0300

    addd the constraint

--- core/migrations/0026_auto_20230605_1134.py
+++ core/migrations/0026_auto_20230605_1134.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             """
-            ALTER TYPE notifications ADD VALUE 'codecov_slack_app';
+            ALTER TYPE notifications ADD VALUE IF NOT exists 'codecov_slack_app';            
             """
         ),
     ]
