69d3ff67a8a91ba9f9940a71c306a835c581e1c3
Scott Nelson <scott@codecov.io>
2023-06-12 15:04:23 -0400

    Added branches filter for merged/unmerged branches

--- core/commands/branch/interactors/fetch_branches.py
+++ core/commands/branch/interactors/fetch_branches.py
@@ -1,16 +1,32 @@
+from django.db.models import OuterRef, Q, Subquery
+
 from codecov.commands.base import BaseInteractor
 from codecov.db import sync_to_async
+from core.models import Commit
 
 
 class FetchRepoBranchesInteractor(BaseInteractor):
-    def apply_filters_to_branches_queryset(self, queryset, filters):
+    @sync_to_async
+    def execute(self, repository, filters):
+        queryset = repository.branches.all()
+
         filters = filters or {}
         search_value = filters.get("search_value")
         if search_value:
             queryset = queryset.filter(name__icontains=search_value)
-        return queryset
 
-    @sync_to_async
-    def execute(self, repository, filters):
-        queryset = repository.branches.all()
-        return self.apply_filters_to_branches_queryset(queryset, filters)
+        merged = filters.get("merged_branches", False)
+        if not merged:
+            queryset = queryset.annotate(
+                merged=Subquery(
+                    Commit.objects.filter(
+                        commitid=OuterRef("head"),
+                        repository_id=OuterRef("repository__repoid"),
+                    ).values("merged")[:1]
+                )
+            ).filter(
+                Q(merged__isnot=True)  # exclude merged branches
+                | Q(name=repository.branch)  # but always include the default branch
+            )
+
+        return queryset
