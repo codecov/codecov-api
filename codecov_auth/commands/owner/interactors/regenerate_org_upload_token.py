e230b659779fbd2cabc3ee38e9fc7e974e9520da
Rula Abu Hasna <rula@codecov.io>
2023-07-11 16:16:15 +0300

    allow only users that are part of org to regenerate

--- codecov_auth/commands/owner/interactors/regenerate_org_upload_token.py
+++ codecov_auth/commands/owner/interactors/regenerate_org_upload_token.py
@@ -1,6 +1,7 @@
 import uuid
 
-from billing.constants import ENTERPRISE_CLOUD_USER_PLAN_REPRESENTATIONS
+from codecov_auth.helpers import current_user_part_of_org
+
 from codecov.commands.base import BaseInteractor
 from codecov.commands.exceptions import Unauthenticated, Unauthorized, ValidationError
 from codecov.db import sync_to_async
@@ -13,6 +14,8 @@ class RegenerateOrgUploadTokenInteractor(BaseInteractor):
             raise Unauthenticated()
         if not owner_obj:
             raise ValidationError("Owner not found")
+        if not current_user_part_of_org(self.current_user, owner_obj):
+            raise Unauthorized()
 
     @sync_to_async
     def execute(self, owner):
