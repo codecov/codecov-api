9303e2d9eebca69b6e688df4737e564e9fb0fa4f
Gguidini <giovanni@codecov.io>
2023-07-21 12:00:20 +0200

    Reactivate label analysis
    Notice that I put label analysis and the upload associated with static analysis and called the whole thing ATS.
    I think that is the most accurate metric as the total overhead for the tests, technically, also includes static analysis.
    
    Ticket: [platform-team #20](https://github.com/codecov/platform-team/issues/20)

--- api/internal/tests/test_pagination.py
+++ api/internal/tests/test_pagination.py
@@ -16,23 +16,25 @@ class PageNumberPaginationTests(APITestCase):
 
         self.client.force_login(user=self.owner)
 
-    def _list(self, kwargs={}, query_params={}):
-        if not kwargs:
-            kwargs = {
-                "service": self.owner.service,
-                "owner_username": self.owner.username,
-            }
-        return self.client.get(reverse("users-list", kwargs=kwargs), data=query_params)
-
     def test_pagination_returned_page_size(self):
-        response = self._list()
+        def _list(kwargs={}, query_params={}):
+            if not kwargs:
+                kwargs = {
+                    "service": self.owner.service,
+                    "owner_username": self.owner.username,
+                }
+            return self.client.get(
+                reverse("users-list", kwargs=kwargs), data=query_params
+            )
+
+        response = _list()
 
         assert response.data["total_pages"] == 1
 
-        response = self._list(query_params={"page_size": "1"})
+        response = _list(query_params={"page_size": "1"})
 
         assert response.data["total_pages"] == 3
 
-        response = self._list(query_params={"page_size": "100"})
+        response = _list(query_params={"page_size": "100"})
 
         assert response.data["total_pages"] == 1
