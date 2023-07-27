dc2995f8e0452aeb971fb2f20f0b96026bb28209
Adrian <adrian@codecov.io>
2023-07-27 12:02:40 -0700

    Added riskysql to migration file

--- utils/migrations.py
+++ utils/migrations.py
@@ -52,20 +52,6 @@ class RiskyAlterIndexTogether(migrations.AlterIndexTogether):
         super().database_backwards(app_label, schema_editor, from_state, to_state)
 
 
-class RiskyAlterField(migrations.AlterField):
-    def database_forwards(self, app_label, schema_editor, from_state, to_state):
-        if settings.SKIP_RISKY_MIGRATION_STEPS:
-            return
-
-        super().database_forwards(app_label, schema_editor, from_state, to_state)
-
-    def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        if settings.SKIP_RISKY_MIGRATION_STEPS:
-            return
-
-        super().database_backwards(app_label, schema_editor, from_state, to_state)
-
-
 class RiskyAddIndex(migrations.AddIndex):
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if settings.SKIP_RISKY_MIGRATION_STEPS:
