08d5e06008cc46b6239ceb9c6c3279735c81b0da
Gguidini <giovanni@codecov.io>
2023-07-31 18:11:15 +0200

    chore: Fix flaky test
    This test (`test_pagination.py`) keeps failing the ATS step.
    These changes are an attempt at fixing it.

--- api/internal/tests/test_pagination.py
+++ api/internal/tests/test_pagination.py
@@ -8,7 +8,9 @@ from codecov_auth.tests.factories import OwnerFactory
 
 class PageNumberPaginationTests(APITestCase):
     def setUp(self):
-        self.client = Client()
+        pass
+
+    def test_pagination_returned_page_size(self):
         self.owner = OwnerFactory(plan="users-free", plan_user_count=5)
         self.users = [
             OwnerFactory(organizations=[self.owner.ownerid]),
@@ -16,27 +18,19 @@ class PageNumberPaginationTests(APITestCase):
             OwnerFactory(organizations=[self.owner.ownerid]),
         ]
 
-    def test_pagination_returned_page_size(self):
-        self.client.force_login(user=self.owner)
+        client = Client()
+        client.force_login(user=self.owner)
 
-        def _list(kwargs={}, query_params={}):
-            if not kwargs:
-                kwargs = {
-                    "service": self.owner.service,
-                    "owner_username": self.owner.username,
-                }
-            return self.client.get(
-                reverse("users-list", kwargs=kwargs), data=query_params
-            )
+        url = reverse(
+            "users-list",
+            kwargs=dict(service=self.owner.service, owner_username=self.owner.username),
+        )
 
-        response = _list()
-
+        response = client.get(url, data={})
         assert response.data["total_pages"] == 1
 
-        response = _list(query_params={"page_size": "1"})
-
+        response = client.get(url, data={"page_size": "1"})
         assert response.data["total_pages"] == 3
 
-        response = _list(query_params={"page_size": "100"})
-
+        response = client.get(url, data={"page_size": "100"})
         assert response.data["total_pages"] == 1
