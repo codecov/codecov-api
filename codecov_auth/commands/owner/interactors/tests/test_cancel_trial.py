2f6bd699871d36d21b5076f13612a0646dfe6dc2
Adrian <adrian@codecov.io>
2023-08-01 11:11:18 -0700

    Code 3644 misc trial additions (#57)
    * Add expiration mutation
    
    * Got rid of print and readded test

---
+++ codecov_auth/commands/owner/interactors/tests/test_cancel_trial.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+
+import pytest
+from asgiref.sync import async_to_sync
+from django.test import TransactionTestCase
+from freezegun import freeze_time
+
+from codecov.commands.exceptions import ValidationError
+from codecov_auth.models import Owner
+from codecov_auth.tests.factories import OwnerFactory
+from plan.constants import PlanName, TrialDaysAmount, TrialStatus
+
+from ..cancel_trial import CancelTrialInteractor
+
+
+class CancelTrialInteractorTest(TransactionTestCase):
+    @async_to_sync
+    def execute(self, current_user, org_username=None):
+        current_user = current_user
+        return CancelTrialInteractor(current_user, "github").execute(
+            org_username=org_username,
+        )
+
+    def test_cancel_trial_raises_exception_when_owner_is_not_in_db(self):
+        current_user = OwnerFactory(
+            username="random-user-123",
+            service="github",
+        )
+        with pytest.raises(ValidationError):
+            self.execute(current_user=current_user, org_username="some-other-username")
+
+    @freeze_time("2022-01-01T00:00:00")
+    def test_cancel_trial_raises_exception_when_owners_trial_status_is_not_started(
+        self,
+    ):
+        trial_start_date = None
+        trial_end_date = None
+        current_user = OwnerFactory(
+            username="random-user-123",
+            service="github",
+            trial_start_date=trial_start_date,
+            trial_end_date=trial_end_date,
+        )
+        with pytest.raises(ValidationError):
+            self.execute(current_user=current_user, org_username=current_user.username)
+
+    @freeze_time("2022-01-01T00:00:00")
+    def test_cancel_trial_raises_exception_when_owners_trial_status_is_expired(self):
+        now = datetime.utcnow()
+        trial_start_date = now + timedelta(days=-10)
+        trial_end_date = now + timedelta(days=-4)
+        current_user = OwnerFactory(
+            username="random-user-123",
+            service="github",
+            trial_start_date=trial_start_date,
+            trial_end_date=trial_end_date,
+        )
+        with pytest.raises(ValidationError):
+            self.execute(current_user=current_user, org_username=current_user.username)
+
+    @freeze_time("2022-01-01T00:00:00")
+    def test_cancel_trial_starts_trial_for_org_that_has_trial_ongoing(self):
+        now = datetime.utcnow()
+        trial_start_date = now
+        trial_end_date = now + timedelta(days=3)
+        # TODO: Add trial_status in CODE-3605-add-trial-logic
+        current_user: Owner = OwnerFactory(
+            username="random-user-123",
+            service="github",
+            trial_start_date=trial_start_date,
+            trial_end_date=trial_end_date,
+        )
+        self.execute(current_user=current_user, org_username=current_user.username)
+        current_user.refresh_from_db()
+
+        now = datetime.utcnow()
+        assert current_user.trial_end_date == now
+        assert current_user.trial_status == TrialStatus.EXPIRED.value
+        assert current_user.plan == PlanName.BASIC_PLAN_NAME.value
+        assert current_user.plan_activated_users == None
+        assert current_user.plan_user_count == 1
+        assert current_user.stripe_subscription_id == None
