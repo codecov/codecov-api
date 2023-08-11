7ecd515b663c7178cf5ef4e7ba7b95dfc87eb972
Gguidini <giovanni@codecov.io>
2023-08-11 10:05:39 +0200

    fix: update archive field cache
    * Handle case were a single model uses multiple archived fields
    (dynamic archived field cached property name)
    * Concentrate getting/setting cache in `__get__` and `__set__` methods

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -75,6 +75,7 @@ class ArchiveField:
         self.public_name = name
         self.db_field_name = "_" + name
         self.archive_field_name = "_" + name + "_storage_path"
+        self.cached_value_property_name = f"__{self.public_name}_cached_value"
 
     def _get_value_from_archive(self, obj):
         repository = obj.get_repository()
@@ -83,9 +84,7 @@ class ArchiveField:
         if archive_field:
             try:
                 file_str = archive_service.read_file(archive_field)
-                value = self.rehydrate_fn(obj, json.loads(file_str))
-                setattr(obj, "__archive_field_cached_value", value)
-                return value
+                return self.rehydrate_fn(obj, json.loads(file_str))
             except FileNotInStorageError:
                 log.error(
                     "Archive enabled field not in storage",
@@ -106,15 +105,16 @@ class ArchiveField:
         return self.default_value
 
     def __get__(self, obj, objtype=None):
-        cached_value = getattr(obj, "__archive_field_cached_value", None)
+        cached_value = getattr(obj, self.cached_value_property_name, None)
         if cached_value:
             return cached_value
         db_field = getattr(obj, self.db_field_name)
         if db_field is not None:
             value = self.rehydrate_fn(obj, db_field)
-            setattr(obj, "__archive_field_cached_value", value)
-            return value
-        return self._get_value_from_archive(obj)
+        else:
+            value = self._get_value_from_archive(obj)
+        setattr(obj, self.cached_value_property_name, value)
+        return value
 
     def __set__(self, obj, value):
         # Set the new value
@@ -137,4 +137,4 @@ class ArchiveField:
             setattr(obj, self.db_field_name, None)
         else:
             setattr(obj, self.db_field_name, value)
-        setattr(obj, "__archive_field_cached_value", value)
+        setattr(obj, self.cached_value_property_name, value)
