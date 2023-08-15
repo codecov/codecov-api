652fdbc6730cfdf82d0b9a430f47ac11444f08d7
Gguidini <giovanni@codecov.io>
2023-08-15 10:47:32 -0300

    fix migration order

--- core/migrations/0033_alter_pull_flare_rename_flare_pull__flare_and_more.py
+++ core/migrations/0033_alter_pull_flare_rename_flare_pull__flare_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0031_auto_20230731_1627"),
+        ("core", "0032_auto_20230731_1641"),
     ]
 
     # BEGIN;
