c2412ad1f65477e0ce815ca6d0d9668495d929f3
joseph-sentry <joseph.sawaya@sentry.io>
2023-08-28 14:17:53 -0400

    Add RiskyAlterField to utils/migrations
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- utils/migrations.py
+++ utils/migrations.py
@@ -24,6 +24,20 @@ class RiskyAddField(migrations.AddField):
         super().database_backwards(app_label, schema_editor, from_state, to_state)
 
 
+class RiskyAlterField(migrations.AlterField):
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        if settings.SKIP_RISKY_MIGRATION_STEPS:
+            return
+
+        super().database_forwards(app_label, schema_editor, from_state, to_state)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        if settings.SKIP_RISKY_MIGRATION_STEPS:
+            return
+
+        super().database_backwards(app_label, schema_editor, from_state, to_state)
+
+
 class RiskyRemoveField(migrations.RemoveField):
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if settings.SKIP_RISKY_MIGRATION_STEPS:
