3aca97919f76e2880341b6c2b32df8b976d6c4d2
Trent Schmidt <trent@codecov.io>
2023-09-08 14:48:02 -0400

    Don't compile since source is available (#106)
    * Don't compile since source is available
    
    * Fix config error
    
    * Readd push to reqs and remove unused local enterprise dockerfile
    
    * Fixing release version var and removing unneeded enterprise config.py
    
    * Fix staging upload?
    
    * Fix staging upload?
    
    * Fix typo in token

--- enterprise/hooks/hook-core.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-
-datas = collect_data_files(
-    "core", include_py_files=True, subdir="migrations"
-) + collect_data_files("legacy_migrations", include_py_files=True, subdir="migrations")
