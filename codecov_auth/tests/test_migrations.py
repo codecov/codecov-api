789dc719dd975fb0e4f6033034e1767e224b68b2
Jerry Feng <jerry.feng@sentry.io>
2023-09-19 16:21:59 -0400

    fix: Remove duplicate values in owners.admins and fix adding dupes in the future (#193)

---
+++ codecov_auth/tests/test_migrations.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from utils.test_utils import TestMigrations
+
+
+class Migration0042Test(TestMigrations):
+
+    migrate_from = "0041_auto_20230918_1825"
+    migrate_to = "0042_dedupe_owner_admin_values"
+
+    def setUpBeforeMigration(self, apps):
+        owners = apps.get_model("codecov_auth", "Owner")
+
+        self.owner_no_dupes = owners.objects.create(
+            ownerid=1,
+            service_id=1,
+            service="github",
+            admins=[1, 2, 3],
+        )
+
+        self.owner_null_admins = owners.objects.create(
+            ownerid=2,
+            service_id=2,
+            service="github",
+            admins=None,
+        )
+
+        self.owner_no_admins = owners.objects.create(
+            ownerid=3,
+            service_id=3,
+            service="github",
+            admins=[],
+        )
+
+        self.owner_one_dupe = owners.objects.create(
+            ownerid=4,
+            service_id=4,
+            service="github",
+            admins=[1, 1],
+        )
+
+        self.owner_multi_dupe = owners.objects.create(
+            ownerid=5,
+            service_id=5,
+            service="github",
+            admins=[1, 1, 2, 3, 3, 3, 4],
+        )
+
+        self.owner_multi_dupe_ordering = owners.objects.create(
+            ownerid=6,
+            service_id=6,
+            service="github",
+            admins=[3, 2, 1, 2, 3],
+        )
+
+    def test_admins_deduped(self):
+        owners = self.apps.get_model("codecov_auth", "Owner")
+
+        owner = owners.objects.get(ownerid=self.owner_no_dupes.ownerid)
+        assert owner.admins == [1, 2, 3]
+
+        owner = owners.objects.get(ownerid=self.owner_null_admins.ownerid)
+        assert owner.admins == []
+
+        owner = owners.objects.get(ownerid=self.owner_no_admins.ownerid)
+        assert owner.admins == []
+
+        owner = owners.objects.get(ownerid=self.owner_one_dupe.ownerid)
+        assert owner.admins == [1]
+
+        owner = owners.objects.get(ownerid=self.owner_multi_dupe.ownerid)
+        assert owner.admins == [1, 2, 3, 4]
+
+        owner = owners.objects.get(ownerid=self.owner_multi_dupe_ordering.ownerid)
+        assert owner.admins == [3, 2, 1]
--- utils/test_utils.py
+++ utils/test_utils.py
@@ -28,3 +28,74 @@ class Client(ClientMixin, DjangoClient):
 
 class APIClient(ClientMixin, DjangoAPIClient):
     pass
+
+
+from django.apps import apps
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
+from django.test import TestCase
+from django.test.client import Client as DjangoClient
+from rest_framework.test import APIClient as DjangoAPIClient
+
+from codecov_auth.models import Owner
+
+
+class BaseTestCase(object):
+    pass
+
+
+class ClientMixin:
+    def force_login_owner(self, owner: Owner):
+        self.force_login(user=owner.user)
+        session = self.session
+        session["current_owner_id"] = owner.pk
+        session.save()
+
+    def logout(self):
+        session = self.session
+        session["current_owner_id"] = None
+        session.save()
+        super().logout()
+
+
+class Client(ClientMixin, DjangoClient):
+    pass
+
+
+class APIClient(ClientMixin, DjangoAPIClient):
+    pass
+
+
+class TestMigrations(TestCase):
+    @property
+    def app(self):
+        return apps.get_containing_app_config(type(self).__module__).name
+
+    migrate_from = None
+    migrate_to = None
+
+    def setUp(self):
+        assert (
+            self.migrate_from and self.migrate_to
+        ), "TestCase '{}' must define migrate_from and migrate_to properties".format(
+            type(self).__name__
+        )
+        self.migrate_from = [(self.app, self.migrate_from)]
+        self.migrate_to = [(self.app, self.migrate_to)]
+        executor = MigrationExecutor(connection)
+        old_apps = executor.loader.project_state(self.migrate_from).apps
+
+        # Reverse to the original migration
+        executor.migrate(self.migrate_from)
+
+        self.setUpBeforeMigration(old_apps)
+
+        # Run the migration to test
+        executor = MigrationExecutor(connection)
+        executor.loader.build_graph()  # reload.
+        executor.migrate(self.migrate_to)
+
+        self.apps = executor.loader.project_state(self.migrate_to).apps
+
+    def setUpBeforeMigration(self, apps):
+        pass
