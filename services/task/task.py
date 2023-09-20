775491dd4c3e4fac4359c4bbacdedbf0cae41c92
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-20 17:15:04 -0400

    Add send email task to task service
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- services/task/task.py
+++ services/task/task.py
@@ -367,3 +367,15 @@ class TaskService(object):
                 report_code=report_code,
             ),
         )
+
+    def send_email(self, ownerid, template_name, from_addr, subject, **kwargs):
+        self._create_signature(
+            "app.tasks.send_email.SendEmail",
+            kwargs=dict(
+                ownerid=ownerid,
+                template_name=template_name,
+                from_addr=from_addr,
+                subject=subject,
+                **kwargs,
+            ),
+        ).apply_async()
