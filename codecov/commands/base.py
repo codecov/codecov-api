2cb2bd2cb172ac872e14628a4284aeb5a07edc03
Rula Abu Hasna <rula@codecov.io>
2023-10-20 17:31:59 +0200

    handle current user in base.py

--- codecov/commands/base.py
+++ codecov/commands/base.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.models import AnonymousUser
 
 from codecov.commands.exceptions import MissingService
-from codecov_auth.models import Owner
+from codecov_auth.models import Owner, User
 
 
 class BaseCommand:
-    def __init__(self, current_owner: Owner, service: str):
+    def __init__(
+        self, current_owner: Owner, service: str, current_user: User = AnonymousUser()
+    ):
+        self.current_user = current_user
         self.current_owner = current_owner
         self.service = service
         self.executor = None
 
     def get_interactor(self, InteractorKlass):
-        return InteractorKlass(self.current_owner, self.service)
+        return InteractorKlass(
+            current_owner=self.current_owner,
+            service=self.service,
+            current_user=self.current_user,
+        )
 
     def get_command(self, namespace):
         """
@@ -29,13 +36,15 @@ class BaseCommand:
 class BaseInteractor:
     requires_service = True
 
-    def __init__(self, current_owner: Owner, service: str):
+    def __init__(
+        self, current_owner: Owner, service: str, current_user: User = AnonymousUser()
+    ):
+        self.current_user = current_user
         self.current_owner = current_owner
         self.service = service
 
         if not self.service and self.requires_service:
             raise MissingService()
 
-        self.current_user = AnonymousUser()
-        if self.current_owner:
-            self.current_user = self.current_owner.user
+        if self.current_owner and not self.current_user.is_authenticated:
+            self.current_user = self.current_owner
--- codecov_auth/commands/owner/interactors/tests/test_save_terms_agreement.py
+++ codecov_auth/commands/owner/interactors/tests/test_save_terms_agreement.py
@@ -1,86 +1,72 @@
 import pytest
 from asgiref.sync import async_to_sync
+from django.contrib.auth.models import AnonymousUser
 from django.test import TransactionTestCase
 from django.utils import timezone
 from freezegun import freeze_time
+from freezegun.api import FakeDatetime
 
-from codecov.commands.exceptions import ValidationError
-from codecov_auth.models import User
-from codecov_auth.tests.factories import OwnerFactory
+from codecov.commands.exceptions import Unauthenticated, ValidationError
+from codecov_auth.tests.factories import UserFactory
 
 from ..save_terms_agreement import SaveTermsAgreementInteractor
 
 
 class UpdateSaveTermsAgreementInteractorTest(TransactionTestCase):
     def setUp(self):
-        self.current_owner = OwnerFactory(
-            username="random-user-123",
-            service="github",
-            business_email="asdfasdfa@gmail.com",
-        )
+        self.current_user = UserFactory(name="codecov-user")
+        self.updated_at = FakeDatetime(2022, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
 
     @async_to_sync
     def execute(
-        self, current_owner, input={"businessEmail": None, "termsAgreement": False}
+        self, current_user, input={"businessEmail": None, "termsAgreement": False}
     ):
-        return SaveTermsAgreementInteractor(current_owner, "github").execute(
+        return SaveTermsAgreementInteractor(None, "github", current_user).execute(
             input=input,
         )
 
     @freeze_time("2022-01-01T00:00:00")
     def test_update_user_when_agreement_is_false(self):
-        self.execute(current_owner=self.current_owner, input={"termsAgreement": False})
-        before_refresh_business_email = self.current_owner.business_email
+        self.execute(current_user=self.current_user, input={"termsAgreement": False})
+        before_refresh_business_email = self.current_user.email
 
-        user: User = User.objects.filter(id=self.current_owner.user_id).first()
-        assert user.terms_agreement == False
-        assert user.terms_agreement_at == timezone.datetime(2022, 1, 1)
+        assert self.current_user.terms_agreement == False
+        assert self.current_user.terms_agreement_at == self.updated_at
 
-        self.current_owner.refresh_from_db()
-        self.current_owner.business_email == before_refresh_business_email
+        self.current_user.refresh_from_db()
+        self.current_user.email == before_refresh_business_email
 
-    @freeze_time("2022-01-02T00:00:00")
+    @freeze_time("2022-01-01T00:00:00")
     def test_update_user_when_agreement_is_true(self):
-        self.execute(current_owner=self.current_owner, input={"termsAgreement": True})
-        before_refresh_business_email = self.current_owner.business_email
+        self.execute(current_user=self.current_user, input={"termsAgreement": True})
+        before_refresh_business_email = self.current_user.email
 
-        user: User = User.objects.filter(id=self.current_owner.user_id).first()
-        assert user.terms_agreement == True
-        assert user.terms_agreement_at == timezone.datetime(2022, 1, 2)
+        assert self.current_user.terms_agreement == True
+        assert self.current_user.terms_agreement_at == self.updated_at
 
-        self.current_owner.refresh_from_db()
-        self.current_owner.business_email == before_refresh_business_email
+        self.current_user.refresh_from_db()
+        self.current_user.email == before_refresh_business_email
 
-    @freeze_time("2022-01-03T00:00:00")
+    @freeze_time("2022-01-01T00:00:00")
     def test_update_owner_and_user_when_email_is_not_empty(self):
         self.execute(
-            current_owner=self.current_owner,
+            current_user=self.current_user,
             input={"businessEmail": "something@email.com", "termsAgreement": True},
         )
 
-        user: User = User.objects.filter(id=self.current_owner.user_id).first()
-        assert user.terms_agreement == True
-        assert user.terms_agreement_at == timezone.datetime(2022, 1, 3)
+        assert self.current_user.terms_agreement == True
+        assert self.current_user.terms_agreement_at == self.updated_at
 
-        self.current_owner.refresh_from_db()
-        self.current_owner.business_email == "something@email.com"
+        self.current_user.refresh_from_db()
+        self.current_user.email == "something@email.com"
 
     def test_validation_error_when_terms_is_none(self):
         with pytest.raises(ValidationError):
-            self.execute(
-                current_owner=self.current_owner, input={"termsAgreement": None}
-            )
+            self.execute(current_user=self.current_user, input={"termsAgreement": None})
 
-    def test_owner_has_no_user(self):
-        owner_without_user = OwnerFactory(
-            username="random-user-123",
-            service="github",
-            business_email="asdfasdfa@gmail.com",
-            user=None,
-        )
-        with pytest.raises(ValidationError) as e:
+    def test_user_is_not_authenticated(self):
+        with pytest.raises(Unauthenticated) as e:
             self.execute(
-                current_owner=owner_without_user,
+                current_user=AnonymousUser(),
                 input={"businessEmail": "something@email.com", "termsAgreement": True},
             )
-        assert str(e.value) == "Owner does not have an associated user"
