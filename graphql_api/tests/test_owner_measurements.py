206841a724c118fe7272487d27d72505db93b832
JerrySentry <jerry.feng@sentry.io>
2023-11-09 09:49:46 -0500

    fix: Add isPublic filter to measurements filter

--- graphql_api/tests/test_owner_measurements.py
+++ graphql_api/tests/test_owner_measurements.py
@@ -44,7 +44,7 @@ class TestOwnerMeasurements(TransactionTestCase, GraphQLTestHelper):
         self.repo2 = RepositoryFactory(
             name="test-repo2",
             author=self.org,
-            private=True,
+            private=False,
         )
         self.owner = OwnerFactory(permission=[self.repo1.pk, self.repo2.pk])
 
@@ -131,3 +131,58 @@ class TestOwnerMeasurements(TransactionTestCase, GraphQLTestHelper):
             start_date=datetime(2022, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
             end_date=datetime(2022, 1, 3, 0, 0, 0, tzinfo=timezone.utc),
         )
+
+    @override_settings(TIMESERIES_ENABLED=True)
+    def test_repository_filtering_by_public_private(
+        self, owner_coverage_measurements_with_fallback
+    ):
+        owner_coverage_measurements_with_fallback.return_value = []
+        query = f"""
+            query Measurements($isPublic: Boolean) {{
+                owner(username: "{self.org.username}") {{
+                    measurements(
+                        interval: INTERVAL_1_DAY
+                        isPublic: $isPublic
+                    ) {{
+                        timestamp
+                    }}
+                }}
+            }}
+        """
+
+        self.gql_request(query, owner=self.owner, variables={"isPublic": False})[
+            "owner"
+        ]["measurements"]
+        params = owner_coverage_measurements_with_fallback.call_args.args
+        # Check that the call is using only repo_ids of the private repo
+        assert params[1] == [self.repo1.pk]
+
+        self.gql_request(query, owner=self.owner, variables={"isPublic": True})[
+            "owner"
+        ]["measurements"]
+        params = owner_coverage_measurements_with_fallback.call_args.args
+        # Check that the call is using only repo_ids of the public repo
+        assert params[1] == [self.repo2.pk]
+
+        self.gql_request(query, owner=self.owner, variables={"isPublic": None})[
+            "owner"
+        ]["measurements"]
+        params = owner_coverage_measurements_with_fallback.call_args.args
+        # Check that the call is using both private and public repos
+        assert set(params[1]) == set([self.repo1.pk, self.repo2.pk])
+
+        query = f"""
+            query Measurements {{
+                owner(username: "{self.org.username}") {{
+                    measurements(
+                        interval: INTERVAL_1_DAY
+                    ) {{
+                        timestamp
+                    }}
+                }}
+            }}
+        """
+        self.gql_request(query, owner=self.owner)["owner"]["measurements"]
+        params = owner_coverage_measurements_with_fallback.call_args.args
+        # Check that the call is using both private and public repos
+        assert set(params[1]) == set([self.repo1.pk, self.repo2.pk])
