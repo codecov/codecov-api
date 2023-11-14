00c183135ed52c8b17ad07753f9ba0aeb6cc35b5
Rylee Fox <rylee.fox@sentry.io>
2023-11-14 15:33:31 -0800

    feat(logging): change access logging format to JSON

--- codecov/settings_base.py
+++ codecov/settings_base.py
@@ -329,6 +329,11 @@ LOGGING = {
             "format": "%(message)s %(asctime)s %(name)s %(levelname)s %(lineno)s %(pathname)s %(funcName)s %(threadName)s",
             "class": "utils.logging_configuration.CustomDatadogJsonFormatter",
         },
+        "gunicorn_json": {
+            "class": "utils.logging_configuration.CustomGunicornLogFormatter",
+            "datefmt": '%Y-%m-%dT%H:%M:%S%z',
+            "format": '%(h)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"',
+        }
     },
     "root": {"handlers": ["default"], "level": "INFO", "propagate": True},
     "handlers": {
@@ -339,9 +344,22 @@ LOGGING = {
             else "json",
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stdout",  # Default is stderr
+        },
+        "json-gunicorn-console": {
+            "level": "INFO",
+            "formatter": "gunicorn_json",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # Default is stderr
         }
     },
-    "loggers": {},
+    "loggers": {
+        "gunicorn.access": {
+            "level": "INFO",
+            "handlers": [
+                "json-gunicorn-console"
+            ]
+        }
+    },
 }
 
 MINIO_ACCESS_KEY = get_config("services", "minio", "access_key_id")
--- utils/logging_configuration.py
+++ utils/logging_configuration.py
@@ -1,4 +1,4 @@
-from pythonjsonlogger.jsonlogger import JsonFormatter
+from pythonjsonlogger.jsonlogger import JsonFormatter, merge_record_extra
 from sentry_sdk import Hub
 
 
@@ -36,3 +36,23 @@ class CustomDatadogJsonFormatter(BaseLogger):
         span = Hub.current.scope.span
         if span and span.trace_id:
             log_record["sentry_trace_id"] = span.trace_id
+
+class CustomGunicornLogFormatter(JsonFormatter):
+    rename_fields = {
+        "levelname": "level",
+        "r": "request",
+        "a": "useragent",
+        "f": "referer", # the biggest concern in my mind: should this be correctly spelled historically (one r) or correctly spelled linguistically (two r)
+        "b": "response_length",
+        "h": "remote_address",
+        "t": "request_time",
+        "s": "status_code",
+    }
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomGunicornLogFormatter, self).add_fields(log_record, record, message_dict)
+        for field in self._required_fields:
+            if field in self.rename_fields:
+                log_record[self.rename_fields[field]] = record.args.get(field)
+                del log_record[field]
+            else:
+                log_record[field] = record.args.get(field)
