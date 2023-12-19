f7e06fd526abf2d1923a6911488cc597860329e5
JerrySentry <jerry.feng@sentry.io>
2023-12-19 17:12:30 -0500

    fix: allow self hosted admins to have parity with cloud admins (for main)

--- graphql_api/tests/mutation/test_delete_flag.py
+++ graphql_api/tests/mutation/test_delete_flag.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from django.test import TransactionTestCase
+from django.test import TransactionTestCase, override_settings
 
 from codecov.commands.exceptions import (
     NotFound,
@@ -135,6 +135,62 @@ class DeleteFlagTest(GraphQLTestHelper, TransactionTestCase):
                 "error": {
                     "__typename": "NotFoundError",
                     "message": "Cant find the requested resource",
+                }
+            }
+        }
+
+    @override_settings(IS_ENTERPRISE=True)
+    @patch("services.self_hosted.get_config")
+    @patch("core.commands.flag.interactors.delete_flag.DeleteFlagInteractor.execute")
+    def test_delete_flag_self_hosted_admin(self, execute_mock, get_config_mock):
+        get_config_mock.return_value = [
+            {"service": "github", "username": "test-owner"},
+        ]
+
+        data = self.gql_request(
+            query,
+            variables={
+                "input": {
+                    "ownerUsername": "test-owner",
+                    "repoName": "test-repo",
+                    "flagName": "test-flag",
+                }
+            },
+        )
+
+        assert data == {"deleteFlag": None}
+
+        assert execute_mock.called_once_with(
+            owner_username="test-owner",
+            repo_name="test-repo",
+            flag_name="test-flag",
+        )
+
+    @override_settings(IS_ENTERPRISE=True)
+    @patch("services.self_hosted.get_config")
+    @patch("core.commands.flag.interactors.delete_flag.DeleteFlagInteractor.execute")
+    def test_delete_flag_self_hosted_not_admin(self, execute_mock, get_config_mock):
+        execute_mock.side_effect = Unauthorized()
+        get_config_mock.return_value = [
+            {"service": "github", "username": "someone-else"},
+        ]
+
+        data = self.gql_request(
+            query,
+            variables={
+                "input": {
+                    "ownerUsername": "test-owner",
+                    "repoName": "test-repo",
+                    "flagName": "test-flag",
+                }
+            },
+        )
+
+        assert data == {
+            "deleteFlag": {
+                "error": {
+                    "__typename": "UnauthorizedError",
+                    "message": "You are not authorized",
                 }
             }
         }
