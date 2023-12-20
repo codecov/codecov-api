4cb9ec32091dc0c5fa015670883e511cd34164fb
trent-codecov <trent@codecov.io>
2023-12-20 15:10:55 -0500

    Only cache when we pull from db. Preventing infinite ttl edge case

--- utils/uploads_used.py
+++ utils/uploads_used.py
@@ -19,9 +19,10 @@ def get_uploads_used(redis, plan_service, limit, owner):
         uploads_used = redis.get(cache_key)
         if uploads_used is None:
             uploads_used = query_uploads_used(plan_service, limit, owner)
+            redis.set(cache_key, uploads_used, ex=cache_time)
         else:
             uploads_used = int(uploads_used)
-        redis.set(cache_key, uploads_used, ex=cache_time)
+
     except OSError as e:
         log.warning(
             f"Error connecting to redis for rate limit check: {e}",
