0b82d71e112414a7bbd75097e468710cb05d7ec1
matt-codecov <137832199+matt-codecov@users.noreply.github.com>
2024-01-19 11:08:12 -0500

    fix: if upload moves commit to other branch, possibly update branch model head (#345)
    that PR updates commit.branch but it doesn't touch the Branch model. Branch has a head and base field, and we may want to update them when moving a commit from one branch to another
    
    as-is this PR will recompute the head of the destination branch, so if the commit being moved is newer than other commits on that branch, it'll become the new head. we may similarly want to fetch any branches that the commit being moved was the head of and recompute heads for those as well.

--- upload/helpers.py
+++ upload/helpers.py
@@ -18,7 +18,7 @@ from shared.reports.enums import UploadType
 from shared.torngit.exceptions import TorngitClientError, TorngitObjectNotFoundError
 
 from codecov_auth.models import Owner
-from core.models import Commit, CommitNotification, Pull, Repository
+from core.models import Branch, Commit, CommitNotification, Pull, Repository
 from plan.constants import USER_PLAN_REPRESENTATIONS
 from reports.models import CommitReport, ReportSession
 from services.analytics import AnalyticsService
@@ -440,19 +440,34 @@ def insert_commit(commitid, branch, pr, repository, owner, parent_commit_id=None
         },
     )
 
-    edited = False
+    edited = []
     if commit.state != "pending":
         commit.state = "pending"
-        edited = True
+        edited.append("state")
     if parent_commit_id and commit.parent_commit_id is None:
         commit.parent_commit_id = parent_commit_id
-        edited = True
+        edited.append("parent_commit_id")
     if branch and commit.branch != branch:
         # A branch head may have been moved; this allows commits to be "moved"
         commit.branch = branch
-        edited = True
+        edited.append("branch")
     if edited:
-        commit.save(update_fields=["parent_commit_id", "state", "branch"])
+        commit.save(update_fields=edited)
+
+    # Update Branch.head object for the new branch
+    # if the current commit is newer than the existing Branch.head commit
+    if "branch" in edited:
+        new_branch = Branch.objects.filter(
+            name=branch, repository_id=repository.repoid
+        ).first()
+        if new_branch:
+            new_branch_head = Commit.objects.filter(
+                commitid=new_branch.head,
+                repository_id=repository.repoid,
+            ).first()
+            if new_branch_head is None or new_branch_head.timestamp < commit.timestamp:
+                new_branch.head = commit.commitid
+                new_branch.save(update_fields=["head"])
     return commit
 
 
