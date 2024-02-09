34c5ee13702fd9603f4fca1112e22e7b1f7451ca
Rohit <rohit.vinnakota@sentry.io>
2024-02-09 14:26:43 -0500

    lint

--- codecov_auth/migrations/0049_ownerprofile_customer_intent.py
+++ codecov_auth/migrations/0049_ownerprofile_customer_intent.py
@@ -4,15 +4,16 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('codecov_auth', '0048_githubappinstallation'),
+        ("codecov_auth", "0048_githubappinstallation"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='ownerprofile',
-            name='customer_intent',
-            field=models.TextField(choices=[('BUSINESS', 'Business'), ('PERSONAL', 'Personal')], null=True),
+            model_name="ownerprofile",
+            name="customer_intent",
+            field=models.TextField(
+                choices=[("BUSINESS", "Business"), ("PERSONAL", "Personal")], null=True
+            ),
         ),
     ]
