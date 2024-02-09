bc375e022a5d64f9880368c84c3c90f56a94003f
joseph-sentry <joseph.sawaya@sentry.io>
2024-02-09 10:31:19 -0500

    fix(test_results): fix logs
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- upload/views/test_results.py
+++ upload/views/test_results.py
@@ -72,7 +72,7 @@ class TestResultsView(
 
         log.info(
             "Checking to see if user is in test results feature rollout",
-            extra=dict(ownerid=owner.ownerid, repoid=repo.repoid, author=repo.author),
+            extra=dict(repoid=repo.repoid, author=repo.author),
         )
 
         if not TEST_RESULTS_UPLOAD_FEATURE_BY_OWNER_SLUG.check_value(
@@ -80,15 +80,13 @@ class TestResultsView(
         ):
             log.warning(
                 "User is not included in rollout, exiting",
-                extra=dict(
-                    ownerid=owner.ownerid, repoid=repo.repoid, author=repo.author
-                ),
+                extra=dict(repoid=repo.repoid, author=repo.author),
             )
             raise PermissionDenied()
 
         log.info(
             "User is included in rollout, continuing",
-            extra=dict(ownerid=owner.ownerid, repoid=repo.repoid, author=repo.author),
+            extra=dict(repoid=repo.repoid, author=repo.author),
         )
 
         commit, _ = Commit.objects.get_or_create(
