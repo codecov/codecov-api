ea7b21edf5cf6870a47318147337f25c00ab8a61
JerrySentry <jerry.feng@sentry.io>
2024-02-14 21:28:51 -0500

    move BundleReport to base

--- graphql_api/types/bundle_analysis/__init__.py
+++ graphql_api/types/bundle_analysis/__init__.py
@@ -5,6 +5,7 @@ from .base import (
     bundle_data_bindable,
     bundle_load_time_bindable,
     bundle_module_bindable,
+    bundle_report_bindable,
     bundle_size_bindable,
 )
 from .comparison import (
@@ -15,7 +16,6 @@ from .comparison import (
 from .report import (
     bundle_analysis_report_bindable,
     bundle_analysis_report_result_bindable,
-    bundle_report_bindable,
 )
 
 bundle_analysis = ariadne_load_local_graphql(__file__, "base.graphql")
--- graphql_api/types/bundle_analysis/base.py
+++ graphql_api/types/bundle_analysis/base.py
@@ -1,7 +1,25 @@
-from ariadne import ObjectType, UnionType
+from ariadne import ObjectType
+
+from services.bundle_analysis import BundleReport
 
 bundle_size_bindable = ObjectType("BundleSize")
 bundle_load_time_bindable = ObjectType("BundleLoadTime")
 bundle_data_bindable = ObjectType("BundleData")
 bundle_module_bindable = ObjectType("BundleModule")
 bundle_asset_bindable = ObjectType("BundleAsset")
+bundle_report_bindable = ObjectType("BundleReport")
+
+
+@bundle_report_bindable.field("name")
+def resolve_name(bundle_report: BundleReport, info) -> str:
+    return bundle_report.bundle_name
+
+
+@bundle_report_bindable.field("sizeTotal")
+def resolve_size_total(bundle_report: BundleReport, info) -> int:
+    return bundle_report.size_total
+
+
+@bundle_report_bindable.field("loadTimeTotal")
+def resolve_load_time_total(bundle_report: BundleReport, info) -> float:
+    return bundle_report.load_time_total
