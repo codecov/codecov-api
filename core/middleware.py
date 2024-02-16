b52dc39b0458e1be46cd39af5e15e89afecdc311
Daniel Yu <daniel.yu@sentry.io>
2024-02-16 13:18:01 -0500

    add user-agent to labels + unit tests

--- core/middleware.py
+++ core/middleware.py
@@ -3,6 +3,11 @@ from typing import Optional
 from django.http import HttpRequest
 from django.urls import resolve
 from django.utils.deprecation import MiddlewareMixin
+from django_prometheus.middleware import (
+    Metrics,
+    PrometheusAfterMiddleware,
+    PrometheusBeforeMiddleware,
+)
 
 from utils.services import get_long_service_name
 
@@ -23,3 +28,59 @@ class ServiceMiddleware(MiddlewareMixin):
         if service:
             view_kwargs["service"] = service
         return None
+
+
+USER_AGENT_METRICS = [
+    "django_http_requests_unknown_latency_including_middlewares_total",
+    "django_http_requests_latency_seconds_by_view_method",
+    "django_http_requests_unknown_latency_total",
+    "django_http_ajax_requests_total",
+    "django_http_requests_total_by_method",
+    "django_http_requests_total_by_transport",
+    "django_http_requests_total_by_view_transport_method",
+    "django_http_requests_body_total_bytes",
+    "django_http_responses_total_by_templatename",
+    "django_http_responses_total_by_status",
+    "django_http_responses_total_by_status_view_method",
+    "django_http_responses_body_total_bytes",
+    "django_http_responses_total_by_charset",
+    "django_http_responses_streaming_total",
+    "django_http_exceptions_total_by_type",
+    "django_http_exceptions_total_by_view",
+]
+
+
+class CustomMetricsWithUA(Metrics):
+    """
+    Prometheus blah blah with user_agent header label
+    """
+
+    def register_metric(self, metric_cls, name, documentation, labelnames=(), **kwargs):
+        if name in USER_AGENT_METRICS:
+            labelnames = list(labelnames) + ["user_agent"]
+        return super().register_metric(
+            metric_cls, name, documentation, labelnames=labelnames, **kwargs
+        )
+
+
+class AppMetricsBeforeMiddlewareWithUA(PrometheusBeforeMiddleware):
+    """
+    Prometheus blah blah but with user_agent header label
+    """
+
+    metrics_cls = CustomMetricsWithUA
+
+
+class AppMetricsAfterMiddlewareWithUA(PrometheusAfterMiddleware):
+    """
+    Prometheus blah blah but with user_agent header label
+    """
+
+    metrics_cls = CustomMetricsWithUA
+
+    def label_metric(self, metric, request, response=None, **labels):
+        new_labels = labels
+        if metric._name in USER_AGENT_METRICS:
+            new_labels = {"user_agent": request.headers.get("User-Agent", "none")}
+            new_labels.update(labels)
+        return super().label_metric(metric, request, response=response, **new_labels)
