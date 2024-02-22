685aec656c8e2edcb0e60586d7d8a5bef17e4a63
Rohit <rohit.vinnakota@sentry.io>
2024-02-22 13:57:54 -0500

    add user check to customer intent resolver

--- graphql_api/tests/test_user.py
+++ graphql_api/tests/test_user.py
@@ -8,6 +8,22 @@ from codecov_auth.tests.factories import OwnerFactory
 
 from .helper import GraphQLTestHelper
 
+query = """
+    {
+        me {
+            user {
+                username
+                name
+                avatarUrl
+                student
+                studentCreatedAt
+                studentUpdatedAt
+                customerIntent
+            }
+        }
+    }
+    """
+
 
 @freeze_time("2023-06-19")
 class UserTestCase(GraphQLTestHelper, TransactionTestCase):
@@ -23,21 +39,18 @@ class UserTestCase(GraphQLTestHelper, TransactionTestCase):
             student_updated_at=timezone.now() + timedelta(days=1),
         )
 
+        self.no_subtype_user = OwnerFactory(
+            username="codecov-user",
+            name="codecov-name",
+            service="github",
+            service_id=self.service_id,
+            student=True,
+            student_created_at=timezone.now(),
+            student_updated_at=timezone.now() + timedelta(days=1),
+            user=None,
+        )
+
     def test_query_user_resolver(self):
-        query = """{
-            me {
-                user {
-                    username
-                    name
-                    avatarUrl
-                    student
-                    studentCreatedAt
-                    studentUpdatedAt
-                    customerIntent
-                }
-            }
-        }
-        """
         data = self.gql_request(query, owner=self.user)
         assert data["me"]["user"] == {
             "username": "codecov-user",
@@ -47,4 +60,16 @@ class UserTestCase(GraphQLTestHelper, TransactionTestCase):
             "studentCreatedAt": "2023-06-19T00:00:00",
             "studentUpdatedAt": "2023-06-20T00:00:00",
             "customerIntent": "Business",
+        }
+
+    def test_customer_intent_with_no_user_subtype(self):
+        data = self.gql_request(query, owner=self.no_subtype_user)
+        assert data["me"]["user"] == {
+            "username": "codecov-user",
+            "name": "codecov-name",
+            "avatarUrl": f"https://avatars0.githubusercontent.com/u/{self.service_id}?v=3&s=55",
+            "student": True,
+            "studentCreatedAt": "2023-06-19T00:00:00",
+            "studentUpdatedAt": "2023-06-20T00:00:00",
+            "customerIntent": None,
         }
