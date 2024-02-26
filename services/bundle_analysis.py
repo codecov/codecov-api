266b9e869ea3748ab9a88599be08948eaf15001d
JerrySentry <jerry.feng@sentry.io>
2024-02-26 09:39:18 -0500

    update file extension handling

--- services/bundle_analysis.py
+++ services/bundle_analysis.py
@@ -49,16 +49,19 @@ def load_time_conversion(size):
 def get_extension(filename: str) -> str:
     """
     Gets the file extension of the file without the dot
-    also handling cases where ?* exists after the . (eg production.js?exports)
     """
+    # At times file can be something like './index.js + 12 modules', only keep the real filepath
+    filename = filename.split(" ")[0]
+    # Retrieve the file extension with the dot
     _, file_extension = os.path.splitext(filename)
+    # Return empty string if file has no extension
     if not file_extension or file_extension[0] != ".":
         return file_extension
-
+    # Remove the dot in the extension
     file_extension = file_extension[1:]
-
+    # At times file can be something like './index.js?module', remove the ?
     if "?" in file_extension:
-        return file_extension[: file_extension.rfind("?")]
+        file_extension = file_extension[: file_extension.rfind("?")]
 
     return file_extension
 
