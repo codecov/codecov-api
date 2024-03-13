0b0e138918e2bfa8e25568429e170dcfe7c08975
Daniel Yu <daniel.yu@sentry.io>
2024-03-13 14:41:35 -0400

    test

---
+++ parallel.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+codecovcli-helper do-upload --dir testcoverage/a & 
+codecovcli-helper do-upload --dir testcoverage/b &
+codecovcli-helper do-upload --dir testcoverage/c &
+codecovcli-helper do-upload --dir testcoverage/d &
+codecovcli-helper do-upload --dir testcoverage/e
