344d6020b397580e1733c6479b503d347441d30d
JerrySentry <jerry.feng@sentry.io>
2024-03-22 12:04:36 -0400

    add components measurements retrieval

--- graphql_api/types/component/__init__.py
+++ graphql_api/types/component/__init__.py
@@ -1,5 +1,9 @@
 from graphql_api.helpers.ariadne import ariadne_load_local_graphql
+from graphql_api.helpers.connection import build_connection_graphql
 
 from .component import component_bindable
 
 component = ariadne_load_local_graphql(__file__, "component.graphql")
+component += build_connection_graphql(
+    "ComponentMeasurementsConnection", "ComponentMeasurements"
+)
