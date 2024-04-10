2d6d5a6ec039d49b88044189e8611bf8c5444677
Rohit <rohit.vinnakota@sentry.io>
2024-04-10 15:37:00 -0400

    Add filter check to coverage tree

--- api/internal/coverage/views.py
+++ api/internal/coverage/views.py
@@ -44,7 +44,10 @@ class CoverageViewSet(viewsets.ViewSet, RepoPropertyMixin):
         url_path="tree",
     )
     def tree(self, request, *args, **kwargs):
+        flags = request.query_params.get("flags")  # Optional flags parameter
+        components = request.query_params.get("components")  # Optional components parameter
         report = self.get_object()
-        paths = ReportPaths(report)
+
+        paths = ReportPaths(report = report, filter_flags=flags, filter_paths=components)
         serializer = TreeSerializer(paths.single_directory(), many=True)
         return Response(serializer.data)
