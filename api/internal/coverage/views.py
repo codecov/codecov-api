f24f52f45cfafdfe2ebb09cf679aa9d0a290a8f2
Rohit <rohit.vinnakota@sentry.io>
2024-04-15 13:34:14 -0400

    refactor components

--- api/internal/coverage/views.py
+++ api/internal/coverage/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from api.shared.mixins import RepoPropertyMixin
 from api.shared.permissions import RepositoryArtifactPermissions
 from api.shared.report.serializers import TreeSerializer
+import services.components as components_service
 from services.path import ReportPaths
 
 
@@ -20,7 +20,7 @@ class CoverageViewSet(viewsets.ViewSet, RepoPropertyMixin):
             branch = self.repo.branches.filter(name=branch_name).first()
             if branch is None:
                 raise NotFound(
-                    f"The branch '{branch_name}' in not in our records. Please provide a valid branch name.",
+                    f"The branch '{branch_name}' is not in our records. Please provide a valid branch name.",
                     404,
                 )
             commit_sha = branch.head
@@ -28,7 +28,7 @@ class CoverageViewSet(viewsets.ViewSet, RepoPropertyMixin):
         commit = self.repo.commits.filter(commitid=commit_sha).first()
         if commit is None:
             raise NotFound(
-                f"The commit {commit_sha} is not in our records. Please specify valid commit.",
+                f"The commit {commit_sha} is not in our records. Please specify a valid commit.",
                 404,
             )
 
@@ -36,18 +36,28 @@ class CoverageViewSet(viewsets.ViewSet, RepoPropertyMixin):
         if report is None:
             raise NotFound(f"Coverage report for {commit_sha} not found")
 
-        return report
+        components = self.request.query_params.getlist("components")
+        component_paths = []
+        if components:
+            all_components = components_service.commit_components(
+                commit, self.request.user
+            )
+            filtered_components = components_service.filter_components_by_name(
+                all_components, components
+            )
+            for component in filtered_components:
+                component_paths.extend(component.paths)
 
-    @action(
-        detail=False,
-        methods=["get"],
-        url_path="tree",
-    )
+        flags = self.request.query_params.getlist("flags")
+
+        paths = ReportPaths(
+            report=report, filter_flags=flags, filter_paths=component_paths
+        )
+
+        return paths
+
+    @action(detail=False, methods=["get"], url_path="tree")
     def tree(self, request, *args, **kwargs):
-        flags = request.query_params.get("flags")
-        components = request.query_params.get("components")
-        report = self.get_object()
-
-        paths = ReportPaths(report=report, filter_flags=flags, filter_paths=components)
+        paths = self.get_object()
         serializer = TreeSerializer(paths.single_directory(), many=True)
         return Response(serializer.data)
