50f1360bdb31402a42288c52397756fed815e1a4
Rohit <rohit.vinnakota@sentry.io>
2024-05-13 09:47:49 -0600

    Update typing

--- graphql_api/types/self_hosted_settings/self_hosted_settings.py
+++ graphql_api/types/self_hosted_settings/self_hosted_settings.py
@@ -2,13 +2,15 @@ from typing import Optional
 
 from ariadne import ObjectType
 from django.conf import settings
+from graphql.type.definition import GraphQLResolveInfo
+
 import services.self_hosted as self_hosted
 
 self_hosted_settings_bindable = ObjectType("SelfHostedSettings")
 
 
 @self_hosted_settings_bindable.field("planAutoActivate")
-def resolve_plan_auto_activate(_, info) -> Optional[bool]:
+def resolve_plan_auto_activate(_, info: GraphQLResolveInfo) -> Optional[bool]:
     if not settings.IS_ENTERPRISE:
         return None
 
@@ -16,7 +18,7 @@ def resolve_plan_auto_activate(_, info) -> Optional[bool]:
 
 
 @self_hosted_settings_bindable.field("seatsLimit")
-def resolve_expiration_date(_, info) -> Optional[int]:
+def resolve_expiration_date(_, info: GraphQLResolveInfo) -> Optional[int]:
     if not settings.IS_ENTERPRISE:
         return None
 
@@ -24,7 +26,7 @@ def resolve_expiration_date(_, info) -> Optional[int]:
 
 
 @self_hosted_settings_bindable.field("seatsUsed")
-def resolve_seats_used(_, info) -> Optional[int]:
+def resolve_seats_used(_, info: GraphQLResolveInfo) -> Optional[int]:
     if not settings.IS_ENTERPRISE:
         return None
 
