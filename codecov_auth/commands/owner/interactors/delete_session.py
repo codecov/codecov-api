451d3dec6150b1f30fb12cc819435eb0783cd1db
JerrySentry <jerry.feng@sentry.io>
2024-05-13 15:33:33 -0400

    handle non existent sessions

--- codecov_auth/commands/owner/interactors/delete_session.py
+++ codecov_auth/commands/owner/interactors/delete_session.py
@@ -14,14 +14,17 @@ class DeleteSessionInteractor(BaseInteractor):
     @sync_to_async
     def execute(self, sessionid: int) -> None:
         self.validate()
-        session_to_delete = Session.objects.get(sessionid=sessionid)
 
-        django_session_to_delete = DjangoSession.objects.get(
-            session_key=session_to_delete.login_session_id
-        )
-        user_id_to_delete = int(
-            django_session_to_delete.get_decoded().get("_auth_user_id", "0")
-        )
+        try:
+            session_to_delete = Session.objects.get(sessionid=sessionid)
+            django_session_to_delete = DjangoSession.objects.get(
+                session_key=session_to_delete.login_session_id
+            )
+            user_id_to_delete = int(
+                django_session_to_delete.get_decoded().get("_auth_user_id", "0")
+            )
 
-        if user_id_to_delete == self.current_user.id:
-            django_session_to_delete.delete()
+            if user_id_to_delete == self.current_user.id:
+                django_session_to_delete.delete()
+        except (Session.DoesNotExist, DjangoSession.DoesNotExist):
+            pass
