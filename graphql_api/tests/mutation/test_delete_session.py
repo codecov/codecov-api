dc56e7c47fcd891ff8c7379f9e054e705352509b
JerrySentry <jerry.feng@sentry.io>
2024-05-13 15:53:30 -0400

    add another test

--- graphql_api/tests/mutation/test_delete_session.py
+++ graphql_api/tests/mutation/test_delete_session.py
@@ -54,3 +54,31 @@ class DeleteSessionTestCase(GraphQLTestHelper, TransactionTestCase):
             query, owner=self.owner, variables={"input": {"sessionid": sessionid}}
         )
         assert len(Session.objects.filter(sessionid=sessionid)) == 0
+
+    def test_when_authenticated_session_not_valid(self):
+        user = UserFactory()
+        self.owner.user = user
+        self.owner.save()
+
+        login_query = "{ me { user { username }} }"
+        self.gql_request(login_query, owner=self.owner)
+
+        user = auth.get_user(self.client)
+        assert user.is_authenticated
+
+        django_session_id = DjangoSession.objects.all()
+        assert len(django_session_id) == 1
+
+        django_session_id = django_session_id[0]
+
+        Session.objects.create(
+            lastseen=timezone.now(),
+            useragent="Firefox",
+            ip="0.0.0.0",
+            login_session=django_session_id,
+            type=Session.SessionType.LOGIN,
+            owner=self.owner,
+        ).sessionid
+
+        self.gql_request(query, owner=self.owner, variables={"input": {"sessionid": 0}})
+        assert len(Session.objects.all()) == 1
