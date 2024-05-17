5f9cf60e803f8ac5259095b14bef327cc9aab81b
Rohit <rohit.vinnakota@sentry.io>
2024-05-17 11:15:19 -0600

    Add tests

--- graphql_api/types/mutation/erase_repository/erase_repository.py
+++ graphql_api/types/mutation/erase_repository/erase_repository.py
@@ -1,21 +1,16 @@
 from ariadne import UnionType, convert_kwargs_to_snake_case
-
-from graphql_api.helpers.mutation import (
-    resolve_union_error_type,
-    wrap_error_handling_mutation,
-    require_authenticated,
-)
+from graphql_api.helpers.mutation import (require_authenticated,
+                                          resolve_union_error_type,
+                                          wrap_error_handling_mutation)
 
 
 @wrap_error_handling_mutation
-@convert_kwargs_to_snake_case
 @require_authenticated
 async def resolve_erase_repository(_, info, input):
     command = info.context["executor"].get_command("repository")
     current_owner = info.context["request"].current_owner
-    repo_name = (input.get("repo_name"),)
-
-    await command.erase_repository(repo=repo_name, owner=current_owner)
+    repo_name = input.get("repoName")
+    await command.erase_repository(repo_name=repo_name, owner=current_owner)
 
 
 error_erase_repository = UnionType("EraseRepositoryError")
