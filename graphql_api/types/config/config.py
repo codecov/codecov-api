08d6b30c2ab254bbf2a85a8b9b96761ea4bda521
Rohit <rohit.vinnakota@sentry.io>
2024-05-22 12:05:10 -0600

    fix sort

--- graphql_api/types/config/config.py
+++ graphql_api/types/config/config.py
@@ -1,10 +1,7 @@
 from distutils.util import strtobool
 from typing import List, Optional
 
-from typing import List
-
 from ariadne import ObjectType
-from distutils.util import strtobool
 from django.conf import settings
 from graphql.type.definition import GraphQLResolveInfo
 
