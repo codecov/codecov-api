d576bafe0b24da9b0334960d61e15461ec19333e
Rohit <rohit.vinnakota@sentry.io>
2024-05-22 11:46:17 -0600

    sort imports

--- core/commands/repository/repository.py
+++ core/commands/repository/repository.py
@@ -1,13 +1,11 @@
 from codecov.commands.base import BaseCommand
 from codecov_auth.models import Owner
-
-from .interactors.activate_measurements import ActivateMeasurementsInteractor
-from .interactors.erase_repository import EraseRepositoryInteractor
 from core.models import Repository
 from timeseries.models import MeasurementName
 
 from .interactors.activate_measurements import ActivateMeasurementsInteractor
 from .interactors.encode_secret_string import EncodeSecretStringInteractor
+from .interactors.erase_repository import EraseRepositoryInteractor
 from .interactors.fetch_repository import FetchRepositoryInteractor
 from .interactors.get_repository_token import GetRepositoryTokenInteractor
 from .interactors.get_upload_token import GetUploadTokenInteractor
@@ -42,7 +40,7 @@ class RepositoryCommands(BaseCommand):
 
     def erase_repository(self, repo_name: str, owner: Owner):
         return self.get_interactor(EraseRepositoryInteractor).execute(repo_name, owner)
-      
+
     def encode_secret_string(self, owner: Owner, repo: Repository, value: str):
         return self.get_interactor(EncodeSecretStringInteractor).execute(
             owner, repo, value
