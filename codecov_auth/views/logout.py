16bff5c0456b7e163d55421476caf84a330f261f
Spencer Murray <spencer.murray@sentry.io>
2024-05-27 12:32:20 -0400

    Type hint for kwargs also

--- codecov_auth/views/logout.py
+++ codecov_auth/views/logout.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 
 
 @api_view(["GET", "POST"])
-def logout_view(request: HttpRequest, **kwargs) -> HttpResponse:
+def logout_view(request: HttpRequest, **kwargs: str) -> HttpResponse:
+    print(kwargs)
     if request.method == "POST":
         response = Response(status=205)
     else:
