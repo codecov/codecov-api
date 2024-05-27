fc7f64106bccc86c3db3182eb158a78b8c1c7b8b
Spencer Murray <spencer.murray@sentry.io>
2024-05-27 10:54:21 -0400

    Allow logout via POST

--- codecov_auth/views/logout.py
+++ codecov_auth/views/logout.py
@@ -1,11 +1,19 @@
 from django.conf import settings
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET', 'POST'])
 def logout_view(request, **kwargs):
-    redirect_url = settings.CODECOV_DASHBOARD_URL
-    response = redirect(redirect_url)
+    if request.method == 'POST':
+        response = Response(status=205)
+    else:
+        # Preserving GET logouts until Gazebo is moved off of it.
+        redirect_url = settings.CODECOV_DASHBOARD_URL
+        response = redirect(redirect_url)
+
     logout(request)
     kwargs_cookie = dict(
         domain=settings.COOKIES_DOMAIN, samesite=settings.COOKIE_SAME_SITE
@@ -16,4 +24,5 @@ def logout_view(request, **kwargs):
     # So we need delete in both samesite Strict / Lax for a little while
     kwargs_cookie = dict(domain=settings.COOKIES_DOMAIN, samesite="Strict")
     response.delete_cookie("staff_user", **kwargs_cookie)
+
     return response
