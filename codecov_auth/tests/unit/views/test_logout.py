9c8a6aadda06ed21d97a940103dbea3c33f9ef82
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-05-29 15:16:31 -0400

    feat: Allow logout by POST request (#580)

--- codecov_auth/tests/unit/views/test_logout.py
+++ codecov_auth/tests/unit/views/test_logout.py
@@ -11,6 +11,9 @@ class LogoutViewTest(TransactionTestCase):
     def _get(self, url):
         return self.client.get(url, content_type="application/json")
 
+    def _post(self, url):
+        return self.client.post(url, content_type="application/json")
+
     def _is_authenticated(self):
         response = self.client.post(
             "/graphql/gh",
@@ -20,10 +23,28 @@ class LogoutViewTest(TransactionTestCase):
         return response.json()["data"]["me"] is not None
 
     def test_logout_when_unauthenticated(self):
+        res = self._post("/logout")
+        assert res.status_code == 401
+
+    def test_logout_when_authenticated(self):
+        owner = OwnerFactory()
+        self.client = Client()
+        self.client.force_login_owner(owner)
+
+        res = self._post("/graphql/gh/")
+        self.assertEqual(self._is_authenticated(), True)
+
+        res = self._post("/logout")
+        self.assertEqual(res.status_code, 205)
+
+        res = self._get("/graphql/gh/")
+        self.assertEqual(self._is_authenticated(), False)
+
+    def test_get_logout_when_unauthenticated(self):
         res = self._get("/logout/gh")
         assert res.status_code == 302
 
-    def test_logout_when_authenticated(self):
+    def test_get_logout_when_authenticated(self):
         owner = OwnerFactory()
         self.client = Client()
         self.client.force_login_owner(owner)
@@ -38,7 +59,7 @@ class LogoutViewTest(TransactionTestCase):
         res = self._get("/graphql/gh/")
         self.assertEqual(self._is_authenticated(), False)
 
-    def test_logout_when_authenticated_with_redirect(self):
+    def test_get_logout_when_authenticated_with_redirect(self):
         owner = OwnerFactory()
         self.client = Client()
         self.client.force_login_owner(owner)
