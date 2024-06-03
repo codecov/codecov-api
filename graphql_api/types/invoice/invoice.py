fe25fa848f4a3a662e9e637f8021db9083954fff
Ajay Singh <ajay.singh@sentry.io>
2024-06-03 11:55:09 -0700

    update comments, add types

--- graphql_api/types/invoice/invoice.py
+++ graphql_api/types/invoice/invoice.py
@@ -2,6 +2,7 @@ from functools import cached_property
 from typing import List, Optional
 
 from ariadne import ObjectType
+from graphql import GraphQLResolveInfo
 from stripe import (
     Invoice,
     InvoiceLineItem,
@@ -13,96 +14,104 @@ invoice_bindable = ObjectType("Invoice")
 
 
 @invoice_bindable.field("id")
-def resolve_invoice_id(invoice: Invoice, info) -> str | None:
+def resolve_invoice_id(invoice: Invoice, info: GraphQLResolveInfo) -> str | None:
     return invoice["id"]
 
 
 @invoice_bindable.field("number")
-def resolve_invoice_number(invoice: Invoice, info) -> str | None:
+def resolve_invoice_number(invoice: Invoice, info: GraphQLResolveInfo) -> str | None:
     return invoice["number"]
 
 
 @invoice_bindable.field("status")
-def resolve_invoice_status(invoice: Invoice, info) -> str | None:
+def resolve_invoice_status(invoice: Invoice, info: GraphQLResolveInfo) -> str | None:
     return invoice["status"]
 
 
 @invoice_bindable.field("created")
-def resolve_invoice_created(invoice: Invoice, info) -> int:
+def resolve_invoice_created(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["created"]
 
 
 @invoice_bindable.field("periodStart")
-def resolve_invoice_period_start(invoice: Invoice, info) -> int:
+def resolve_invoice_period_start(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["period_start"]
 
 
 @invoice_bindable.field("periodEnd")
-def resolve_invoice_period_end(invoice: Invoice, info) -> int:
+def resolve_invoice_period_end(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["period_end"]
 
 
 @invoice_bindable.field("dueDate")
-def resolve_invoice_due_date(invoice: Invoice, info) -> int | None:
+def resolve_invoice_due_date(invoice: Invoice, info: GraphQLResolveInfo) -> int | None:
     return invoice["due_date"]
 
 
 @invoice_bindable.field("customerName")
-def resolve_invoice_customer_name(invoice: Invoice, info) -> str | None:
+def resolve_invoice_customer_name(
+    invoice: Invoice, info: GraphQLResolveInfo
+) -> str | None:
     return invoice["customer_name"]
 
 
-# NOTE: This doesn't currently look to be used in gazebo, maybe can remove?
+# NOTE: Not currently used in Gazebo, keep for address updates
 @invoice_bindable.field("customerAddress")
-def resolve_invoice_customer_address(invoice: Invoice, info) -> str | None:
+def resolve_invoice_customer_address(
+    invoice: Invoice, info: GraphQLResolveInfo
+) -> str | None:
     if invoice["customer_address"]:
         return str(invoice["customer_address"])
     return None
 
 
-# NOTE: This doesn't currently look to be used in gazebo, maybe can remove?
+# NOTE: Not currently used in Gazebo, keep for tax id updates
 @invoice_bindable.field("currency")
-def resolve_invoice_currency(invoice: Invoice, info) -> str:
+def resolve_invoice_currency(invoice: Invoice, info: GraphQLResolveInfo) -> str:
     return invoice["currency"]
 
 
 @invoice_bindable.field("amountPaid")
-def resolve_invoice_amount_paid(invoice: Invoice, info) -> int:
+def resolve_invoice_amount_paid(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["amount_paid"]
 
 
 @invoice_bindable.field("amountDue")
-def resolve_invoice_amount_due(invoice: Invoice, info) -> int:
+def resolve_invoice_amount_due(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["amount_due"]
 
 
 @invoice_bindable.field("total")
-def resolve_invoice_total(invoice: Invoice, info) -> int:
+def resolve_invoice_total(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["total"]
 
 
 @invoice_bindable.field("subtotal")
-def resolve_invoice_subtotal(invoice: Invoice, info) -> int:
+def resolve_invoice_subtotal(invoice: Invoice, info: GraphQLResolveInfo) -> int:
     return invoice["subtotal"]
 
 
 @invoice_bindable.field("lineItems")
-def resolve_invoice_line_items(invoice: Invoice, info) -> ListObject[InvoiceLineItem]:
+def resolve_invoice_line_items(
+    invoice: Invoice, info: GraphQLResolveInfo
+) -> ListObject[InvoiceLineItem]:
     return invoice["lines"]["data"]
 
 
 @invoice_bindable.field("footer")
-def resolve_invoice_footer(invoice: Invoice, info) -> str | None:
+def resolve_invoice_footer(invoice: Invoice, info: GraphQLResolveInfo) -> str | None:
     return invoice["footer"]
 
 
 @invoice_bindable.field("customerEmail")
-def resolve_invoice_customer_email(invoice: Invoice, info) -> str | None:
+def resolve_invoice_customer_email(
+    invoice: Invoice, info: GraphQLResolveInfo
+) -> str | None:
     return invoice["customer_email"]
 
 
 @invoice_bindable.field("defaultPaymentMethod")
 def resolve_invoice_default_payment_method(
-    invoice: Invoice, info
+    invoice: Invoice, info: GraphQLResolveInfo
 ) -> PaymentMethod | None:
     return invoice["default_payment_method"]
