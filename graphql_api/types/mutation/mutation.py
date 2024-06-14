51372ce26066473249fd5b126715bb5322bdb408
Rohit <rohit.vinnakota@sentry.io>
2024-06-14 12:23:20 -0400

    lint

--- graphql_api/types/mutation/mutation.py
+++ graphql_api/types/mutation/mutation.py
@@ -1,43 +1,55 @@
 from ariadne import MutationType
 
-from .activate_measurements import (error_activate_measurements,
-                                    resolve_activate_measurements)
+from .activate_measurements import (
+    error_activate_measurements,
+    resolve_activate_measurements,
+)
 from .cancel_trial import error_cancel_trial, resolve_cancel_trial
 from .create_api_token import error_create_api_token, resolve_create_api_token
-from .create_user_token import (error_create_user_token,
-                                resolve_create_user_token)
+from .create_user_token import error_create_user_token, resolve_create_user_token
 from .delete_component_measurements import (
-    error_delete_component_measurements, resolve_delete_component_measurements)
+    error_delete_component_measurements,
+    resolve_delete_component_measurements,
+)
 from .delete_flag import error_delete_flag, resolve_delete_flag
 from .delete_session import error_delete_session, resolve_delete_session
 from .erase_repository import error_erase_repository, resolve_erase_repository
 from .onboard_user import error_onboard_user, resolve_onboard_user
-from .regenerate_org_upload_token import (error_generate_org_upload_token,
-                                          resolve_regenerate_org_upload_token)
-from .regenerate_repository_token import (error_regenerate_repository_token,
-                                          resolve_regenerate_repository_token)
+from .regenerate_org_upload_token import (
+    error_generate_org_upload_token,
+    resolve_regenerate_org_upload_token,
+)
+from .regenerate_repository_token import (
+    error_regenerate_repository_token,
+    resolve_regenerate_repository_token,
+)
 from .regenerate_repository_upload_token import (
     error_regenerate_repository_upload_token,
-    resolve_regenerate_repository_upload_token)
-from .revoke_user_token import (error_revoke_user_token,
-                                resolve_revoke_user_token)
-from .save_sentry_state import (error_save_sentry_state,
-                                resolve_save_sentry_state)
-from .save_terms_agreement import (error_save_terms_agreement,
-                                   resolve_save_terms_agreement)
+    resolve_regenerate_repository_upload_token,
+)
+from .revoke_user_token import error_revoke_user_token, resolve_revoke_user_token
+from .save_sentry_state import error_save_sentry_state, resolve_save_sentry_state
+from .save_terms_agreement import (
+    error_save_terms_agreement,
+    resolve_save_terms_agreement,
+)
 from .set_yaml_on_owner import error_set_yaml_error, resolve_set_yaml_on_owner
 from .start_trial import error_start_trial, resolve_start_trial
-from .store_event_metrics import (error_store_event_metrics,
-                                  resolve_store_event_metrics)
-from .sync_with_git_provider import (error_sync_with_git_provider,
-                                     resolve_sync_with_git_provider)
-from .update_default_organization import (error_update_default_organization,
-                                          resolve_update_default_organization)
+from .store_event_metrics import error_store_event_metrics, resolve_store_event_metrics
+from .sync_with_git_provider import (
+    error_sync_with_git_provider,
+    resolve_sync_with_git_provider,
+)
+from .update_default_organization import (
+    error_update_default_organization,
+    resolve_update_default_organization,
+)
 from .update_profile import error_update_profile, resolve_update_profile
-from .update_repository import (error_update_repository,
-                                resolve_update_repository)
-from .update_self_hosted_settings import (error_update_self_hosted_settings,
-                                          resolve_update_self_hosted_settings)
+from .update_repository import error_update_repository, resolve_update_repository
+from .update_self_hosted_settings import (
+    error_update_self_hosted_settings,
+    resolve_update_self_hosted_settings,
+)
 
 mutation_bindable = MutationType()
 
