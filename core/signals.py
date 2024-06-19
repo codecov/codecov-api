fcf399804327ad18304b5843b9867efe2fef7100
Adrian <adrian@codecov.io>
2024-06-18 21:46:25 -0600

    add signal to dispatch sync task to gcloud

--- core/signals.py
+++ core/signals.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 from google.cloud import pubsub_v1
 
 from core.models import Repository
+from shared.django_apps.core.models import Commit
 
 _pubsub_publisher = None
 
@@ -34,6 +35,26 @@ def update_repository(sender, instance: Repository, **kwargs):
                         "type": "repo",
                         "sync": "one",
                         "id": instance.repoid,
+                    }
+                ).encode("utf-8"),
+            )
+
+@receiver(post_save, sender=Commit, dispatch_uid="shelter_sync_commit")
+def update_commit(sender, instance: Commit, **kwargs):
+    branch = instance.branch
+    if branch and ":" in branch:
+        pubsub_project_id = settings.SHELTER_PUBSUB_PROJECT_ID
+        topic_id = settings.SHELTER_PUBSUB_SYNC_REPO_TOPIC_ID
+        if pubsub_project_id and topic_id:
+            publisher = _get_pubsub_publisher()
+            topic_path = publisher.topic_path(pubsub_project_id, topic_id)
+            publisher.publish(
+                topic_path,
+                json.dumps(
+                    {
+                        "type": "commit",
+                        "sync": "one",
+                        "id": instance.id,
                     }
                 ).encode("utf-8"),
             )
