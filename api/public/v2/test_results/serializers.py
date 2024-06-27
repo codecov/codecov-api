5dc076fa33e2b04f33b2f8872283cd0812524907
Rula Abu Hasna <rula@codecov.io>
2024-06-27 13:08:04 +0300

    Update with single retrieve

--- api/public/v2/test_results/serializers.py
+++ api/public/v2/test_results/serializers.py
@@ -5,9 +5,9 @@ from reports.models import TestInstance
 
 
 class TestInstanceSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(label="id")
     failure_message = serializers.CharField(label="test name")
-    duration_seconds = serializers.BooleanField(label="duration in seconds")
-    flaky_status = serializers.DateTimeField(label="flaky status")
+    duration_seconds = serializers.FloatField(label="duration in seconds")
     commitid = serializers.CharField(label="commit SHA")
     outcome = serializers.CharField(label="outcome")
     branch = serializers.CharField(label="branch name")
@@ -15,6 +15,7 @@ class TestInstanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestInstance
         read_only_fields = (
+            "id",
             "failure_message",
             "duration_seconds",
             "flaky_status",
