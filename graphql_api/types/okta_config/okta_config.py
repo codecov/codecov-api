03ff64d0f777bb625486ee269e46e7c0a8faf4b0
Rula Abu Hasna <rula@codecov.io>
2024-07-02 00:38:02 +0300

    update with more typing

--- graphql_api/types/okta_config/okta_config.py
+++ graphql_api/types/okta_config/okta_config.py
@@ -6,25 +6,25 @@ okta_config_bindable = ObjectType("OktaConfig")
 
 
 @okta_config_bindable.field("clientId")
-def resolve_client_id(okta_config, info):
+def resolve_client_id(okta_config: OktaSettings, info) -> str:
     return okta_config.client_id
 
 
 @okta_config_bindable.field("clientSecret")
-def resolve_client_secret(okta_config, info):
+def resolve_client_secret(okta_config: OktaSettings, info) -> str:
     return okta_config.client_secret
 
 
 @okta_config_bindable.field("url")
-def resolve_url(okta_config, info):
+def resolve_url(okta_config: OktaSettings, info) -> str:
     return okta_config.url
 
 
 @okta_config_bindable.field("enabled")
-def resolve_enabled(okta_config, info):
+def resolve_enabled(okta_config: OktaSettings, info) -> bool:
     return okta_config.enabled
 
 
 @okta_config_bindable.field("enforced")
-def resolve_enforced(okta_config, info):
+def resolve_enforced(okta_config: OktaSettings, info) -> bool:
     return okta_config.enforced
