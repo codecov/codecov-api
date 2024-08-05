2ce548e7f4c746388b6e921e00fec3fe9d031fdb
Ajay Singh <ajay.singh@sentry.io>
2024-08-05 15:07:01 -0700

    add validation for user in calling org before canceling or starting trial

--- codecov_auth/commands/owner/interactors/tests/test_start_trial.py
+++ codecov_auth/commands/owner/interactors/tests/test_start_trial.py
@@ -6,7 +6,7 @@ from asgiref.sync import async_to_sync
 from django.test import TransactionTestCase
 from freezegun import freeze_time
 
-from codecov.commands.exceptions import ValidationError
+from codecov.commands.exceptions import Unauthorized, ValidationError
 from codecov_auth.models import Owner
 from codecov_auth.tests.factories import OwnerFactory
 from plan.constants import TRIAL_PLAN_SEATS, PlanName, TrialDaysAmount, TrialStatus
@@ -29,6 +29,18 @@ class StartTrialInteractorTest(TransactionTestCase):
         )
         with pytest.raises(ValidationError):
             self.execute(current_user=current_user, org_username="some-other-username")
+
+    def test_cancel_trial_raises_exception_when_current_user_not_part_of_org(self):
+        current_user = OwnerFactory(
+            username="random-user-123",
+            service="github",
+        )
+        OwnerFactory(
+            username="random-user-456",
+            service="github",
+        )
+        with pytest.raises(Unauthorized):
+            self.execute(current_user=current_user, org_username="random-user-456")
 
     @freeze_time("2022-01-01T00:00:00")
     def test_start_trial_raises_exception_when_owners_trial_status_is_ongoing(self):
