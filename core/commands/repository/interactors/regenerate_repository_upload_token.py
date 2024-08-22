f15956b3a360abbc5ddd2696b94e2dbccbcacdde
Rohit <rohit.vinnakota@sentry.io>
2024-08-21 21:06:39 -0400

    Ensure repo author is the same

--- core/commands/repository/interactors/regenerate_repository_upload_token.py
+++ core/commands/repository/interactors/regenerate_repository_upload_token.py
@@ -10,12 +10,12 @@ from core.models import Repository
 class RegenerateRepositoryUploadTokenInteractor(BaseInteractor):
     @sync_to_async
     def execute(self, repo_name: str, owner_username: str) -> uuid.UUID:
-        current_owner = Owner.objects.filter(
+        author = Owner.objects.filter(
             username=owner_username, service=self.service
         ).first()
         repo = (
-            Repository.objects.viewable_repos(current_owner)
-            .filter(name=repo_name)
+            Repository.objects.viewable_repos(self.current_owner)
+            .filter(author=author, name=repo_name)
             .first()
         )
         if not repo:
