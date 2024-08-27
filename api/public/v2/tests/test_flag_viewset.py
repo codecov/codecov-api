ae3f4d8e598f9584042dab301f29c3500332d896
JerrySentry <142266253+JerrySentry@users.noreply.github.com>
2024-08-27 14:34:14 -0400

    REST API: add flag coverage to flag list endpoint (#765)

--- api/public/v2/tests/test_flag_viewset.py
+++ api/public/v2/tests/test_flag_viewset.py
@@ -2,11 +2,39 @@ from unittest.mock import patch
 
 from django.test import TestCase
 from rest_framework.reverse import reverse
+from shared.reports.resources import Report, ReportFile, ReportLine
+from shared.utils.sessions import Session
 
 from codecov_auth.tests.factories import OwnerFactory
-from core.tests.factories import RepositoryFactory
+from core.tests.factories import CommitFactory, RepositoryFactory
 from reports.tests.factories import RepositoryFlagFactory
 from utils.test_utils import APIClient
+
+
+def flags_report():
+    report = Report()
+    session_a_id, _ = report.add_session(Session(flags=["foo"]))
+    session_b_id, _ = report.add_session(Session(flags=["bar"]))
+
+    file_a = ReportFile("foo/file1.py")
+    file_a.append(1, ReportLine.create(coverage=1, sessions=[[session_a_id, 1]]))
+    file_a.append(2, ReportLine.create(coverage=0, sessions=[[session_a_id, 0]]))
+    file_a.append(3, ReportLine.create(coverage=1, sessions=[[session_a_id, 1]]))
+    file_a.append(5, ReportLine.create(coverage=1, sessions=[[session_a_id, 1]]))
+    file_a.append(6, ReportLine.create(coverage=0, sessions=[[session_a_id, 0]]))
+    file_a.append(8, ReportLine.create(coverage=1, sessions=[[session_a_id, 1]]))
+    file_a.append(9, ReportLine.create(coverage=1, sessions=[[session_a_id, 1]]))
+    file_a.append(10, ReportLine.create(coverage=0, sessions=[[session_a_id, 0]]))
+    report.append(file_a)
+
+    file_b = ReportFile("bar/file2.py")
+    file_b.append(12, ReportLine.create(coverage=1, sessions=[[session_b_id, 1]]))
+    file_b.append(
+        51, ReportLine.create(coverage="1/2", type="b", sessions=[[session_b_id, 2]])
+    )
+    report.append(file_b)
+
+    return report
 
 
 @patch("api.shared.repo.repository_accessors.RepoAccessors.get_repo_permissions")
@@ -38,7 +66,7 @@ class FlagViewSetTestCase(TestCase):
         )
         return self.client.get(url)
 
-    def test_flag_list(self, get_repo_permissions):
+    def test_flag_list_no_commit(self, get_repo_permissions):
         get_repo_permissions.return_value = (True, True)
 
         res = self._request_flags()
@@ -47,6 +75,55 @@ class FlagViewSetTestCase(TestCase):
             "count": 2,
             "next": None,
             "previous": None,
-            "results": [{"flag_name": "foo"}, {"flag_name": "bar"}],
+            "results": [
+                {"flag_name": "foo", "coverage": None},
+                {"flag_name": "bar", "coverage": None},
+            ],
+            "total_pages": 1,
+        }
+
+    def test_flag_list_no_report(self, get_repo_permissions):
+        CommitFactory(
+            author=self.org,
+            repository=self.repo,
+            branch=self.repo.branch,
+        )
+        get_repo_permissions.return_value = (True, True)
+
+        res = self._request_flags()
+        assert res.status_code == 200
+        assert res.json() == {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {"flag_name": "foo", "coverage": None},
+                {"flag_name": "bar", "coverage": None},
+            ],
+            "total_pages": 1,
+        }
+
+    @patch("shared.reports.api_report_service.build_report_from_commit")
+    def test_flag_list_with_coverage(
+        self, build_report_from_commit, get_repo_permissions
+    ):
+        CommitFactory(
+            author=self.org,
+            repository=self.repo,
+            branch=self.repo.branch,
+        )
+        get_repo_permissions.return_value = (True, True)
+        build_report_from_commit.return_value = flags_report()
+
+        res = self._request_flags()
+        assert res.status_code == 200
+        assert res.json() == {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {"flag_name": "foo", "coverage": 62.5},
+                {"flag_name": "bar", "coverage": 100},
+            ],
             "total_pages": 1,
         }
