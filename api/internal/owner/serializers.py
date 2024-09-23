2b6e43e6e8c22d074a709e147744a4f39f0650c9
Spencer Murray <spencer.murray@sentry.io>
2024-09-23 17:23:02 -0400

    Make attribute access safer

--- api/internal/owner/serializers.py
+++ api/internal/owner/serializers.py
@@ -236,7 +236,7 @@ class ScheduleDetailSerializer(serializers.Serializer):
                 extra=dict(
                     ownerid=schedule.metadata.obo_organization,
                     requesting_user_id=schedule.metadata.obo,
-                    phases=schedule["phases"],
+                    phases=schedule.get("phases", "no phases"),
                 ),
             )
             return None
