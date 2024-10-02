023d1fbff60e1a9ca62aa5fca3776afa4cf7adfa
joseph-sentry <joseph.sawaya@sentry.io>
2024-10-02 15:37:08 -0400

    fix: add typing and remove unused imports

--- graphql_api/types/test_results_headers/test_results_headers.py
+++ graphql_api/types/test_results_headers/test_results_headers.py
@@ -1,23 +1,33 @@
+from typing import TypedDict
+
 from ariadne import ObjectType
+from graphql import GraphQLResolveInfo
 
 test_results_headers_bindable = ObjectType("TestResultsHeaders")
 
 
+class TestResultsHeaders(TypedDict):
+    total_run_time: float
+    slowest_tests_duration: float
+    fails: int
+    skips: int
+
+
 @test_results_headers_bindable.field("totalRunTime")
-def resolve_name(obj, _) -> float:
+def resolve_name(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> float:
     return obj["total_run_time"]
 
 
 @test_results_headers_bindable.field("slowestTestsRunTime")
-def resolve_updated_at(obj, _) -> float:
+def resolve_updated_at(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> float:
     return obj["slowest_tests_duration"]
 
 
 @test_results_headers_bindable.field("totalFails")
-def resolve_commits_failed(obj, _) -> int:
+def resolve_commits_failed(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> int:
     return obj["fails"]
 
 
 @test_results_headers_bindable.field("totalSkips")
-def resolve_failure_rate(obj, _) -> int:
+def resolve_failure_rate(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> int:
     return obj["skips"]
