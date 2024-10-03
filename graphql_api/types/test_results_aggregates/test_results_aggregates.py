80c160e3f428137214b9c38365472fc301b7df6f
joseph-sentry <joseph.sawaya@sentry.io>
2024-10-03 10:21:39 -0400

    fix: address feedback

--- graphql_api/types/test_results_aggregates/test_results_aggregates.py
+++ graphql_api/types/test_results_aggregates/test_results_aggregates.py
@@ -13,21 +13,21 @@ class TestResultsHeaders(TypedDict):
     skips: int
 
 
-@test_results_headers_bindable.field("totalRunTime")
+@test_results_aggregates_bindable.field("totalRunTime")
 def resolve_name(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> float:
     return obj["total_run_time"]
 
 
-@test_results_headers_bindable.field("slowestTestsRunTime")
+@test_results_aggregates_bindable.field("slowestTestsRunTime")
 def resolve_updated_at(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> float:
     return obj["slowest_tests_duration"]
 
 
-@test_results_headers_bindable.field("totalFails")
+@test_results_aggregates_bindable.field("totalFails")
 def resolve_commits_failed(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> int:
     return obj["fails"]
 
 
-@test_results_headers_bindable.field("totalSkips")
+@test_results_aggregates_bindable.field("totalSkips")
 def resolve_failure_rate(obj: TestResultsHeaders, _: GraphQLResolveInfo) -> int:
     return obj["skips"]
