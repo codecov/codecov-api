d0a25eeb110b2ab88e00753137cbd62b5c3f503c
Spencer Murray <spencer.murray@sentry.io>
2024-10-03 15:35:11 -0400

    Add info type on account resolver signatures

--- graphql_api/types/account/account.py
+++ graphql_api/types/account/account.py
@@ -1,4 +1,5 @@
 from ariadne import ObjectType
+from graphql import GraphQLResolveInfo
 
 from codecov.db import sync_to_async
 from codecov_auth.models import Account, OktaSettings
@@ -7,15 +8,15 @@ account_bindable = ObjectType("Account")
 
 
 @account_bindable.field("name")
-def resolve_name(account: Account, info) -> str:
+def resolve_name(account: Account, info: GraphQLResolveInfo) -> str:
     return account.name
 
 
 @account_bindable.field("oktaConfig")
 @sync_to_async
-def resolve_okta_config(account: Account, info) -> OktaSettings | None:
+def resolve_okta_config(account: Account, info: GraphQLResolveInfo) -> OktaSettings | None:
     return OktaSettings.objects.filter(account_id=account.pk).first()
 
 @account_bindable.field("totalSeatCount")
-def resolve_total_seat_count(account: Account, info) -> int:
+def resolve_total_seat_count(account: Account, info: GraphQLResolveInfo) -> int:
     return account.plan_seat_count + account.free_seat_count
