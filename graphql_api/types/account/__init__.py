9ac0d1622b5488ce3bb88940e4c3e9e624c14387
Spencer Murray <spencer.murray@sentry.io>
2024-10-08 09:54:41 -0400

    Remove get_current_license exports

--- graphql_api/types/account/__init__.py
+++ graphql_api/types/account/__init__.py
@@ -1,5 +1,3 @@
-from shared.license import get_current_license
-
 from .account import account, account_bindable
 
-__all__ = ["get_current_license", "account_bindable", "account"]
+__all__ = ["account_bindable", "account"]
