3f4c3485e7d77647fd1a6262a5b4a52f9aaaabcb
Spencer Murray <159931558+spalmurray-codecov@users.noreply.github.com>
2024-10-08 12:56:01 -0400

    feat: Add organizations array resolver on Account type (#863)

--- graphql_api/types/config/__init__.py
+++ graphql_api/types/config/__init__.py
@@ -1,5 +1,3 @@
-from shared.license import get_current_license
-
 from graphql_api.helpers.ariadne import ariadne_load_local_graphql
 
 from .config import config_bindable
@@ -7,4 +5,4 @@ from .config import config_bindable
 config = ariadne_load_local_graphql(__file__, "config.graphql")
 
 
-__all__ = ["get_current_license", "config_bindable"]
+__all__ = ["config_bindable"]
