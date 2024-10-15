c74cf2f95a67a07a44d00a77ac2023cb48f82dbc
Rohit <rohit.vinnakota@sentry.io>
2024-10-15 10:51:15 -0700

    Add sentiment class

---
+++ utils/sentiment.py
@@ -0,0 +1,18 @@
+from textblob import TextBlob
+
+def detect_sentiment(text):
+    sentences = text.split('. ')
+    sentiment_analysis = []
+    
+    for sentence in sentences:
+        blob = TextBlob(sentence)
+        polarity = blob.sentiment.polarity
+        if polarity > 0:
+            sentiment = 'Positive'
+        elif polarity < 0:
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Neutral'
+        sentiment_analysis.append((sentence, sentiment))
+    
+    return sentiment_analysis
