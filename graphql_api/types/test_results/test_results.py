1df975843083d5e98d8cbc846951f5b1ec925d55
joseph-sentry <joseph.sawaya@sentry.io>
2024-10-15 17:02:10 -0400

    fix: address feedback

--- graphql_api/types/test_results/test_results.py
+++ graphql_api/types/test_results/test_results.py
@@ -14,9 +14,9 @@ class TestDict(TypedDict):
     last_duration: float
     flake_rate: float
     total_fail_count: int
+    total_flaky_fail_count: int
     total_skip_count: int
     total_pass_count: int
-    total_flaky_fail_count: int
     computed_name: str | None
 
 
@@ -63,6 +63,11 @@ def resolve_total_fail_count(test: TestDict, _: GraphQLResolveInfo) -> int:
     return test["total_fail_count"]
 
 
+@test_result_bindable.field("totalFlakyFailCount")
+def resolve_total_flaky_fail_count(test: TestDict, _: GraphQLResolveInfo) -> int:
+    return test["total_flaky_fail_count"]
+
+
 @test_result_bindable.field("totalSkipCount")
 def resolve_total_skip_count(test: TestDict, _: GraphQLResolveInfo) -> int:
     return test["total_skip_count"]
@@ -71,8 +76,3 @@ def resolve_total_skip_count(test: TestDict, _: GraphQLResolveInfo) -> int:
 @test_result_bindable.field("totalPassCount")
 def resolve_total_pass_count(test: TestDict, _: GraphQLResolveInfo) -> int:
     return test["total_pass_count"]
-
-
-@test_result_bindable.field("totalFlakyFailCount")
-def resolve_total_flaky_fail_count(test: TestDict, _: GraphQLResolveInfo) -> int:
-    return test["total_flaky_fail_count"]
