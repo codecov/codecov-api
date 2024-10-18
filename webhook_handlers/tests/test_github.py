f15481caa111b4c9ecf52c842aaf833387f06147
Matt Hammerly <matt.hammerly@sentry.io>
2024-10-18 13:06:32 -0700

    webhooks: force sync when user is removed from org

--- webhook_handlers/tests/test_github.py
+++ webhook_handlers/tests/test_github.py
@@ -1127,8 +1127,10 @@ class GithubWebhookHandlerTests(APITestCase):
             repos_affected=[("12321", "R_kgDOG2tZYQ"), ("12343", "R_kgDOG2tABC")],
         )
 
+    @patch("services.task.TaskService.refresh")
     def test_organization_with_removed_action_removes_user_from_org_and_activated_user_list(
         self,
+        mock_refresh,
     ):
         org = OwnerFactory(service_id="4321", service=Service.GITHUB.value)
         user = OwnerFactory(
@@ -1149,7 +1151,13 @@ class GithubWebhookHandlerTests(APITestCase):
         user.refresh_from_db()
         org.refresh_from_db()
 
-        assert org.ownerid not in user.organizations
+        mock_refresh.assert_called_with(
+            ownerid=user.ownerid,
+            username=user.username,
+            sync_teams=True,
+            sync_repos=True,
+            using_integration=False,
+        )
         assert user.ownerid not in org.plan_activated_users
 
     def test_organization_member_removed_with_nonexistent_org_doesnt_crash(self):
