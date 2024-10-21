f8ac094b4128e181571cc6d2821c7e42f82bb75a
calvin-codecov <calvin.yau@sentry.io>
2024-10-21 14:20:16 -0700

    feat: Clean up old graphql commit fields (#898)

--- graphql_api/tests/test_pull.py
+++ graphql_api/tests/test_pull.py
@@ -52,8 +52,10 @@ default_pull_request_detail_query = """
         username
     }
     head {
-        totals {
-            coverage
+        coverageAnalytics {
+            totals {
+                coverage
+            }
         }
     }
     comparedTo {
@@ -80,8 +82,10 @@ pull_request_detail_query_with_bundle_analysis = """
         username
     }
     head {
-        totals {
-            coverage
+        coverageAnalytics {
+            totals {
+                coverage
+            }
         }
     }
     comparedTo {
@@ -188,7 +192,7 @@ class TestPullRequestList(GraphQLTestHelper, TransactionTestCase):
             "pullId": my_pull.pullid,
             "updatestamp": "2021-02-02T00:00:00",
             "author": {"username": "test-pull-user"},
-            "head": {"totals": None},
+            "head": {"coverageAnalytics": {"totals": None}},
             "comparedTo": None,
             "compareWithBase": {
                 "__typename": "MissingBaseCommit",
@@ -445,7 +449,7 @@ class TestPullRequestList(GraphQLTestHelper, TransactionTestCase):
             "pullId": my_pull.pullid,
             "updatestamp": "2021-02-02T00:00:00",
             "author": {"username": "test-pull-user"},
-            "head": {"totals": {"coverage": 78.38}},
+            "head": {"coverageAnalytics": {"totals": {"coverage": 78.38}}},
             "comparedTo": {"commitid": "9asd78fa7as8d8fa97s8d7fgagsd8fa9asd8f77s"},
             "compareWithBase": {
                 "__typename": "Comparison",
@@ -604,7 +608,7 @@ class TestPullRequestList(GraphQLTestHelper, TransactionTestCase):
             "pullId": my_pull.pullid,
             "updatestamp": "2021-02-02T00:00:00",
             "author": {"username": "test-pull-user"},
-            "head": {"totals": {"coverage": 78.38}},
+            "head": {"coverageAnalytics": {"totals": {"coverage": 78.38}}},
             "comparedTo": {"commitid": "9asd78fa7as8d8fa97s8d7fgagsd8fa9asd8f77s"},
             "compareWithBase": {
                 "__typename": "Comparison",
