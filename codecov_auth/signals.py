2b93d9b54ed134a90dcb112fdaec839700c848c2
Nora Shapiro <nora.shapiro@sentry.io>
2024-10-29 16:49:19 -0700

    add signal for Owner

--- codecov_auth/signals.py
+++ codecov_auth/signals.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.conf import settings
 from django.db.models.signals import post_save
@@ -6,6 +7,8 @@ from django.dispatch import receiver
 from google.cloud import pubsub_v1
 
 from codecov_auth.models import OrganizationLevelToken, Owner, OwnerProfile
+
+log = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Owner)
@@ -45,3 +48,38 @@ def update_org_token(sender, instance: OrganizationLevelToken, **kwargs):
                 }
             ).encode("utf-8"),
         )
+
+
+@receiver(post_save, sender=Owner, dispatch_uid="shelter_sync_owner")
+def update_owner(sender, instance: Owner, **kwargs):
+    """
+    Shelter tracks a limited set of Owner fields - only update if those fields have changed.
+    """
+    created = kwargs["created"]
+    tracked_fields = [
+        "upload_token_required_for_public_repos",
+        "username",
+        "service",
+    ]
+    if created or any(instance.tracker.has_changed(field) for field in tracked_fields):
+        pubsub_project_id = settings.SHELTER_PUBSUB_PROJECT_ID
+        topic_id = settings.SHELTER_PUBSUB_SYNC_REPO_TOPIC_ID
+        if pubsub_project_id and topic_id:
+            try:
+                publisher = _get_pubsub_publisher()
+                topic_path = publisher.topic_path(pubsub_project_id, topic_id)
+                publisher.publish(
+                    topic_path,
+                    json.dumps(
+                        {
+                            "type": "owner",
+                            "sync": "one",
+                            "id": instance.ownerid,
+                        }
+                    ).encode("utf-8"),
+                )
+            except Exception as e:
+                log.warning(
+                    "Failed to publish message for owner",
+                    extra=dict(owner_id=instance.pk, error=e),
+                )
--- core/tests/test_signals.py
+++ core/tests/test_signals.py
@@ -3,6 +3,7 @@ from unittest.mock import call
 
 import pytest
 from django.test import override_settings
+from shared.django_apps.codecov_auth.tests.factories import OwnerFactory
 from shared.django_apps.core.tests.factories import CommitFactory, RepositoryFactory
 
 
@@ -18,22 +19,30 @@ def test_shelter_repo_sync(mocker):
     publish = mocker.patch("google.cloud.pubsub_v1.PublisherClient.publish")
 
     # this triggers the publish via Django signals
-    repo = RepositoryFactory(repoid=91728376)
+    repo = RepositoryFactory(repoid=91728376, author=OwnerFactory(ownerid=555))
 
     # triggers publish on create
-    publish.assert_called_once_with(
-        "projects/test-project-id/topics/test-topic-id",
-        b'{"type": "repo", "sync": "one", "id": 91728376}',
+    publish.assert_has_calls(
+        [
+            call(
+                "projects/test-project-id/topics/test-topic-id",
+                b'{"type": "owner", "sync": "one", "id": 555}',
+            ),
+            call(
+                "projects/test-project-id/topics/test-topic-id",
+                b'{"type": "repo", "sync": "one", "id": 91728376}',
+            ),
+        ]
     )
 
     repo.upload_token = "b69cf44c-89d8-48c2-80c9-5508610d3ced"
     repo.save()
 
     publish_calls = publish.call_args_list
-    assert len(publish_calls) == 2
+    assert len(publish_calls) == 3
 
     # triggers publish on update
-    assert publish_calls[1] == call(
+    assert publish_calls[2] == call(
         "projects/test-project-id/topics/test-topic-id",
         b'{"type": "repo", "sync": "one", "id": 91728376}',
     )
@@ -43,7 +52,7 @@ def test_shelter_repo_sync(mocker):
 
     publish_calls = publish.call_args_list
     # does not trigger another publish
-    assert len(publish_calls) == 2
+    assert len(publish_calls) == 3
 
 
 @override_settings(
@@ -57,15 +66,21 @@ def test_shelter_commit_sync(mocker):
     publish = mocker.patch("google.cloud.pubsub_v1.PublisherClient.publish")
 
     # this triggers the publish via Django signals - has to have this format
-    commit = CommitFactory(id=167829367, branch="random:branch")
+    owner = OwnerFactory(ownerid=555)
+    commit = CommitFactory(
+        id=167829367,
+        branch="random:branch",
+        author=owner,
+        repository=RepositoryFactory(author=owner),
+    )
 
     publish_calls = publish.call_args_list
-    # Twice cause there's a signal triggered when the commit factory creates a Repository
+    # 3x cause there's a signal triggered when the commit factory requires a Repository and Owner
     # which can't be null
-    assert len(publish_calls) == 2
+    assert len(publish_calls) == 3
 
     # triggers publish on update
-    assert publish_calls[1] == call(
+    assert publish_calls[2] == call(
         "projects/test-project-id/topics/test-topic-id",
         b'{"type": "commit", "sync": "one", "id": 167829367}',
     )
@@ -75,4 +90,4 @@ def test_shelter_commit_sync(mocker):
 
     publish_calls = publish.call_args_list
     # does not trigger another publish since unchanged length
-    assert len(publish_calls) == 2
+    assert len(publish_calls) == 3
