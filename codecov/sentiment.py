67db86c772b91963455b000eab5a85221f2b7ff4
Rohit <rohit.vinnakota@sentry.io>
2024-10-30 11:57:02 -0400

    Lint

--- codecov/sentiment.py
+++ codecov/sentiment.py
@@ -1,9 +1,10 @@
 from textblob import TextBlob
 
+
 class SentimentAnalyzer:
     def __init__(self):
         pass
-    
+
     def get_sentiment(self, sentence: str) -> str:
         polarity = self._get_polarity(sentence)
         if polarity > 0:
@@ -12,23 +13,23 @@ class SentimentAnalyzer:
             return "negative"
         else:
             return "neutral"
-    
+
     def get_polarity_score(self, sentence: str) -> float:
         return self._get_polarity(sentence)
-    
+
     def get_subjectivity_score(self, sentence: str) -> float:
         return self._get_subjectivity(sentence)
-    
+
     def is_neutral(self, sentence: str) -> bool:
         return self.get_sentiment(sentence) == "neutral"
-    
+
     def is_positive(self, sentence: str) -> bool:
         return self.get_sentiment(sentence) == "positive"
 
     def _get_polarity(self, sentence: str) -> float:
         analysis = TextBlob(sentence)
         return analysis.sentiment.polarity
-    
+
     def _get_subjectivity(self, sentence: str) -> float:
         analysis = TextBlob(sentence)
         return analysis.sentiment.subjectivity
