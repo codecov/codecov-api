93b20ef56c9d0efe4a0a3825e571d50f6b588ddb
Tom Hu <thomas.hu@sentry.io>
2024-11-01 13:50:38 -0500

    fix: add the actual utc time

--- utils/logging_configuration.py
+++ utils/logging_configuration.py
@@ -9,7 +9,10 @@ class BaseLogger(JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(BaseLogger, self).add_fields(log_record, record, message_dict)
 
-        log_record["utctime"] = log_record.get('asctime')
+        asctime_format = '%Y-%m-%d %H:%M:%S,%f'
+        asctime = datetime.strptime(log_record.get('asctime'), asctime_format)
+
+        log_record["utctime"] = asctime.isoformat()
 
     def format_json_on_new_lines(self, json_str):
         # Parse the input JSON string
