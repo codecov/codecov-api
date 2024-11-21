4f32d40060e493943096ab257b8f6d4f6eb0f075
Adrian <adrian@codecov.io>
2024-11-21 15:15:36 -0700

    WIP

--- codecov_auth/commands/owner/interactors/set_yaml_on_owner.py
+++ codecov_auth/commands/owner/interactors/set_yaml_on_owner.py
@@ -2,6 +2,8 @@ import html
 from typing import Optional
 
 import yaml
+from shared.django_apps.core.models import Repository
+from shared.django_apps.utils.model_utils import get_ownerid_if_member
 from shared.validation.exceptions import InvalidYamlException
 from shared.yaml.validation import validate_yaml
 
@@ -50,13 +52,45 @@ class SetYamlOnOwnerInteractor(BaseInteractor):
             message = f"Error at {str(e.error_location)}: {e.error_message}"
             raise ValidationError(message)
 
+    def yaml_side_effects(self, old_yaml: dict, new_yaml: dict):
+        old_yaml_branch = old_yaml.get("codecov", {}).get("branch")
+        new_yaml_branch = new_yaml.get("codecov", {}).get("branch")
+
+        # Update all repositories from owner if branch is updated in yaml
+        if new_yaml_branch != old_yaml_branch:
+            repos = Repository.objects.filter(self.owner.ownerid)
+            repos.update(
+                branch=new_yaml_branch or old_yaml_branch
+            )  # Keeps old_branch if new_branch is None
+
+        old_yaml_bot = old_yaml.get("codecov", {}).get("bot")
+        new_yaml_bot = new_yaml.get("codecov", {}).get("bot")
+
+        # Update owner's bot column if bot is updated in yaml
+        if new_yaml_bot != old_yaml_bot:
+            new_bot = (
+                get_ownerid_if_member(
+                    service=self.owner.service,
+                    owner_username=new_yaml_bot,
+                    owner_id=self.owner.ownerid,
+                )
+                or old_yaml_bot
+                or None
+            )
+            self.owner.bot = new_bot
+            self.owner.save()
+
     @sync_to_async
     def execute(self, username: str, yaml_input: str) -> Owner:
         self.validate()
         self.owner = self.get_owner(username)
         self.authorize()
+        old_yaml = self.owner.yaml
         self.owner.yaml = self.convert_yaml_to_dict(yaml_input)
         if self.owner.yaml:
             self.owner.yaml[OWNER_YAML_TO_STRING_KEY] = yaml_input
         self.owner.save()
+
+        # side effects
+        self.yaml_side_effects(old_yaml=old_yaml, new_yaml=self.owner.yaml)
         return self.owner
