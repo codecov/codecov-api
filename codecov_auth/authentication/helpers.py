59d775683471752b7a46ddfc2619c312c0df1912
Tony Le <tony.le@sentry.io>
2024-11-26 11:52:32 -0500

    Use different matching to retain commitid

--- codecov_auth/authentication/helpers.py
+++ codecov_auth/authentication/helpers.py
@@ -13,7 +13,7 @@ class UploadInfo(NamedTuple):
 def get_upload_info_from_request_path(request: HttpRequest) -> UploadInfo | None:
     path_info = request.get_full_path_info()
     # The repo part comes from https://stackoverflow.com/a/22312124
-    upload_views_prefix_regex = r"\/upload\/(\w+)\/([\w\.@:_/\-~]+)\/(commits(?:\/([a-f0-9]{40}))?|upload-coverage)"
+    upload_views_prefix_regex = r"\/upload\/(\w+)\/([\w\.@:_/\-~]+)\/(commits|upload-coverage)(?:\/([a-f0-9]{40}))?"
     match = re.search(upload_views_prefix_regex, path_info)
 
     if match is None:
@@ -21,6 +21,9 @@ def get_upload_info_from_request_path(request: HttpRequest) -> UploadInfo | None
 
     service = match.group(1)
     encoded_slug = match.group(2)
-    commitid = match.group(3)
+    if match.group(3) == "commits":
+        commitid = match.group(4)
+    else:
+        commitid = None
 
     return UploadInfo(service, encoded_slug, commitid)
