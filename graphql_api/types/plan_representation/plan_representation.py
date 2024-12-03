1d7539e1694eda95d4f321429e75b4bb4b93f2ed
Rola Abuhasna <rula@codecov.io>
2024-12-03 15:39:11 +0100

    chore: Migrate to using shared imports for the plan service  (#1014)

--- graphql_api/types/plan_representation/plan_representation.py
+++ graphql_api/types/plan_representation/plan_representation.py
@@ -2,9 +2,9 @@ from typing import List, Optional
 
 from ariadne import ObjectType
 from shared.plan.constants import PlanData
+from shared.plan.service import PlanService
 
 from graphql_api.helpers.ariadne import ariadne_load_local_graphql
-from plan.service import PlanService
 
 plan_representation = ariadne_load_local_graphql(
     __file__, "plan_representation.graphql"
