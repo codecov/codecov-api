ec522e08c6fa08a31787408519a0be0da4c4e04d
Rohit <rohit.vinnakota@sentry.io>
2024-12-09 10:32:51 -0500

    Only send total misses

--- services/comparison.py
+++ services/comparison.py
@@ -884,7 +884,6 @@ class ImpactedFile:
     def misses_count(self) -> int:
         total_misses = 0
         total_misses += self._direct_misses_count
-        total_misses += self._unintended_misses_count
         return total_misses
 
     @cached_property
