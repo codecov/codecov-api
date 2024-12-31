24784cebf6e45c3c7f79c38c146c2c47aaf233c8
Suejung Shin <suejung.shin@sentry.io>
2024-12-31 10:53:46 -0800

    fix: Restrict deactivated enterprise user access (#910)

--- graphql_api/types/query/query.py
+++ graphql_api/types/query/query.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Optional
 
 from ariadne import ObjectType
 from django.conf import settings
@@ -20,7 +20,7 @@ def query_name(info: GraphQLResolveInfo) -> Optional[str]:
         return info.operation.name.value
 
 
-def configure_sentry_scope(query_name: str):
+def configure_sentry_scope(query_name: Optional[str]) -> None:
     # this sets the Sentry transaction name to the GraphQL query name which
     # should make it easier to search/filter transactions
     # we're configuring this here since it's the main entrypoint into GraphQL resolvers
@@ -33,14 +33,16 @@ def configure_sentry_scope(query_name: str):
 
 @query_bindable.field("me")
 @sync_to_async
-def resolve_me(_, info) -> Optional[Owner]:
+def resolve_me(_: Any, info: GraphQLResolveInfo) -> Optional[Owner]:
     configure_sentry_scope(query_name(info))
     # will be `None` for anonymous users or users w/ no linked owners
     return info.context["request"].current_owner
 
 
 @query_bindable.field("owner")
-def resolve_owner(_, info, username):
+async def resolve_owner(
+    _: Any, info: GraphQLResolveInfo, username: str
+) -> Optional[Owner]:
     configure_sentry_scope(query_name(info))
 
     service = info.context["service"]
@@ -50,11 +52,15 @@ def resolve_owner(_, info, username):
         if not user or not user.is_authenticated:
             raise UnauthorizedGuestAccess()
 
-    return get_owner(service, username)
+        target = await get_owner(service, username)
+        if user.ownerid not in target.plan_activated_users:
+            raise UnauthorizedGuestAccess()
+
+    return await get_owner(service, username)
 
 
 @query_bindable.field("config")
-def resolve_config(_, info):
+def resolve_config(_: Any, info: GraphQLResolveInfo) -> object:
     configure_sentry_scope(query_name(info))
 
     # we have to return something here just to allow access to the child resolvers
