aaed14e74525db4f5e9d5637a71b42486ec20c8f
Trent Schmidt <trent@codecov.io>
2025-01-02 12:59:54 -0500

    Update gunicorn conf (#1077)

--- prod.sh
+++ prod.sh
@@ -7,8 +7,12 @@ if [ -f "/usr/local/bin/berglas" ]; then
   prefix="berglas exec --"
 fi
 
-export PROMETHEUS_MULTIPROC_DIR="${PROMETHEUS_MULTIPROC_DIR:-$HOME/.prometheus}"
-rm -r ${PROMETHEUS_MULTIPROC_DIR?}/* 2> /dev/null
-mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
+GUNICORN_WORKERS=${GUNICORN_WORKERS:-2}
+if [ "$GUNICORN_WORKERS" -gt 1 ];
+then
+    export PROMETHEUS_MULTIPROC_DIR="${PROMETHEUS_MULTIPROC_DIR:-$HOME/.prometheus}"
+    rm -r ${PROMETHEUS_MULTIPROC_DIR?}/* 2> /dev/null
+    mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
+fi
 
-$prefix gunicorn codecov.wsgi:application --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT} --timeout "${GUNICORN_TIMEOUT:-600}"
+$prefix gunicorn codecov.wsgi:application --workers=${GUNICORN_WORKERS} --threads=${GUNICORN_THREADS:-1} --worker-connections=${GUNICORN_WORKER_CONNECTIONS:-1000} --bind 0.0.0.0:8000 --access-logfile '-' --statsd-host ${STATSD_HOST}:${STATSD_PORT} --timeout "${GUNICORN_TIMEOUT:-600}" --disable-redirect-access-to-syslog --max-requests=50000 --max-requests-jitter=300
--- staging.sh
+++ staging.sh
@@ -8,13 +8,12 @@ prefix=""
 if [ -f "/usr/local/bin/berglas" ]; then
   prefix="berglas exec --"
 fi
-suffix=""
-if [[ "$STATSD_HOST" ]]; then
-  suffix="--statsd-host ${STATSD_HOST}:${STATSD_PORT}"
+
+if [ "$GUNICORN_WORKERS" -gt 1 ];
+then
+    export PROMETHEUS_MULTIPROC_DIR="${PROMETHEUS_MULTIPROC_DIR:-$HOME/.prometheus}"
+    rm -r ${PROMETHEUS_MULTIPROC_DIR?}/* 2> /dev/null
+    mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
 fi
 
-export PROMETHEUS_MULTIPROC_DIR="${PROMETHEUS_MULTIPROC_DIR:-$HOME/.prometheus}"
-rm -r ${PROMETHEUS_MULTIPROC_DIR?}/* 2> /dev/null
-mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
-
-$prefix gunicorn codecov.wsgi:application --reload --workers=2 --bind 0.0.0.0:8000 --access-logfile '-' --timeout "${GUNICORN_TIMEOUT:-600}" $suffix
+$prefix gunicorn codecov.wsgi:application --reload --workers=${GUNICORN_WORKERS:-2} --threads=${GUNICORN_THREADS:-1} --worker-connections=${GUNICORN_WORKER_CONNECTIONS:-1000} --bind 0.0.0.0:8000 --access-logfile '-' --timeout "${GUNICORN_TIMEOUT:-600}" --disable-redirect-access-to-syslog --config=gunicorn.conf.py
