7e5a1a982f53c6c1fb9346d2fdaf9395e811c474
Rohit <rohit.vinnakota@sentry.io>
2025-01-15 17:28:37 -0500

    undo

--- api/internal/tests/test_pagination.py
+++ api/internal/tests/test_pagination.py
@@ -1,47 +1,41 @@
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
-from shared.django_apps.core.tests.factories import OwnerFactory, RepositoryFactory
+from shared.django_apps.core.tests.factories import OwnerFactory
+
 from utils.test_utils import Client
-from codecov_auth.models import GithubAppInstallation
-from graphql_api.types.owner.owner import AI_FEATURES_GH_APP_ID
 
 
-class GenAIAuthViewTests(APITestCase):
+class PageNumberPaginationTests(APITestCase):
     def setUp(self):
         self.client = Client()
+        self.owner = OwnerFactory(plan="users-free", plan_user_count=5)
+        self.users = [
+            OwnerFactory(organizations=[self.owner.ownerid]),
+            OwnerFactory(organizations=[self.owner.ownerid]),
+            OwnerFactory(organizations=[self.owner.ownerid]),
+        ]
 
-        self.owner = OwnerFactory(service="github")
-        self.repo1 = RepositoryFactory(author=self.owner, name="Repo1")
-        self.repo2 = RepositoryFactory(author=self.owner, name="Repo2")
-
-        self.ai_install = GithubAppInstallation.objects.create(
-            app_id=AI_FEATURES_GH_APP_ID,
-            owner=self.owner,
-            repository_service_ids=[self.repo1.service_id, self.repo2.service_id],
-        )
-
-    def test_no_owner_id(self):
-        url = reverse("gen-ai-consent")
+    def test_pagination_returned_page_size(self):
         self.client.force_login_owner(self.owner)
-        response = self.client.get(url)
-        assert response.status_code == 200
-        assert response.data["is_valid"] is False
 
-    def test_owner_without_install(self):
-        # Remove the AI installation so it doesn't exist
-        self.ai_install.delete()
+        def _list(kwargs={}, query_params={}):
+            if not kwargs:
+                kwargs = {
+                    "service": self.owner.service,
+                    "owner_username": self.owner.username,
+                }
+            return self.client.get(
+                reverse("users-list", kwargs=kwargs), data=query_params
+            )
 
-        url = reverse("gen-ai-consent")
-        self.client.force_login_owner(self.owner)
-        response = self.client.get(url, data={"owner_id": self.owner.id})
-        assert response.status_code == 200
-        assert response.data["is_valid"] is False
+        response = _list()
 
-    def test_valid_owner_with_install(self):
-        url = reverse("gen-ai-consent")
-        self.client.force_login_owner(self.owner)
-        response = self.client.get(url, data={"owner_id": self.owner.id})
-        assert response.status_code == 200
-        assert response.data["is_valid"] is True
-        assert len(response.data["repos"]) == 2
-        assert set(response.data["repos"]) == {"Repo1", "Repo2"}
+        assert response.data["total_pages"] == 1
+
+        response = _list(query_params={"page_size": "1"})
+
+        assert response.data["total_pages"] == 3
+
+        response = _list(query_params={"page_size": "100"})
+
+        assert response.data["total_pages"] == 1
