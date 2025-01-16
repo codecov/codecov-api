f28e35c652a1134ba3dd6ead7176306dd3413d5c
Calvin Yau <calvin.yau@sentry.io>
2025-01-16 01:48:22 -0800

    fix: Revert changes that need to be rolled out later in phase

--- graphql_api/tests/mutation/test_save_terms_agreement.py
+++ graphql_api/tests/mutation/test_save_terms_agreement.py
@@ -18,6 +18,20 @@ query = """
 
 
 class SaveTermsAgreementMutationTest(GraphQLTestHelper, TransactionTestCase):
+    def _request_deprecated(self, owner=None):
+        return self.gql_request(
+            query,
+            variables={"input": {"termsAgreement": True, "customerIntent": "Business"}},
+            owner=owner,
+        )
+
+    def _request_invalid_customer_intent_deprecated(self, owner=None):
+        return self.gql_request(
+            query,
+            variables={"input": {"termsAgreement": True, "customerIntent": "invalid"}},
+            owner=owner,
+        )
+
     def _request(self, owner=None):
         return self.gql_request(
             query,
@@ -30,6 +44,31 @@ class SaveTermsAgreementMutationTest(GraphQLTestHelper, TransactionTestCase):
             },
             owner=owner,
         )
+
+    def test_invalid_customer_intent_deprecated(self):
+        owner = OwnerFactory()
+        assert self._request_invalid_customer_intent_deprecated(owner=owner) == {
+            "saveTermsAgreement": {
+                "error": {
+                    "__typename": "ValidationError",
+                    "message": "Invalid customer intent provided",
+                }
+            }
+        }
+
+    def test_unauthenticated_deprecated(self):
+        assert self._request_deprecated() == {
+            "saveTermsAgreement": {
+                "error": {
+                    "__typename": "UnauthenticatedError",
+                    "message": "You are not authenticated",
+                }
+            }
+        }
+
+    def test_authenticated_deprecated(self):
+        owner = OwnerFactory()
+        assert self._request_deprecated(owner=owner) == {"saveTermsAgreement": None}
 
     def test_unauthenticated(self):
         assert self._request() == {
