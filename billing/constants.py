b66b68ef28e4ebf43569b8d4003f34a82a01fb17
Suejung Shin <suejung.shin@sentry.io>
2025-01-21 14:59:27 -0800

    feat: Add ACH webhook flows

--- billing/constants.py
+++ billing/constants.py
@@ -20,6 +20,15 @@ class StripeWebhookEvents:
         "subscription_schedule.created",
         "subscription_schedule.released",
         "subscription_schedule.updated",
+        "setup_intent.succeeded",
+        "setup_intent.payment_method_attached",
+        "setup_intent.payment_method_automatically_updated",
+        "setup_intent.payment_method_changed",
+        "setup_intent.payment_method_expired",
+        "setup_intent.payment_method_removed",
+        "setup_intent.setup_future_usage_updated",
+        "setup_intent.setup_future_usage_expired",
+        "setup_intent.setup_future_usage_automatically_updated",
     )
 
 
