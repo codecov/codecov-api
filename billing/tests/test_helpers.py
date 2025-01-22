91ea64bd5e4dd1f2b372fd2f35feeeff2924dfcd
RulaKhaled <rola.abuhasna@sentry.io>
2025-01-22 18:39:20 +0100

    update api with mocked plans

--- billing/tests/test_helpers.py
+++ billing/tests/test_helpers.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, override_settings
 from shared.django_apps.core.tests.factories import OwnerFactory
-from shared.plan.constants import ENTERPRISE_CLOUD_USER_PLAN_REPRESENTATIONS
-
+from shared.plan.constants import ENTERPRISE_CLOUD_USER_PLAN_REPRESENTATIONS, TierName, PlanName
+from shared.django_apps.codecov_auth.tests.factories import PlanFactory, TierFactory
 from billing.helpers import on_enterprise_plan
 
 
@@ -12,6 +12,17 @@ class HelpersTestCase(TestCase):
         assert on_enterprise_plan(owner) == True
 
     def test_on_enterprise_plan_enterpise_cloud(self):
+        enterprise_tier = TierFactory(tier_name=TierName.ENTERPRISE.value)
+        PlanFactory(
+            name=PlanName.ENTERPRISE_CLOUD_MONTHLY.value,
+            tier=enterprise_tier,
+            is_active=True,
+        )
+        PlanFactory(
+            name=PlanName.ENTERPRISE_CLOUD_YEARLY.value,
+            tier=enterprise_tier,
+            is_active=True,
+        )
         for plan in ENTERPRISE_CLOUD_USER_PLAN_REPRESENTATIONS.keys():
             owner = OwnerFactory(plan=plan)
             assert on_enterprise_plan(owner) == True
