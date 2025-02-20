1ef6d080a039afa8c27c857ce0b780d44cb2b3b1
Rohit <rohit.vinnakota@sentry.io>
2025-02-20 14:41:18 -0500

    Undo

--- graphql_api/actions/repository.py
+++ graphql_api/actions/repository.py
@@ -5,45 +5,53 @@ import sentry_sdk
 from django.db.models import QuerySet
 from shared.django_apps.codecov_auth.models import GithubAppInstallation, Owner
 from shared.django_apps.core.models import Repository
-
 from utils.config import get_config
+
 
 log = logging.getLogger(__name__)
 AI_FEATURES_GH_APP_ID = get_config("github", "ai_features_app_id")
 
 
-def basic_filters(queryset: QuerySet, filters: dict[str, Any]) -> QuerySet:
+def apply_filters_to_queryset(
+    queryset: QuerySet, filters: dict[str, Any] | None, owner: Owner
+) -> QuerySet:
     filters = filters or {}
-    if repo_names := filters.get("repo_names"):
+    term = filters.get("term")
+    active = filters.get("active")
+    activated = filters.get("activated")
+    repo_names = filters.get("repo_names")
+    is_public = filters.get("is_public")
+    ai_enabled = filters.get("ai_enabled")
+
+    if repo_names:
         queryset = queryset.filter(name__in=repo_names)
-    if term := filters.get("term"):
+    if term:
         queryset = queryset.filter(name__contains=term)
-    for field in ("activated", "active"):
-        if filters.get(field) is not None:
-            queryset = queryset.filter(**{field: filters[field]})
-    if filters.get("is_public") is not None:
-        queryset = queryset.filter(private=not filters["is_public"])
+    if activated is not None:
+        queryset = queryset.filter(activated=activated)
+    if active is not None:
+        queryset = queryset.filter(active=active)
+    if is_public is not None:
+        queryset = queryset.filter(private=not is_public)
+    if ai_enabled is not None:
+        queryset = filter_queryset_by_ai_enabled_repos(queryset, owner)
     return queryset
 
 
 def filter_queryset_by_ai_enabled_repos(queryset: QuerySet, owner: Owner) -> QuerySet:
-    install = GithubAppInstallation.objects.filter(
+    ai_features_app_install = GithubAppInstallation.objects.filter(
         app_id=AI_FEATURES_GH_APP_ID, owner=owner
     ).first()
-    if not install:
+
+    if not ai_features_app_install:
         return Repository.objects.none()
-    if install.repository_service_ids:
-        queryset = queryset.filter(service_id__in=install.repository_service_ids)
+
+    if ai_features_app_install.repository_service_ids:
+        queryset = queryset.filter(
+            service_id__in=ai_features_app_install.repository_service_ids
+        )
+
     return queryset
-
-
-def apply_filters(
-    queryset: QuerySet, filters: dict[str, Any] | None, owner: Owner
-) -> QuerySet:
-    filters = filters or {}
-    if filters.get("ai_enabled"):
-        return filter_queryset_by_ai_enabled_repos(queryset, owner)
-    return basic_filters(queryset, filters)
 
 
 @sentry_sdk.trace
@@ -54,14 +62,23 @@ def list_repository_for_owner(
     okta_account_auths: list[int],
     exclude_okta_enforced_repos: bool = True,
 ) -> QuerySet:
+    queryset = Repository.objects.viewable_repos(current_owner)
     filters = filters or {}
-    qs = Repository.objects.viewable_repos(current_owner)
-    if filters.get("ai_enabled"):
-        return filter_queryset_by_ai_enabled_repos(qs, owner)
+    ai_enabled_filter = filters.get("ai_enabled")
+
+    if ai_enabled_filter:
+        return filter_queryset_by_ai_enabled_repos(queryset, owner)
+
     if exclude_okta_enforced_repos:
-        qs = qs.exclude_accounts_enforced_okta(okta_account_auths)
-    qs = qs.with_recent_coverage().with_latest_commit_at().filter(author=owner)
-    return basic_filters(qs, filters)
+        queryset = queryset.exclude_accounts_enforced_okta(okta_account_auths)
+
+    if not ai_enabled_filter:
+        queryset = (
+            queryset.with_recent_coverage().with_latest_commit_at().filter(author=owner)
+        )
+
+    queryset = apply_filters_to_queryset(queryset, filters, owner)
+    return queryset
 
 
 @sentry_sdk.trace
@@ -71,14 +88,16 @@ def search_repos(
     okta_account_auths: list[int],
     exclude_okta_enforced_repos: bool = True,
 ) -> QuerySet:
-    filters = filters or {}
-    authors = [current_owner.ownerid] + (current_owner.organizations or [])
-    qs = Repository.objects.viewable_repos(current_owner)
+    authors_from = [current_owner.ownerid] + (current_owner.organizations or [])
+    queryset = Repository.objects.viewable_repos(current_owner)
+
     if exclude_okta_enforced_repos:
-        qs = qs.exclude_accounts_enforced_okta(okta_account_auths)
-    qs = (
-        qs.with_recent_coverage()
+        queryset = queryset.exclude_accounts_enforced_okta(okta_account_auths)
+
+    queryset = (
+        queryset.with_recent_coverage()
         .with_latest_commit_at()
-        .filter(author__ownerid__in=authors)
+        .filter(author__ownerid__in=authors_from)
     )
-    return apply_filters(qs, filters, current_owner)
+    queryset = apply_filters_to_queryset(queryset, filters)
+    return queryset
