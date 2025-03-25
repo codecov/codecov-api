75ae71ba38b222ece0c40d846af13570459c2e92
Spencer Murray <spencer.murray@sentry.io>
2025-03-25 14:15:51 -0400

    Add tests for private repo cases

--- graphs/tests/test_bundle_badge_handler.py
+++ graphs/tests/test_bundle_badge_handler.py
@@ -162,6 +162,47 @@ class TestBundleBadgeHandler(APITestCase):
         assert expected_badge == badge
         assert response.status_code == status.HTTP_200_OK
 
+    def test_unknown_badge_private_repo_wrong_token(self):
+        gh_owner = OwnerFactory(service="github")
+        RepositoryFactory(
+            author=gh_owner, active=True, private=True, name="repo1", image_token="asdf"
+        )
+        response = self._get(
+            kwargs={
+                "service": "gh",
+                "owner_username": gh_owner.username,
+                "repo_name": "repo1",
+                "ext": "svg",
+                "bundle": "asdf",
+            }
+        )
+        expected_badge = """<svg xmlns="http://www.w3.org/2000/svg" width="106" height="20">
+    <linearGradient id="CodecovBadgeGradient" x2="0" y2="100%">
+        <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
+        <stop offset="1" stop-opacity=".1" />
+    </linearGradient>
+    <mask id="CodecovBadgeMask106px">
+        <rect width="106" height="20" rx="3" fill="#fff" />
+    </mask>
+    <g mask="url(#CodecovBadgeMask106px)">
+        <path fill="#555" d="M0 0h47v20H0z" />
+        <path fill="#2C2433" d="M47 0h59v20H47z" />
+        <path fill="url(#CodecovBadgeGradient)" d="M0 0h106v20H0z" />
+    </g>
+    <g fill="#fff" text-anchor="left" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
+        <text x="5" y="15" fill="#010101" fill-opacity=".3">bundle</text>
+        <text x="5" y="14">bundle</text>
+        <text x="52" y="15" fill="#010101" fill-opacity=".3">unknown</text>
+        <text x="52" y="14">unknown</text>
+    </g>
+</svg>
+"""
+        badge = response.content.decode("utf-8")
+        badge = [line.strip() for line in badge.split("\n")]
+        expected_badge = [line.strip() for line in expected_badge.split("\n")]
+        assert expected_badge == badge
+        assert response.status_code == status.HTTP_200_OK
+
     def test_unknown_badge_no_branch(self):
         gh_owner = OwnerFactory(service="github")
         RepositoryFactory(author=gh_owner, active=True, private=False, name="repo1")
@@ -457,6 +498,38 @@ class TestBundleBadgeHandler(APITestCase):
                 "bundle": "asdf",
             },
             data={"precision": "asdf"},
+        )
+        expected_badge = "1.23MB"
+
+        badge = response.content.decode("utf-8")
+        badge = [line.strip() for line in badge.split("\n")]
+        expected_badge = [line.strip() for line in expected_badge.split("\n")]
+        assert expected_badge == badge
+
+    @patch("graphs.views.load_report")
+    def test_bundle_badge_private_repo_correct_token(self, mock_load_report):
+        gh_owner = OwnerFactory(service="github")
+        repo = RepositoryFactory(
+            author=gh_owner, active=True, private=True, name="repo1", image_token="asdf"
+        )
+        branch = BranchFactory(name="main", repository=repo)
+        repo.branch = branch
+        commit = CommitFactory(
+            repository=repo, commitid=repo.branch.head, branch="main"
+        )
+        branch.head = commit.commitid
+
+        mock_load_report.return_value = MockBundleAnalysisReport()
+
+        response = self._get(
+            kwargs={
+                "service": "gh",
+                "owner_username": gh_owner.username,
+                "repo_name": "repo1",
+                "ext": "txt",
+                "bundle": "asdf",
+            },
+            data={"token": "asdf"},
         )
         expected_badge = "1.23MB"
 
