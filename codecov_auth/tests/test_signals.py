005f2f82f36bba4e6aeb7268bd2a2713e1efcf2f
Arpad Borsos <arpad.borsos@sentry.io>
2025-04-04 13:07:06 +0200

    turn shelter off in tests, and do mocking a different way

--- codecov_auth/tests/test_signals.py
+++ codecov_auth/tests/test_signals.py
@@ -2,42 +2,37 @@ from unittest import mock
 from unittest.mock import call
 
 import pytest
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from shared.django_apps.codecov_auth.models import Service
 from shared.django_apps.codecov_auth.tests.factories import (
     OrganizationLevelTokenFactory,
     OwnerFactory,
 )
 
+from utils.shelter import ShelterPubsub
+
 
 @pytest.mark.django_db
 def test_shelter_org_token_sync(mocker):
-    publish = mocker.patch("google.cloud.pubsub_v1.PublisherClient.publish")
+    publish = mocker.patch("utils.shelter.ShelterPubsub.publish")
 
     # this triggers the publish via Django signals
     OrganizationLevelTokenFactory(id=91728376, owner=OwnerFactory(ownerid=111))
 
     publish.assert_has_calls(
         [
-            call(
-                "projects/test-project-id/topics/test-topic-id",
-                b'{"type": "owner", "sync": "one", "id": 111}',
-            ),
-            call(
-                "projects/test-project-id/topics/test-topic-id",
-                b'{"type": "org_token", "sync": "one", "id": 91728376}',
-            ),
+            call({"type": "owner", "sync": "one", "id": 111}),
+            call({"type": "org_token", "sync": "one", "id": 91728376}),
         ]
     )
 
 
-@mock.patch("google.cloud.pubsub_v1.PublisherClient.publish")
+@mock.patch("utils.shelter.ShelterPubsub.publish")
 class TestCodecovAuthSignals(TestCase):
     def test_sync_on_create(self, mock_publish):
         OwnerFactory(ownerid=12345)
         mock_publish.assert_called_once_with(
-            "projects/test-project-id/topics/test-topic-id",
-            b'{"type": "owner", "sync": "one", "id": 12345}',
+            {"type": "owner", "sync": "one", "id": 12345}
         )
 
     def test_sync_on_update_upload_token_required_for_public_repos(self, mock_publish):
@@ -46,14 +41,8 @@ class TestCodecovAuthSignals(TestCase):
         owner.save()
         mock_publish.assert_has_calls(
             [
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
+                call({"type": "owner", "sync": "one", "id": 12345}),
+                call({"type": "owner", "sync": "one", "id": 12345}),
             ]
         )
 
@@ -63,14 +52,8 @@ class TestCodecovAuthSignals(TestCase):
         owner.save()
         mock_publish.assert_has_calls(
             [
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
+                call({"type": "owner", "sync": "one", "id": 12345}),
+                call({"type": "owner", "sync": "one", "id": 12345}),
             ]
         )
 
@@ -80,14 +63,8 @@ class TestCodecovAuthSignals(TestCase):
         owner.save()
         mock_publish.assert_has_calls(
             [
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
-                call(
-                    "projects/test-project-id/topics/test-topic-id",
-                    b'{"type": "owner", "sync": "one", "id": 12345}',
-                ),
+                call({"type": "owner", "sync": "one", "id": 12345}),
+                call({"type": "owner", "sync": "one", "id": 12345}),
             ]
         )
 
@@ -96,26 +73,31 @@ class TestCodecovAuthSignals(TestCase):
         owner.name = "world"
         owner.save()
         mock_publish.assert_called_once_with(
-            "projects/test-project-id/topics/test-topic-id",
-            b'{"type": "owner", "sync": "one", "id": 12345}',
+            {"type": "owner", "sync": "one", "id": 12345}
         )
 
-    @mock.patch("logging.Logger.warning")
-    def test_sync_error(self, mock_log, mock_publish):
-        mock_publish.side_effect = Exception("publish error")
 
-        OwnerFactory(ownerid=12345)
+@mock.patch("logging.Logger.warning")
+@mock.patch("google.cloud.pubsub_v1.PublisherClient.publish")
+@mock.patch("utils.shelter.ShelterPubsub.get_instance")
+@override_settings(SHELTER_ENABLED=True)
+@pytest.mark.django_db
+def test_sync_error(mock_instance, mock_publish, mock_log):
+    mock_instance.return_value = ShelterPubsub()
+    mock_publish.side_effect = Exception("publish error")
 
-        # publish is still called, raises an Exception
-        mock_publish.assert_called_once_with(
-            "projects/test-project-id/topics/test-topic-id",
-            b'{"type": "owner", "sync": "one", "id": 12345}',
-        )
+    OwnerFactory(ownerid=12345)
 
-        mock_log.assert_called_once_with(
-            "Failed to publish a message",
-            extra=dict(
-                data_to_publish={"type": "owner", "sync": "one", "id": 12345},
-                error=mock_publish.side_effect,
-            ),
-        )
+    # publish is still called, raises an Exception
+    mock_publish.assert_called_once_with(
+        "projects/test-project-id/topics/test-topic-id",
+        b'{"type": "owner", "sync": "one", "id": 12345}',
+    )
+
+    mock_log.assert_called_once_with(
+        "Failed to publish a message",
+        extra=dict(
+            data_to_publish={"type": "owner", "sync": "one", "id": 12345},
+            error=mock_publish.side_effect,
+        ),
+    )
